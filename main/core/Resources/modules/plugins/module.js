2ea471ebee9d3448bc1a17c238b448e9e7926702
ngodfraind <nicolas.godfraind@gmail.com>
2016-04-19 17:02:16 +0200

    js linter

--- main/core/Resources/modules/plugins/module.js
+++ main/core/Resources/modules/plugins/module.js
@@ -10,12 +10,12 @@ import PluginDirective from './Directive/PluginDirective'
 import Interceptors from '../interceptorsDefault'
 
 angular.module('PluginManager', [
-    'ui.fos-js-router',
-    'ui.bootstrap',
-    'ui.bootstrap.tpls',
-    'ui.translation',
+  'ui.fos-js-router',
+  'ui.bootstrap',
+  'ui.bootstrap.tpls',
+  'ui.translation',
 ])
-   .controller('PluginController', ['$http', '$uibModal', PluginController])
-   .controller('WarningController', WarningController)
-   .directive('pluginManager', () => new PluginDirective)
-   .config(Interceptors)
+  .controller('PluginController', ['$http', '$uibModal', PluginController])
+  .controller('WarningController', WarningController)
+  .directive('pluginManager', () => new PluginDirective)
+  .config(Interceptors)
