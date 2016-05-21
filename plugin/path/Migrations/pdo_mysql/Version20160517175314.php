3b6eba9a411243baa0ef2c54c8cb1d02bf974635
Dechepy olivier <dechepy.olivier@chu-amiens.fr>
2016-05-21 07:36:06 +0200

    php-cs-fixer pass on bundle

--- plugin/path/Migrations/pdo_mysql/Version20160517175314.php
+++ plugin/path/Migrations/pdo_mysql/Version20160517175314.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration based on mapping information: modify it with caution
+ * Auto-generated migration based on mapping information: modify it with caution.
  *
  * Generation date: 2016/05/17 05:53:15
  */
@@ -14,17 +14,17 @@ class Version20160517175314 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE innova_path 
             ADD is_complete_blocking_condition TINYINT(1) NOT NULL
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE innova_path 
             DROP is_complete_blocking_condition
-        ");
+        ');
     }
 }
