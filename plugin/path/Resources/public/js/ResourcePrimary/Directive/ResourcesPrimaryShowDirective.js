87379d03a1b100d269fe826dbe5946ccc23799e2
dtisch <david.tischmacher@gmail.com>
2016-06-27 15:42:58 +0200

    Removed commented code and log statements, added CSS fix

--- plugin/path/Resources/public/js/ResourcePrimary/Directive/ResourcesPrimaryShowDirective.js
+++ plugin/path/Resources/public/js/ResourcePrimary/Directive/ResourcesPrimaryShowDirective.js
@@ -19,8 +19,6 @@
                 bindToController: true,
                 link: function (scope, element, attr) {
                     $(window).on('message',function(e) {
-                        console.log('received');
-                        console.log(e);
 
                         if (  (typeof e.originalEvent.data === 'string') && (e.originalEvent.data.indexOf('documentHeight:') > -1) ) {
 
@@ -30,43 +28,7 @@
                             // do stuff with the height
                             $(element).css('height', parseInt(height) + 15);
                         }
-
-
-
-                        // Check that message being passed is the documentHeight
-                        /*if (typeof e.data === 'object' && e.data.documentHeight) {
-                         }*/
                     });
-
-                    /*var iframeChangeTimeout = null;
-                     var resizeIframe = function (element) {
-                     var newheight = element.contentDocument.body.scrollHeight;
-                     element.height = (newheight) + "px";
-                     console.log(newheight);
-                     var height = $(element).find('body').first().height();
-                     /!*console.log(height);
-                     if (height) {
-                     $(element).css('height', height + 15);
-                     }*!/
-                     };
-                     // Manage the height of the iFrame
-                     element.on('load', function () {
-                     console.log('load iframe')
-                     resizeIframe(element);
-                     $(element.contentDocument.body).on('resize', function () {
-                     resizeIframe(element)
-                     });
-                     });
-                     $(window).on('resize', function () {
-                     resizeIframe(element)
-                     });*/
-
-
-
-                    /*clearTimeout(iframeChangeTimeout);
-                     iframeChangeTimeout = setTimeout(function () {
-                     resizeIframe(element)
-                     }, 300);*/
                 }
             };
         }
