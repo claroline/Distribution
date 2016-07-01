dec879b0356d5a769e3e0884e08581c36ef4a7b6
pitrackster <pitrackster@gmail.com>
2016-07-01 11:29:09 +0200

    import jquery in observe

--- plugin/video-player/Resources/modules/watcher/observe.js
+++ plugin/video-player/Resources/modules/watcher/observe.js
@@ -1,3 +1,4 @@
+import $ from 'jquery'
 export default function observe (selector, callback, containers = [document.body]) {
   window.MutationObserver = window.MutationObserver || window.WebKitMutationObserver
 
