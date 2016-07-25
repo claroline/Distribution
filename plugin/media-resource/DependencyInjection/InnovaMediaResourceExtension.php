ab6c6358fbb3884af55b697327d5251a81f26f21
pitrackster <pitrackster@gmail.com>
2016-07-25 10:10:21 +0200

    extension file

--- plugin/media-resource/DependencyInjection/InnovaMediaResourceExtension.php
+++ plugin/media-resource/DependencyInjection/InnovaMediaResourceExtension.php
@@ -2,9 +2,7 @@
 
 namespace Innova\MediaResourceBundle\DependencyInjection;
 
-use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
-use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 class InnovaMediaResourceExtension extends Extension
@@ -14,7 +12,5 @@ class InnovaMediaResourceExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        //$locator = new FileLocator(__DIR__.'/../Resources/config');
-        //$loader = new YamlFileLoader($container, $locator);
     }
 }
