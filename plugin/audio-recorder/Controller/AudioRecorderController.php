14713a6ff1d882aa6e6f14cb59d70baff68af9b4
pitrackster <pitrackster@gmail.com>
2016-07-26 14:37:43 +0200

    move configuration in plugin administration

--- plugin/audio-recorder/Controller/AudioRecorderController.php
+++ plugin/audio-recorder/Controller/AudioRecorderController.php
@@ -2,14 +2,15 @@
 
 namespace Innova\AudioRecorderBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Innova\AudioRecorderBundle\Manager\AudioRecorderManager;
 use Innova\AudioRecorderBundle\Entity\AudioRecorderConfiguration;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Innova\AudioRecorderBundle\Form\Type\AudioRecorderConfigurationType;
+use Innova\AudioRecorderBundle\Manager\AudioRecorderManager;
 use JMS\DiExtraBundle\Annotation as DI;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class AudioRecorderController extends Controller
 {
@@ -17,12 +18,30 @@ class AudioRecorderController extends Controller
 
     /**
      * @DI\InjectParams({
-     *      "manager"         = @DI\Inject("innova.audio_recorder.manager")
+     *      "manager"         = @DI\Inject("innova.audio_recorder.manager"),
      * })
      */
     public function __construct(AudioRecorderManager $manager)
     {
         $this->manager = $manager;
+    }
+
+    /**
+     * @Route(
+     *     "/configure/form",
+     *     name="audio_recorder_config_form"
+     * )
+     * @ParamConverter("authenticatedUser", options={"authenticatedUser" = true})
+     */
+    public function pluginConfigureFormAction()
+    {
+        $config = $this->manager->getConfig();
+        $form = $this->container->get('form.factory')->create(new AudioRecorderConfigurationType(), $config);
+
+        return $this->render(
+            'InnovaAudioRecorderBundle:AudioRecorder:options.form.html.twig',
+            ['form' => $form->createView(), 'id' => $config->getId()]
+        );
     }
 
     /**
@@ -35,13 +54,13 @@ class AudioRecorderController extends Controller
         $postData = $request->request->get('audio_recorder_configuration');
         if (isset($postData['max_try']) && isset($postData['max_recording_time'])) {
             $this->manager->updateConfiguration($config, $postData);
-            $msg = $this->get('translator')->trans('config_update_success', array(), 'tools');
+            $msg = $this->get('translator')->trans('config_update_success', [], 'tools');
             $this->get('session')->getFlashBag()->set('success', $msg);
         } else {
-            $msg = $this->get('translator')->trans('config_update_error', array(), 'tools');
+            $msg = $this->get('translator')->trans('config_update_error', [], 'tools');
             $this->get('session')->getFlashBag()->set('error', $msg);
         }
 
-        return $this->redirectToRoute('claro_desktop_open_tool', array('toolName' => 'home'));
+        return $this->redirectToRoute('audio_recorder_config_form');
     }
 }
--- plugin/video-recorder/Controller/VideoRecorderController.php
+++ plugin/video-recorder/Controller/VideoRecorderController.php
@@ -2,14 +2,15 @@
 
 namespace Innova\VideoRecorderBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Request;
-use Innova\VideoRecorderBundle\Manager\VideoRecorderManager;
 use Innova\VideoRecorderBundle\Entity\VideoRecorderConfiguration;
+use Innova\VideoRecorderBundle\Form\Type\VideoRecorderConfigurationType;
+use Innova\VideoRecorderBundle\Manager\VideoRecorderManager;
 use JMS\DiExtraBundle\Annotation as DI;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class VideoRecorderController extends Controller
 {
@@ -26,6 +27,24 @@ class VideoRecorderController extends Controller
     }
 
     /**
+     * @Route(
+     *     "/configure/form",
+     *     name="video_recorder_config_form"
+     * )
+     * @ParamConverter("authenticatedUser", options={"authenticatedUser" = true})
+     */
+    public function pluginConfigureFormAction()
+    {
+        $config = $this->manager->getConfig();
+        $form = $this->container->get('form.factory')->create(new VideoRecorderConfigurationType(), $config);
+
+        return $this->render(
+            'InnovaVideoRecorderBundle:VideoRecorder:options.form.html.twig',
+            ['form' => $form->createView(), 'id' => $config->getId()]
+        );
+    }
+
+    /**
      * @Route("/update/configuration/{id}", name="video_recorder_config_save")
      * @ParamConverter("config", class="InnovaVideoRecorderBundle:VideoRecorderConfiguration")
      * @Method("POST")
@@ -36,14 +55,14 @@ class VideoRecorderController extends Controller
             $postData = $request->request->get('video_recorder_configuration');
             if (isset($postData['max_recording_time'])) {
                 $this->manager->updateConfiguration($config, $postData);
-                $msg = $this->get('translator')->trans('config_update_success', array(), 'tools');
+                $msg = $this->get('translator')->trans('config_update_success', [], 'tools');
                 $this->get('session')->getFlashBag()->set('success', $msg);
             } else {
-                $msg = $this->get('translator')->trans('config_update_error', array(), 'tools');
+                $msg = $this->get('translator')->trans('config_update_error', [], 'tools');
                 $this->get('session')->getFlashBag()->set('error', $msg);
             }
 
-            return $this->redirectToRoute('claro_desktop_open_tool', array('toolName' => 'home'));
+            return $this->redirectToRoute('video_recorder_config_form');
         }
     }
 }
