afa278af0fd0c57b1117b2b1bdd032f0adfeb5ab
Gruber Laurent <laurent@sk1.be>
2016-08-04 11:36:40 +0200

    Update DocumentController.php
    fix #886

--- plugin/collecticiel/Controller/DocumentController.php
+++ plugin/collecticiel/Controller/DocumentController.php
@@ -21,6 +21,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Cocur\Slugify\Slugify;
 
 class DocumentController extends DropzoneBaseController
 {
