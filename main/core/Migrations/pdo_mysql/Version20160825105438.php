11264f28c97ac113c1f02bfad0cf8c478bdff7e4
ngodfraind <nicolas.godfraind@gmail.com>
2016-08-25 14:29:04 +0200

    phpcs

--- main/core/Migrations/pdo_mysql/Version20160825105438.php
+++ main/core/Migrations/pdo_mysql/Version20160825105438.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration based on mapping information: modify it with caution
+ * Auto-generated migration based on mapping information: modify it with caution.
  *
  * Generation date: 2016/08/25 10:54:39
  */
@@ -14,17 +14,17 @@ class Version20160825105438 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
-            ALTER TABLE claro_user 
+        $this->addSql('
+            ALTER TABLE claro_user
             ADD is_removed TINYINT(1) NOT NULL
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
-            ALTER TABLE claro_user 
+        $this->addSql('
+            ALTER TABLE claro_user
             DROP is_removed
-        ");
+        ');
     }
 }
