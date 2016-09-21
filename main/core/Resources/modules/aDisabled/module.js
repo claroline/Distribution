c8b713cb0dabbed45f3ca1b9525040151acc5a5f
pitrackster <pitrackster@gmail.com>
2016-09-21 17:58:16 +0200

    core directive allowing to disable a link. closes #1199

---
+++ main/core/Resources/modules/aDisabled/module.js
@@ -0,0 +1,32 @@
+/* disable link attr directive */
+
+import angular from 'angular/index'
+
+angular
+  .module('LinkDisabled', [])
+  .directive('aDisabled', function() {
+    return {
+      compile: function(tElement, tAttrs) {
+        //Disable ngClick
+        tAttrs['ngClick'] = '!('+tAttrs['aDisabled']+') && ('+tAttrs['ngClick']+')'
+
+        //return a link function
+        return function (scope, iElement, iAttrs) {
+
+          //Toggle "disabled" to class when aDisabled becomes true
+          scope.$watch(iAttrs['aDisabled'], function(newValue) {
+            if (newValue !== undefined) {
+              iElement.toggleClass('disabled', newValue)
+            }
+          })
+
+          //Disable href on click
+          iElement.on('click', function(e) {
+            if (scope.$eval(iAttrs['aDisabled'])) {
+              e.preventDefault()
+            }
+          })
+        }
+      }
+    }
+  })
