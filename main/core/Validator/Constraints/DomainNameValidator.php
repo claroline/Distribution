dea811795c99df8431a02dcfc0cd190658ce129d
ngodfraind <nicolas.godfraind@gmail.com>
2016-09-27 16:50:56 +0200

    [CoreBundle] Domain Name Validator

---
+++ main/core/Validator/Constraints/DomainNameValidator.php
@@ -0,0 +1,71 @@
+<?php
+
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Claroline\CoreBundle\Validator\Constraints;
+
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\ConstraintValidator;
+
+/**
+ * @DI\Validator("domain_name_validator")
+ */
+class DomainNameValidator extends ConstraintValidator
+{
+    public function validate($value, Constraint $constraint)
+    {
+        //null is perfectly valid: the system will find it itself.
+        if ($value === '' || $value === null) {
+            return;
+        }
+
+        $protocols = ['http', 'https'];
+        $urls = array_map(
+            function ($protocol) use ($value) {
+                return $protocol.'://'.$value;
+            },
+            $protocols
+        );
+
+        $found = false;
+
+        foreach ($urls as $url) {
+            if ($this->urlExists($value)) {
+                $found = true;
+            }
+        }
+
+        if (!$found) {
+            $this->context->addViolation($constraint->message);
+        }
+    }
+
+    public function urlExists($url)
+    {
+        $found = false;
+        $handle = curl_init($url);
+        curl_setopt($handle,  CURLOPT_RETURNTRANSFER, true);
+
+        /* Get the HTML or whatever is linked in $url. */
+        $response = curl_exec($handle);
+
+        /* Check for 404 (file not found). */
+        $httpCode = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+
+        if ($httpCode === 200) {
+            $found = true;
+        }
+
+        curl_close($handle);
+
+        return $found;
+    }
+}
