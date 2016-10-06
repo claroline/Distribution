1df76dc1bb099cb68cafb69b23201a36d4ffc38d
ngodfraind <nicolas.godfraind@gmail.com>
2016-10-06 10:19:31 +0200

    phpcs

--- main/core/Migrations/pdo_mysql/Version20161005170840.php
+++ main/core/Migrations/pdo_mysql/Version20161005170840.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration based on mapping information: modify it with caution
+ * Auto-generated migration based on mapping information: modify it with caution.
  *
  * Generation date: 2016/10/05 05:08:40
  */
@@ -14,45 +14,45 @@ class Version20161005170840 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             CREATE INDEX name_idx ON claro_workspace (name)
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             CREATE INDEX name_idx ON claro__event (name)
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             CREATE INDEX action_idx ON claro_log (action)
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             CREATE INDEX tool_idx ON claro_log (tool_name)
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             CREATE INDEX doer_type_idx ON claro_log (doer_type)
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             CREATE INDEX name_idx ON claro_workspace_tag (name)
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             DROP INDEX name_idx ON claro__event
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             DROP INDEX action_idx ON claro_log
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             DROP INDEX tool_idx ON claro_log
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             DROP INDEX doer_type_idx ON claro_log
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             DROP INDEX name_idx ON claro_workspace
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             DROP INDEX name_idx ON claro_workspace_tag
-        ");
+        ');
     }
 }
