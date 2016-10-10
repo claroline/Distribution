e98ddd657990222258b21600b96f9273914b12b7
kitan1982 <anhthao.pham@claroline.net>
2016-10-10 10:12:52 +0200

    php-cs-fixer

--- plugin/result/Tests/Listener/ResultListenerTest.php
+++ plugin/result/Tests/Listener/ResultListenerTest.php
@@ -11,23 +11,19 @@
 
 namespace Claroline\ResultBundle\Listener;
 
-use Claroline\CoreBundle\Entity\Widget\WidgetInstance;
 use Claroline\CoreBundle\Event\CreateFormResourceEvent;
 use Claroline\CoreBundle\Event\CreateResourceEvent;
 use Claroline\CoreBundle\Event\DeleteResourceEvent;
-use Claroline\CoreBundle\Event\DisplayWidgetEvent;
 use Claroline\CoreBundle\Event\OpenResourceEvent;
 use Claroline\CoreBundle\Library\Testing\TransactionalTestCase;
 use Claroline\CoreBundle\Persistence\ObjectManager;
 use Claroline\ResultBundle\Entity\Result;
 use Claroline\ResultBundle\Testing\Persister;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorage;
-use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class ResultListenerTest extends TransactionalTestCase
 {
-    /** @var  ResultListener */
+    /** @var ResultListener */
     private $listener;
     /** @var ObjectManager */
     private $om;
