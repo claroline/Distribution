8761639a97df40a9d2440c9feb4bd91c55492a54
ngodfraind <nicolas.godfraind@gmail.com>
2016-10-13 17:19:21 +0200

    update

--- main/core/Resources/modules/modernizr/modernizr.js
+++ main/core/Resources/modules/modernizr/modernizr.js
@@ -1,19 +1,1 @@
 import modernizr from 'modernizr'
-import $ from 'jquery'
-
-/* global Translator */
-
-if (!modernizr.mutationobserver) {
-  showWarning()
-}
-
-function showWarning () {
-  const warning = `
-        <div class="alert alert-danger" style="text-align='center';">
-            <a class="close" data-dismiss="alert" href="#" aria-hidden="true">&times;</a>
-            <div align="center">${Translator.trans('outdated_brower_message', {}, 'platform')} </div>
-        </div>
-    `
-
-  $('#claroline-base-layout-body').prepend(warning)
-}
