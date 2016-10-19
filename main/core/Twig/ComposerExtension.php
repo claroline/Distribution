b1843ba7ec8daf2c1e2716d731df6556e397e718
ngodfraind <nicolas.godfraind@gmail.com>
2016-10-19 11:05:45 +0200

    done

--- main/core/Twig/ComposerExtension.php
+++ main/core/Twig/ComposerExtension.php
@@ -52,45 +52,9 @@ class ComposerExtension extends \Twig_Extension
 
     public function getCurrentDistributionCommit()
     {
-        $previous = $this->openRepository($this->installedRepoFile);
+        $repo = new InstalledFilesystemRepository(new JsonFile($this->installedRepoFile));
+        $distrib = $repo->findPackage('claroline/distribution', '*');
 
-        foreach ($previous->getCanonicalPackages() as $package) {
-            if ($package->getName() === 'claroline/distribution') {
-                return $package->getSourceReference();
-            }
-        }
-    }
-
-    /**
-     * @param string $repoFile
-     * @param bool   $filter
-     *
-     * Currently copy pasted from OperationExecutor
-     *
-     * @return InstalledFilesystemRepository
-     */
-    private function openRepository($repoFile, $filter = true)
-    {
-        $json = new JsonFile($repoFile);
-
-        if (!$json->exists()) {
-            throw new \RuntimeException(
-                "Repository file '{$repoFile}' doesn't exist",
-                123 // this code is there for unit testing only
-            );
-        }
-
-        $repo = new InstalledFilesystemRepository($json);
-
-        if ($filter) {
-            foreach ($repo->getPackages() as $package) {
-                if ($package->getType() !== 'claroline-core'
-                    && $package->getType() !== 'claroline-plugin') {
-                    $repo->removePackage($package);
-                }
-            }
-        }
-
-        return $repo;
+        return $distrib->getSourceReference();
     }
 }
