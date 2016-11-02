8fcf7ee1db4342353544d1428db990ce10b5c37c
Rémy Taymans <remytaymans@gmail.com>
2016-11-02 14:46:30 +0100

    [FlashCardBundle] Run php-cs

--- plugin/flashcard/Migrations/pdo_mysql/Version20161102141717.php
+++ plugin/flashcard/Migrations/pdo_mysql/Version20161102141717.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration based on mapping information: modify it with caution
+ * Auto-generated migration based on mapping information: modify it with caution.
  *
  * Generation date: 2016/11/02 02:17:21
  */
@@ -14,25 +14,25 @@ class Version20161102141717 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_fcbundle_user_preference 
             DROP theme
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             ALTER TABLE claro_fcbundle_deck 
             DROP theme
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_fcbundle_deck 
             ADD theme VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci
-        ");
-        $this->addSql("
+        ');
+        $this->addSql('
             ALTER TABLE claro_fcbundle_user_preference 
             ADD theme VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci
-        ");
+        ');
     }
 }
