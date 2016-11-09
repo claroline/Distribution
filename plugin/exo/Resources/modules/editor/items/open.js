20a7e425fa9e5d2da9ce89ffb4970ba6aafa04ad
pitrackster <pitrackster@gmail.com>
2016-11-09 12:16:14 +0100

    [ExoBundle] Editor v2 open question

--- plugin/exo/Resources/modules/editor/items/open.js
+++ plugin/exo/Resources/modules/editor/items/open.js
@@ -1,32 +1,75 @@
 import {Open as component} from './open.jsx'
 import {ITEM_CREATE} from './../actions'
-import {update} from './../util'
 import {notBlank, number, gteZero} from './../lib/validate'
+import {makeActionCreator} from './../util'
+import cloneDeep from 'lodash/cloneDeep'
+import merge from 'lodash/merge'
+import set from 'lodash/set'
+
+const UPDATE_OPEN = 'UPDATE_OPEN'
+
+export const actions = {
+  updateOpen: makeActionCreator(UPDATE_OPEN, 'id', 'property', 'value')
+}
 
 function reduce(item = {}, action) {
   switch (action.type) {
     case ITEM_CREATE: {
-      return update(open, {
-        maxScore: {$set: 0},
-        maxLength: {$set: 0}
+      return Object.assign({}, item, {
+        maxScore: 0,
+        maxLength: 0
       })
+    }
+
+    case UPDATE_OPEN: {
+      const newItem = cloneDeep(item)
+      newItem._touched = merge(
+        newItem._touched || {},
+        set({}, action.property, true)
+      )
+      const newValue = parseFloat(action.value)
+      if(action.property === 'maxScore'){
+        newItem.maxScore = newValue
+      } else if(action.property === 'maxLength'){
+        newItem.maxLength = newValue
+      }
+      return newItem
     }
   }
   return item
 }
 
-// function initialFormValues(item) {
-//   return update(item, {
-//     maxScore: {$set: item.score.max}
-//   })
-// }
 
 function validate(values) {
-  return {
-    maxScore: notBlank(values.maxScore)
-      || number(values.maxScore)
-      || gteZero(values.maxScore)
+  let errors = {}
+  if(undefined !== notBlank(values.maxScore)){
+    errors =  {
+      maxScore: notBlank(values.maxScore)
+    }
+  } else if(undefined !== number(values.maxScore)){
+    errors = {
+      maxScore : number(values.maxScore)
+    }
+  } else if(undefined !== gteZero(values.maxScore)){
+    errors = {
+      maxScore : gteZero(values.maxScore)
+    }
   }
+
+  if(undefined !== notBlank(values.maxLength)){
+    errors =  {
+      maxLength: notBlank(values.maxLength)
+    }
+  } else if(undefined !== number(values.maxLength)){
+    errors = {
+      maxLength : number(values.maxLength)
+    }
+  } else if(undefined !== gteZero(values.maxLength)){
+    errors = {
+      maxLength : gteZero(values.maxLength)
+    }
+  }
+  return errors
 }
 
 export default {
