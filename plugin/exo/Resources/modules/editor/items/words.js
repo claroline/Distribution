d20711c02fc979495452675c6ef8adf03df3d2f4
pitrackster <pitrackster@gmail.com>
2016-11-14 14:40:50 +0100

    words question type

--- plugin/exo/Resources/modules/editor/items/words.js
+++ plugin/exo/Resources/modules/editor/items/words.js
@@ -1,91 +1,97 @@
 import {Words as component} from './words.jsx'
 import {ITEM_CREATE} from './../actions'
-import {notBlank, number, gteZero} from './../lib/validate'
+import {notBlank, number, chain} from './../lib/validate'
 import {makeActionCreator} from './../util'
 import cloneDeep from 'lodash/cloneDeep'
 import merge from 'lodash/merge'
 import set from 'lodash/set'
+import {tex} from './../lib/translate'
 
 const UPDATE_SOLUTION = 'UPDATE_SOLUTION'
 const ADD_SOLUTION = 'ADD_SOLUTION'
 const REMOVE_SOLUTION = 'REMOVE_SOLUTION'
+const UPDATE_PROP = 'UPDATE_PROP'
 
 export const actions = {
   updateSolution: makeActionCreator(UPDATE_SOLUTION, 'index', 'property', 'value'),
   addSolution: makeActionCreator(ADD_SOLUTION),
-  removeSolution: makeActionCreator(REMOVE_SOLUTION, 'index')
+  removeSolution: makeActionCreator(REMOVE_SOLUTION, 'index'),
+  updateProperty: makeActionCreator(UPDATE_PROP, 'property', 'value')
 }
 
+function decorate(item) {
+
+  const solutionsWithDeletable = item.solutions.map(
+    solution => Object.assign({}, solution, {
+      _deletable: item.solutions.length > 1
+    })
+  )
+
+  let decorated = Object.assign({}, item, {
+    _wordsCaseSensitive: false,
+    solutions: solutionsWithDeletable
+  })
+
+  return decorated
+}
 
 function reduce(item = {}, action) {
   switch (action.type) {
     case ITEM_CREATE: {
-      return Object.assign({}, item, {
-        solutions: {
-          text:'',
-          caseSensitive: false,
-          score: 1,
-          feedback: ''
-        }
-      })
+      return decorate(Object.assign({}, item, {
+        solutions: [
+          {
+            text:'',
+            caseSensitive: false,
+            score: 1,
+            feedback: ''
+          }
+        ]
+      }))
     }
 
     case UPDATE_SOLUTION: {
       const newItem = cloneDeep(item)
 
       // mark as touched
-      const solution = newItem.solutions[action.index]
-
-      //const choiceIndex = newItem.choices.findIndex(choice => choice.id === action.id)
       const value = action.property === 'score' ? parseFloat(action.value) : action.value
-      const decoratedName = action.property === 'data' ? 'data' : `_${action.property}`
+      newItem.solutions[action.index]._touched = merge(
+        newItem.solutions[action.index]._touched || {},
+        set({}, action.property, true)
+      )
+      newItem.solutions[action.index][action.property] = value
 
-      if (decoratedName === '_checked' && !item.multiple) {
-        newItem.choices.forEach(choice => choice._checked = false)
-      }
-
-      newItem.choices[choiceIndex][decoratedName] = value
-
-      if (newItem.score.type === SCORE_FIXED) {
-        setScores(newItem, choice => choice._checked ? 1 : 0)
-      }
-
-      if (action.property === 'score' || action.property === 'feedback') {
-        const solutionIndex = newItem.solutions.findIndex(
-          solution => solution.id === action.id
-        )
-        newItem.solutions[solutionIndex][action.property] = value
-      }
-
-      return setChoiceTicks(newItem)
-    }
-    case ADD_SOLUTION: {
-      const newItem = cloneDeep(item)
-      const choiceId = makeId()
-      newItem.choices.push({
-        id: choiceId,
-        data: '',
-        _feedback: '',
-        _score: 0,
-        _checked: false,
-        _deletable: true
-      })
-      newItem.solutions.push({
-        id: choiceId,
-        feedback: '',
-        score: 0
-      })
-      const deletable = newItem.choices.length > 2
-      newItem.choices.forEach(choice => choice._deletable = deletable)
       return newItem
     }
+
+    case ADD_SOLUTION: {
+      const newItem = cloneDeep(item)
+      newItem.solutions.push({
+        text:'',
+        caseSensitive: false,
+        score: 1,
+        feedback: ''
+      })
+      const deletable = newItem.solutions.length > 1
+      newItem.solutions.forEach(solution => solution._deletable = deletable)
+      return newItem
+    }
+
     case REMOVE_SOLUTION: {
       const newItem = cloneDeep(item)
-      const choiceIndex = newItem.choices.findIndex(choice => choice.id === action.id)
-      const solutionIndex = newItem.solutions.findIndex(solution => solution.id === action.id)
-      newItem.choices.splice(choiceIndex, 1)
-      newItem.solutions.splice(solutionIndex, 1)
-      newItem.choices.forEach(choice => choice._deletable = newItem.choices.length > 2)
+      newItem.solutions.splice(action.index, 1)
+      newItem.solutions.forEach(solution => solution._deletable = newItem.solutions.length > 1)
+      return newItem
+    }
+
+    case UPDATE_PROP: {
+      // for now it is only used for casSensitive activation / deactivation
+      const newItem = cloneDeep(item)
+      newItem[action.property] = Boolean(action.value)
+      if (false === Boolean(action.value)){
+        newItem.solutions.forEach(solution => solution.caseSensitive = false)
+      }
+
       return newItem
     }
   }
@@ -93,15 +99,35 @@ function reduce(item = {}, action) {
 }
 
 
-function validate(values) {
-  let errors = {}
+function validate(item) {
+  const errors = {}
 
+  if (item.solutions.find(solution => notBlank(solution.text, true))) {
+    errors.solutions = tex('words_empty_text_error')
+  }
+
+  if (undefined === item.solutions.find(solution => solution.score > 0)) {
+    errors.solutions = tex('words_no_valid_solution')
+  }
+
+  /*const errors = {}
+  setIfError(errors, 'solutions', )
+
+  return errors*/
+
+  if (undefined !== item.solutions.find(solution => chain(solution.score, [notBlank, number]))) {
+    errors.solutions = tex('words_score_not_valid')
+  }
+
+  /*if (undefined !== item.solutions.find(solution => undefined !== number(solution.score) || undefined !== notBlank(solution.score))) {
+    errors.solutions = tex('words_score_not_valid')
+  }*/
   return errors
 }
 
 export default {
   type: 'application/x.words+json',
-  name: 'open',
+  name: 'words',
   component,
   reduce,
   validate
