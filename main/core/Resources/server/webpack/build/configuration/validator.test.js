29355afe3a87ac49455941297eb21698cf5ee14f
ngodfraind <nicolas.godfraind@gmail.com>
2016-11-14 10:31:35 +0100

    Tests

--- main/core/Resources/server/webpack/build/configuration/validator.test.js
+++ main/core/Resources/server/webpack/build/configuration/validator.test.js
@@ -3,11 +3,13 @@ const assert = require('assert')
 const actionFail = require('./test-stubs/actions-fail')
 const actionPass = require('./test-stubs/actions-pass')
 
-it('Test the action validation process', () => {
-  function executeError() {
-    validator.validate(actionFail)
-  }
+describe('Configuration', () => {
+  it('Test the action validation process', () => {
+    function executeError() {
+      validator.validate(actionFail)
+    }
 
-  assert.throws(executeError, Error, 'Error thrown')
-  assert.equal(validator.validate(actionPass), true)
+    assert.throws(executeError, Error, 'Error thrown')
+    assert.equal(validator.validate(actionPass), true)
+  })
 })
