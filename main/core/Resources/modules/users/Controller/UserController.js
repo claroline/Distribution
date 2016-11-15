90111ba6ac3b4247fcbc11108d8a5fb04d124317
ngodfraind <nicolas.godfraind@gmail.com>
2016-11-15 17:34:17 +0100

    renaming folder

--- main/core/Resources/modules/users/Controller/UserController.js
+++ main/core/Resources/modules/users/Controller/UserController.js
@@ -4,7 +4,7 @@ import UserInfoHtml from '../Partial/user_info.html'
 import angular from 'angular/index'
 import removeTpl from '../Partial/csv_remove.html'
 import importTpl from '../Partial/csv_facet.html'
-import Configuration from '#/main/core/librairy/Configuration/Configuration'
+import Configuration from '#/main/core/library/Configuration/Configuration'
 
 /* global Routing */
 /* global Translator */
