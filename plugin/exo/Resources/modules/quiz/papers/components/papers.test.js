6e56080912310664c127cd1ad72497eb4e4c4383
Stéphane Klein <stephaneklein221@gmail.com>
2017-01-03 10:43:02 +0100

    Fix and and tests

--- plugin/exo/Resources/modules/quiz/papers/components/papers.test.js
+++ plugin/exo/Resources/modules/quiz/papers/components/papers.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
-import {shallow} from 'enzyme'
+import {mount} from 'enzyme'
+import configureMockStore from 'redux-mock-store'
 import {spyConsole, renew, ensure, mockTranslator} from './../../../utils/test'
 import {Papers} from './papers.jsx'
 
@@ -12,7 +13,40 @@ describe('<Papers/>', () => {
   afterEach(spyConsole.restore)
 
   it('renders a list of papers', () => {
-    shallow(<Papers/>)
+    const store = configureMockStore()({
+      quiz: {
+        meta: {
+          editable: true
+        }
+      },
+      papers: {
+        papers: [
+          {
+            id: '123',
+            number: 1,
+            user: {
+              name: 'John Doe'
+            },
+            startDate: '1986/02/12',
+            finished: true
+          },
+          {
+            id: '456',
+            number: 2,
+            user: {
+              name: 'Jane Doe'
+            },
+            startDate: '2015/11/03',
+            finished: false
+          }
+        ]
+      }
+    })
+
+    const papers = mount(<Papers store={store}/>)
+
     ensure.propTypesOk()
+    ensure.equal(papers.find('table').length, 1)
+    ensure.equal(papers.find('tr').length, 3) // 2 papers + 1 header line
   })
 })
