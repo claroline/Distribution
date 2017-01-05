7e6a4a71d3aa10e6d581a85925cface5130fb914
pitrackster <pitrackster@gmail.com>
2017-01-05 15:17:08 +0100

    less file for editor and overview

--- plugin/exo/Resources/modules/quiz/player/components/player.jsx
+++ plugin/exo/Resources/modules/quiz/player/components/player.jsx
@@ -12,12 +12,15 @@ import {PlayerNav} from './nav-bar.jsx'
 export const Player = props =>
   <div className="quiz-player">
     <h2 className="h4 step-title">
-      {tex('step')}&nbsp;{props.number}
-      {props.step.title && <small>&nbsp;{props.step.title}</small>}
+      {props.step.title ?
+        <small>&nbsp;{props.step.title}</small>
+        :
+        <span>{tex('step')}&nbsp; {props.number}</span>
+      }
     </h2>
 
     {props.step.description &&
-      <div className="exercise-description panel panel-default">
+      <div className="step-description panel panel-default">
         <div
           className="panel-body"
           dangerouslySetInnerHTML={{ __html: props.step.description }}
