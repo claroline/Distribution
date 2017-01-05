187424d3cb84fa90d40b7b75c4a605103ef3e240
ngodfraind <nicolas.godfraind@gmail.com>
2017-01-05 11:57:21 +0100

    Tabs icon

--- plugin/exo/Resources/modules/items/choice/paper.jsx
+++ plugin/exo/Resources/modules/items/choice/paper.jsx
@@ -3,7 +3,10 @@ import classes from 'classnames'
 import {tex} from '../../utils/translate'
 import {tcex} from '../../utils/translate'
 import Tab from 'react-bootstrap/lib/Tab'
-import Tabs from 'react-bootstrap/lib/Tabs'
+import Row from 'react-bootstrap/lib/Row'
+import Col from 'react-bootstrap/lib/Col'
+import Nav from 'react-bootstrap/lib/Nav'
+import NavItem from 'react-bootstrap/lib/NavItem'
 import Popover from 'react-bootstrap/lib/Popover'
 import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger'
 
@@ -16,6 +19,20 @@ SolutionScore.propTypes = {
   solution: T.shape({
     score: T.number
   })
+}
+
+const ChoiceMetadata = props => {
+  return(
+    <div className="panel panel-body">
+      <h4>{props.title}</h4>
+      <i>{props.description}</i>
+    </div>
+  )
+}
+
+ChoiceMetadata.propTypes = {
+  title: T.string,
+  description: T.string
 }
 
 const Feedback = props => {
@@ -92,80 +109,92 @@ export class ChoicePaper extends Component
 
   render() {
     return(
-      <Tabs
-        id={`${this.props.item.id}-paper`}
-        activeKey={this.state.key}
-        onSelect={this.handleSelect}
-      >
-        <Tab eventKey={1} title={tex('your_answer')}>
-          <div className="container choice-paper">
-            <h4>{this.props.item.title}</h4>
-            <div><i>{this.props.item.description}</i></div>
-            {this.props.item.solutions.map(solution =>
-              <div
-                key={this.answerId(solution.id)}
-                className={classes(
-                  'item',
-                  this.props.item.multiple ? 'checkbox': 'radio',
-                  this.getAnswerClassForSolution(solution, this.props.answer.data)
-                )}>
-                <WarningIcon solution={solution} answers={this.props.answer.data}/>
-                <input
-                  className={this.props.item.multiple ? 'checkbox': 'radio'}
-                  checked={this.isSolutionChecked(solution, this.props.answer.data)}
-                  id={this.answerId(solution.id)}
-                  name={this.answerId(this.props.item.id)}
-                  type={this.props.item.multiple ? 'checkbox': 'radio'}
-                  disabled
-                />
-                <label
-                  className="control-label"
-                  htmlFor={this.answerId(solution.id)}
-                  dangerouslySetInnerHTML={{__html: this.getChoiceById(solution.id).data}}
-                />
-              <Feedback
-                id={`${solution.id}-feedback`}
-                feedback={solution.feedback}
-              />
-                <SolutionScore solution={solution}/>
-              </div>
-            )}
-          </div>
-        </Tab>
-        <Tab eventKey={2} title={tex('expected_answer')}>
-          <div className="container choice-paper">
-            {this.props.item.solutions.map(solution =>
-              <div
-                key={this.expectedId(solution.id)}
-                className={classes(
-                  'item',
-                  this.props.item.multiple ? 'checkbox': 'radio'
-                )}
-              >
-                <span className="answer-warning-span"></span>
-                <input
-                  className={this.props.item.multiple ? 'checkbox': 'radio'}
-                  checked={solution.score > 0}
-                  id={this.expectedId(solution.id)}
-                  name={this.expectedId(this.props.item.id)}
-                  type={this.props.item.multiple ? 'checkbox': 'radio'}
-                  disabled
-                />
-                <label
-                  className="control-label"
-                  htmlFor={this.expectedId(solution.id)}
-                  dangerouslySetInnerHTML={{__html: this.getChoiceById(solution.id).data}}
-                />
-                <Feedback
-                  id={`${solution.id}-feedback-expected`}
-                  feedback={solution.feedback}
-                />
-                <SolutionScore solution={solution}/>
-              </div>
-            )}
-          </div>
-        </Tab>
-      </Tabs>
+      <Tab.Container id={`${this.props.item.id}-paper`} defaultActiveKey="first">
+        <Row className="clearfix">
+          <Col sm={12}>
+            <Nav bsStyle="tabs">
+              <NavItem eventKey="first">
+                  <span className="fa fa-user"></span> {tex('your_answer')}
+              </NavItem>
+              <NavItem eventKey="second">
+                <span className="fa fa-check"></span> {tex('expected_answer')}
+              </NavItem>
+            </Nav>
+          </Col>
+          <Col sm={12}>
+            <Tab.Content animation>
+              <Tab.Pane eventKey="first">
+                <ChoiceMetadata title={this.props.item.title} description={this.props.item.description}/>
+                <div className="container choice-paper">
+                  {this.props.item.solutions.map(solution =>
+                    <div
+                      key={this.answerId(solution.id)}
+                      className={classes(
+                        'item',
+                        this.props.item.multiple ? 'checkbox': 'radio',
+                        this.getAnswerClassForSolution(solution, this.props.answer.data)
+                      )}>
+                      <WarningIcon solution={solution} answers={this.props.answer.data}/>
+                      <input
+                        className={this.props.item.multiple ? 'checkbox': 'radio'}
+                        checked={this.isSolutionChecked(solution, this.props.answer.data)}
+                        id={this.answerId(solution.id)}
+                        name={this.answerId(this.props.item.id)}
+                        type={this.props.item.multiple ? 'checkbox': 'radio'}
+                        disabled
+                      />
+                      <label
+                        className="control-label"
+                        htmlFor={this.answerId(solution.id)}
+                        dangerouslySetInnerHTML={{__html: this.getChoiceById(solution.id).data}}
+                      />
+                    <Feedback
+                      id={`${solution.id}-feedback`}
+                      feedback={solution.feedback}
+                    />
+                      <SolutionScore solution={solution}/>
+                    </div>
+                  )}
+                </div>
+              </Tab.Pane>
+              <Tab.Pane eventKey="second">
+                <ChoiceMetadata title={this.props.item.title} description={this.props.item.description}/>
+                <div className="container choice-paper">
+                  {this.props.item.solutions.map(solution =>
+                    <div
+                      key={this.expectedId(solution.id)}
+                      className={classes(
+                        'item',
+                        this.props.item.multiple ? 'checkbox': 'radio'
+                      )}
+                    >
+                      <span className="answer-warning-span"></span>
+                      <input
+                        className={this.props.item.multiple ? 'checkbox': 'radio'}
+                        checked={solution.score > 0}
+                        id={this.expectedId(solution.id)}
+                        name={this.expectedId(this.props.item.id)}
+                        type={this.props.item.multiple ? 'checkbox': 'radio'}
+                        disabled
+                      />
+                      <label
+                        className="control-label"
+                        htmlFor={this.expectedId(solution.id)}
+                        dangerouslySetInnerHTML={{__html: this.getChoiceById(solution.id).data}}
+                      />
+                      <Feedback
+                        id={`${solution.id}-feedback-expected`}
+                        feedback={solution.feedback}
+                      />
+                      <SolutionScore solution={solution}/>
+                    </div>
+                  )}
+                </div>
+              </Tab.Pane>
+            </Tab.Content>
+          </Col>
+        </Row>
+      </Tab.Container>
     )
   }
 }
