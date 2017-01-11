081c8c4bd955111e7c45266939e6de89ad51470a
ngodfraind <nicolas.godfraind@gmail.com>
2017-01-11 17:23:45 +0100

    Test

--- plugin/exo/Resources/modules/items/cloze/editor.js
+++ plugin/exo/Resources/modules/items/cloze/editor.js
@@ -1,10 +1,21 @@
 import {Cloze as component} from './editor.jsx'
+import {makeActionCreator} from './../../utils/utils'
 
-function reduce(cloze = {}) {
-  return cloze
+export const actions = {
+  updateProperty: makeActionCreator(UPDATE_TEXT, 'text')
 }
 
 export default {
   component,
   reduce
+}
+
+const UPDATE_TEXT = 'UPDATE_TEXT'
+
+function reduce(item = {}, action) {
+  switch (action.type) {
+    case UPDATE_TEXT: {
+      alert('test')
+    }
+  }
 }
--- plugin/exo/Resources/modules/items/cloze/editor.jsx
+++ plugin/exo/Resources/modules/items/cloze/editor.jsx
@@ -1,4 +1,17 @@
-import React from 'react'
+import React, {PropTypes as T} from 'react'
+import {Textarea} from './../../components/form/textarea.jsx'
 
-export const Cloze = () =>
-  <div>CLOZE</div>
+export const Cloze = (props) => {
+  return(
+    <div>
+      <div> Cloze text </div>
+      <Textarea
+        id={props.item.id}
+      />
+    </div>
+  )
+}
+
+Cloze.propTypes = {
+  item: T.object()
+}
