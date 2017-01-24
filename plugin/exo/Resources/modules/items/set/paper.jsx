ec099a14968e831a60958f17c9b1dd3ebab90e4f
pitrackster <pitrackster@gmail.com>
2017-01-24 10:17:03 +0100

    set question paper and feedback

--- plugin/exo/Resources/modules/items/set/paper.jsx
+++ plugin/exo/Resources/modules/items/set/paper.jsx
@@ -3,29 +3,8 @@ import classes from 'classnames'
 import {Feedback} from './../components/feedback-btn.jsx'
 import {SolutionScore} from './../components/score.jsx'
 import {PaperTabs} from './../components/paper-tabs.jsx'
-
-
-const OddList = props =>
-  <div className="odd-list">
-    <ul>
-      { props.odd.map((item) =>
-        <li key={item.id}>
-          <div className="item">
-            <div className="item-content" dangerouslySetInnerHTML={{__html: item.data}} />
-            <Feedback
-                id={`odd-${item.id}-feedback`}
-                feedback={item.feedback}
-            />
-            <SolutionScore score={item.score}/>
-          </div>
-        </li>
-      )}
-    </ul>
-  </div>
-
-OddList.propTypes = {
-  odd: T.arrayOf(T.object).isRequired
-}
+import {utils} from './utils/utils'
+import {WarningIcon} from './utils/warning-icon.jsx'
 
 export const SetPaper = props => {
   return (
@@ -38,31 +17,83 @@ export const SetPaper = props => {
 
           </div>
           <div className="sets-col">
-          </div>
-        </div>
-      }
-      expected={
-        <div className="set-paper">
-          <div className="items-col">
-            <OddList odd={props.item.solutions.odds || []} />
-          </div>
-          <div className="sets-col">
             <ul>
               {props.item.sets.map((set) =>
-                <li key={`set-id-${set.id}`}>
+                <li key={`your-answer-set-id-${set.id}`}>
                   <div className="set">
                     <div className="set-heading">
                       <div className="set-heading-content" dangerouslySetInnerHTML={{__html: set.data}} />
                     </div>
                     <div className="set-body">
                       <ul>
-                      { props.item.solutions.associations.filter(ass => ass.setId === set.id).map(ass =>
-                        <li key={`${ass.itemId}-${ass.setId}`}>
+                      { utils.getSetItems(set.id, props.answer).map(answer =>
+                        <li key={`your-answer-assocation-${answer.itemId}-${answer.setId}`}>
+                          { utils.answerInSolutions(answer, props.item.solutions.associations) ?
+                            <div className={classes(
+                                'association',
+                                {'bg-success text-success': utils.isValidAnswer(answer, props.item.solutions.associations)},
+                                {'bg-danger text-danger': !utils.isValidAnswer(answer, props.item.solutions.associations)}
+                              )}>
+                              <WarningIcon valid={utils.isValidAnswer(answer, props.item.solutions.associations)}/>
+                              <div className="association-data" dangerouslySetInnerHTML={{__html: utils.getSolutionItemData(answer.itemId, props.item.items)}} />
+                              <Feedback
+                                    id={`ass-${answer.itemId}-${answer.setId}-feedback`}
+                                    feedback={utils.getAnswerSolutionFeedback(answer, props.item.solutions.associations)}
+                                />
+                              <SolutionScore score={utils.getAnswerSolutionScore(answer, props.item.solutions.associations)}/>
+                            </div>
+                            :
+                            <div className="association bg-danger text-danger">
+                              <WarningIcon valid={false}/>
+                              <div className="association-data" dangerouslySetInnerHTML={{__html: utils.getSolutionItemData(answer.itemId, props.item.items)}} />
+                            </div>
+                          }
+                        </li>
+                      )}
+                      </ul>
+                    </div>
+                  </div>
+                </li>
+              )}
+            </ul>
+          </div>
+        </div>
+      }
+      expected={
+        <div className="set-paper">
+          <div className="items-col">
+            <ul>
+              { props.item.solutions.odd && props.item.solutions.odd.map((item) =>
+                <li key={`expected-${item.id}`}>
+                  <div className="item">
+                    <div className="item-content" dangerouslySetInnerHTML={{__html: utils.getSolutionItemData(item.itemId, props.item.items)}} />
+                    <Feedback
+                        id={`odd-${item.itemId}-feedback`}
+                        feedback={item.feedback}
+                    />
+                    <SolutionScore score={item.score}/>
+                  </div>
+                </li>
+              )}
+            </ul>
+          </div>
+          <div className="sets-col">
+            <ul>
+              {props.item.sets.map((set) =>
+                <li key={`expected-set-id-${set.id}`}>
+                  <div className="set">
+                    <div className="set-heading">
+                      <div className="set-heading-content" dangerouslySetInnerHTML={{__html: set.data}} />
+                    </div>
+                    <div className="set-body">
+                      <ul>
+                      { utils.getSetItems(set.id, props.item.solutions.associations).map(ass =>
+                        <li key={`expected-association-${ass.itemId}-${ass.setId}`}>
                           <div className={classes(
                               'association',
                               {'bg-info text-info': ass.score > 0}
                             )}>
-                            <div className="association-data" dangerouslySetInnerHTML={{__html: props.item.items.find(el => el.id === ass.itemId ).data}} />
+                            <div className="association-data" dangerouslySetInnerHTML={{__html: utils.getSolutionItemData(ass.itemId, props.item.items)}} />
                             <Feedback
                                   id={`ass-${ass.itemId}-${ass.setId}-feedback`}
                                   feedback={ass.feedback}
