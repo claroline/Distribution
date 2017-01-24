6a4eced6d445e463c95bd2f1fc77cd70de1316c8
Stéphane Klein <stephaneklein221@gmail.com>
2017-01-24 17:30:05 +0100

    Test expurged version of preview

--- main/dev/Resources/travis/send-preview.sh
+++ main/dev/Resources/travis/send-preview.sh
@@ -15,6 +15,10 @@ PREVIEW="pr-$TRAVIS_PULL_REQUEST-`date +%s`.tar.gz"
 
 mysqldump --opt --no-create-db claroline_test -uroot --password="" > claroline.sql
 rm -rf app/cache/* app/logs/* web/bundles
+
+# test
+rm -rf vendor node_modules web/packages web/dist
+
 tar --exclude=".git" -czf $PREVIEW *
 
 export SSHPASS=$REMOTE_PASS
