1da755f477ea8cf200f36fcf6a9a40c0b6cd266e
kitan1982 <anhthao.pham@claroline.net>
2017-01-24 13:32:45 +0100

    eslint

--- plugin/exo/Resources/modules/quiz/correction/reducer.js
+++ plugin/exo/Resources/modules/quiz/correction/reducer.js
@@ -1,6 +1,9 @@
 import {CORRECTION_INIT, QUESTION_CURRENT, SCORE_UPDATE, FEEDBACK_UPDATE, REMOVE_ANSWERS} from './actions'
 
 export const reduceCorrection = (state = {}, action = {}) => {
+  let answers = []
+  let question = {}
+
   switch (action.type) {
     case CORRECTION_INIT:
       return Object.assign({}, state, {
@@ -13,7 +16,7 @@ export const reduceCorrection = (state = {}, action = {}) => {
         currentQuestionId: action.id
       })
     case SCORE_UPDATE:
-      const scoreAnswers = state.answers.map((answer) => {
+      answers = state.answers.map((answer) => {
         if (answer.id === action.answerId) {
           return Object.assign({}, answer, {score: action.score})
         } else {
@@ -21,10 +24,10 @@ export const reduceCorrection = (state = {}, action = {}) => {
         }
       })
       return Object.assign({}, state, {
-        answers: scoreAnswers
+        answers: answers
       })
     case FEEDBACK_UPDATE:
-      const feedbackAnswers = state.answers.map((answer) => {
+      answers = state.answers.map((answer) => {
         if (answer.id === action.answerId) {
           return Object.assign({}, answer, {feedback: action.feedback})
         } else {
@@ -32,10 +35,10 @@ export const reduceCorrection = (state = {}, action = {}) => {
         }
       })
       return Object.assign({}, state, {
-        answers: feedbackAnswers
+        answers: answers
       })
     case REMOVE_ANSWERS:
-      const question = state.questions.find(q => q.id === action.questionId)
+      question = state.questions.find(q => q.id === action.questionId)
       return Object.assign({}, state, {
         answers: state.answers.filter(a =>
           a.questionId !== action.questionId || a.score > question.score.max || isNaN(a.score) || a.score === null || a.score === undefined
