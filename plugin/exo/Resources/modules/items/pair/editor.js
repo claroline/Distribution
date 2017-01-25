b238e29a17f090c9e8862e11b3355eabc0de3ba5
pitrackster <pitrackster@gmail.com>
2017-01-25 11:07:58 +0100

    pair first commit

--- plugin/exo/Resources/modules/items/pair/editor.js
+++ plugin/exo/Resources/modules/items/pair/editor.js
@@ -1,10 +1,128 @@
-import {Pair as component} from './editor.jsx'
+import cloneDeep from 'lodash/cloneDeep'
+import {ITEM_CREATE} from './../../quiz/editor/actions'
+import {makeId, makeActionCreator} from './../../utils/utils'
+import {notBlank, number, chain} from './../../utils/validate'
+import {tex} from './../../utils/translate'
+import {PairForm as component} from './editor.jsx'
 
-function reduce(pair = {}) {
+const UPDATE_PROP = 'UPDATE_PROP'
+const ADD_ITEM = 'ADD_ITEM'
+const REMOVE_ITEM = 'REMOVE_ITEM'
+const UPDATE_ITEM = 'UPDATE_ITEM'
+const ADD_SOLUTION = 'ADD_SOLUTION'
+const REMOVE_SOLUTION = 'REMOVE_SOLUTION'
+const UPDATE_SOLUTION = 'UPDATE_SOLUTION'
+
+export const actions = {
+  updateProperty: makeActionCreator(UPDATE_PROP, 'property', 'value'),
+  addItem:  makeActionCreator(ADD_ITEM, 'isRightItem', 'isOdd'),
+  removeItem:  makeActionCreator(REMOVE_ITEM, 'id', 'isRightItem', 'isOdd'),
+  updateItem:  makeActionCreator(UPDATE_ITEM, 'id', 'property', 'value', 'isOdd'),
+  addSolution: makeActionCreator(ADD_SOLUTION, 'leftId', 'rightId', 'data'),
+  removeSolution: makeActionCreator(REMOVE_SOLUTION, 'leftId', 'rightId'),
+  updateSolution: makeActionCreator(UPDATE_SOLUTION, 'leftId', 'rightId', 'property', 'value')
+}
+
+
+function decorate(pair) {
+
+  // consider items that are not in solutions.odd
+  const itemDeletable = question.items.filter(item => undefined === question.solutions.odd.find(el => el.itemId === item.id)).length > 1
+  const itemsWithDeletable = question.items.filter(item => undefined === question.solutions.odd.find(el => el.itemId === item.id)).map(
+    item => Object.assign({}, item, {
+      _deletable: itemDeletable
+    })
+  )
+
+  const setsWithDeletable = question.sets.map(
+    item => Object.assign({}, item, {
+      _deletable: question.sets.length > 1
+    })
+  )
+  let decorated = Object.assign({}, question, {
+    items: itemsWithDeletable,
+    sets: setsWithDeletable,
+    solutions: Object.assign({}, question.solutions, {
+      associations: associationsWithItemData
+    })
+  })
+
+  return decorated
+}
+
+function reduce(pair = {}, action) {
+  switch (action.type) {
+
+    case ITEM_CREATE: {
+      return decorate(Object.assign({}, pair, {
+        random: false,
+        penalty: 0,
+        left: [
+          {
+            id: makeId(),
+            type: 'text/html',
+            data: ''
+          }
+        ],
+        right: [
+          {
+            id: makeId(),
+            type: 'text/html',
+            data: ''
+          }
+        ],
+        solutions: []
+      }))
+    }
+
+    case UPDATE_PROP: {
+      const newItem = cloneDeep(pair)
+      return newItem
+    }
+
+    case ADD_ITEM: {
+      const newItem = cloneDeep(pair)
+      return newItem
+    }
+
+    case UPDATE_ITEM: {
+      const newItem = cloneDeep(pair)
+      return newItem
+    }
+
+    case REMOVE_ITEM: {
+      const newItem = cloneDeep(pair)
+      return newItem
+    }
+
+    case ADD_SOLUTION: {
+      const newItem = cloneDeep(pair)
+      return newItem
+    }
+
+    case REMOVE_SOLUTION: {
+      const newItem = cloneDeep(pair)
+      return newItem
+    }
+
+    case UPDATE_SOLUTION: {
+      const newItem = cloneDeep(pair)
+      return newItem
+    }
+
+  }
   return pair
+}
+
+function validate(pair) {
+  const errors = {}
+
+  return errors
 }
 
 export default {
   component,
-  reduce
+  reduce,
+  decorate,
+  validate
 }
--- plugin/exo/Resources/modules/items/pair/editor.jsx
+++ plugin/exo/Resources/modules/items/pair/editor.jsx
@@ -1,4 +1,101 @@
-import React from 'react'
+import React, {Component, PropTypes as T} from 'react'
+import get from 'lodash/get'
+import classes from 'classnames'
+import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger'
+import Tooltip from 'react-bootstrap/lib/Tooltip'
+import {tex, t} from './../../utils/translate'
+import {Textarea} from './../../components/form/textarea.jsx'
+import {makeDraggable, makeDroppable} from './../../utils/dragAndDrop'
+import {TooltipButton} from './../../components/form/tooltip-button.jsx'
+import {actions} from './editor'
 
-export const Pair = () =>
-  <div>PAIR</div>
+
+
+
+const PairForm = (props) => {
+  return(
+    <div className="set-question-container">
+      {get(this.props.item, '_errors.item') &&
+        <div className="error-text">
+          <span className="fa fa-warning"></span>
+          {this.props.item._errors.item}
+        </div>
+      }
+      {get(this.props.item, '_errors.items') &&
+        <div className="error-text">
+          <span className="fa fa-warning"></span>
+          {this.props.item._errors.items}
+        </div>
+      }
+      {get(this.props.item, '_errors.sets') &&
+        <div className="error-text">
+          <span className="fa fa-warning"></span>
+          {this.props.item._errors.sets}
+        </div>
+      }
+      {get(this.props.item, '_errors.solutions') &&
+        <div className="error-text">
+          <span className="fa fa-warning"></span>
+          {this.props.item._errors.solutions}
+        </div>
+      }
+      {get(this.props.item, '_errors.odd') &&
+        <div className="error-text">
+          <span className="fa fa-warning"></span>
+          {this.props.item._errors.odd}
+        </div>
+      }
+      <div className="form-group">
+        <label htmlFor="set-penalty">{tex('set_penalty_label')}</label>
+        <input
+          id="set-penalty"
+          className="form-control"
+          value={this.props.item.penalty}
+          type="number"
+          min="0"
+          onChange={e => this.props.onChange(
+             actions.updateProperty('penalty', e.target.value)
+          )}
+        />
+      </div>
+      <div className="checkbox">
+        <label>
+          <input
+            type="checkbox"
+            checked={this.props.item.random}
+            onChange={e => this.props.onChange(
+              actions.updateProperty('random', e.target.value)
+            )}
+          />
+        {tex('set_shuffle_labels_and_proposals')}
+        </label>
+      </div>
+      <hr/>
+      <div className="sets-builder-container">
+        <div className="pool-col">
+          <ItemList onChange={this.props.onChange} solutions={this.props.item.solutions} items={this.props.item.items} />
+          <hr/>
+          <OddList onChange={this.props.onChange} solutions={this.props.item.solutions} odd={this.props.item.items} />
+        </div>
+        <div className="sets-col">
+          <SetList solutions={this.props.item.solutions} onChange={this.props.onChange} sets={this.props.item.sets} />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+PairForm.propTypes = {
+  item: T.shape({
+    id: T.string.isRequired,
+    random: T.bool.isRequired,
+    penalty: T.number.isRequired,
+    left: T.arrayOf(T.object).isRequired,
+    right: T.arrayOf(T.object).isRequired,
+    solutions: T.arrayOf(T.object).isRequired,
+    _errors: T.object
+  }).isRequired,
+  onChange: T.func.isRequired
+}
+
+export {PairForm}
