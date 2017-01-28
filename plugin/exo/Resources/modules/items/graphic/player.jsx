f68a4ce96a9da83f3173de1987b22527dbc19096
Stéphane Klein <stephaneklein221@gmail.com>
2017-01-28 22:07:36 +0100

    Player wip

--- plugin/exo/Resources/modules/items/graphic/player.jsx
+++ plugin/exo/Resources/modules/items/graphic/player.jsx
@@ -1,4 +1,16 @@
-import React from 'react'
+import React, {PropTypes as T} from 'react'
+import {asset} from '#/main/core/asset'
 
-export const GraphicPlayer = () =>
-  <div>Graphic player</div>
+export const GraphicPlayer = props =>
+  <div className="graphic-player">
+    <img src={props.item.image.data || asset(props.item.image.url)}></img>
+  </div>
+
+GraphicPlayer.propTypes = {
+  item: T.shape({
+    image: T.oneOfType([
+      {data: T.string.isRequired},
+      {url: T.string.isRequired}
+    ]).isRequired
+  }).isRequired
+}
