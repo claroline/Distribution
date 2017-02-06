be3eae68e0a1d4940703136a33102eb4a45cc435
Elorfin <penin.axel@gmail.com>
2017-02-06 09:16:33 +0100

    fixes cloze answers migration

--- plugin/exo/Installation/Updater/Updater090000.php
+++ plugin/exo/Installation/Updater/Updater090000.php
@@ -130,7 +130,12 @@ class Updater090000
         $proposalSth->execute();
         $proposals = $proposalSth->fetchAll();
 
-        $holeSth = $this->connection->prepare('SELECT id, uuid, selector FROM ujm_hole');
+        $holeSth = $this->connection->prepare('
+            SELECT h.id, h.uuid, h.`position`, h.selector, q.uuid AS question_id
+            FROM ujm_hole AS h
+            JOIN ujm_interaction_hole AS i ON (h.interaction_hole_id = i.id)
+            JOIN ujm_question AS q ON (i.question_id = q.id)
+        ');
         $holeSth->execute();
         $holes = $holeSth->fetchAll();
 
@@ -140,7 +145,7 @@ class Updater090000
 
         // Load answers
         $sth = $this->connection->prepare('
-            SELECT q.mime_type, a.id AS answerId, a.response AS data
+            SELECT q.mime_type, a.id AS answerId, a.response AS data, a.question_id
             FROM ujm_response AS a
             LEFT JOIN ujm_question AS q ON (a.question_id = q.uuid)
             WHERE a.response IS NOT NULL 
@@ -224,21 +229,48 @@ class Updater090000
                     $answerData = json_decode($answer['data'], true);
 
                     $newData = [];
-                    foreach ($answerData as $answerText) {
+                    foreach ($answerData as $position => $answerText) {
                         if (!empty($answerText)) {
-                            $answeredHole = $this->getAnswerPart($answerText['holeId'], $holes);
-                            $hole = new \stdClass();
-                            $hole->holeId = $answeredHole['uuid'];
+                            $answeredHole = null;
+                            $oldAnswer = null;
+                            if (is_array($answerText)) {
+                                // Format = [{"holeId":"61","answerText":""},{"holeId":"62","answerText":""}]
 
-                            if (!$answeredHole['selector']) {
-                                $hole->answerText = $answerText['answerText'];
+                                // Retrieve answered hole
+                                $answeredHole = $this->getAnswerPart($answerText['holeId'], $holes);
+
+                                // Get answer
+                                $oldAnswer = $answerText['answerText'];
                             } else {
-                                // replace keyword id by its value
-                                $keyword = $this->getAnswerPart($answerText['answerText'], $keywords);
-                                $hole->answerText = $keyword['response'];
+                                // Format = {"1":"travail","2":"salaire"}
+
+                                // Retrieve answered hole
+                                foreach ($holes as $hole) {
+                                    if ((int) $hole['position'] === (int) $position
+                                        && $hole['question_id'] === $answer['question_id']) {
+                                        $answeredHole = $hole;
+                                        break;
+                                    }
+                                }
+
+                                // Get answer
+                                $oldAnswer = $answerText;
                             }
 
-                            $newData[] = $hole;
+                            if (!empty($answeredHole) && !empty($oldAnswer)) {
+                                $hole = new \stdClass();
+                                $hole->holeId = $answeredHole['uuid'];
+
+                                if (!$answeredHole['selector']) {
+                                    $hole->answerText = $oldAnswer;
+                                } else {
+                                    // replace keyword id by its value
+                                    $keyword = $this->getAnswerPart($oldAnswer, $keywords);
+                                    $hole->answerText = $keyword['response'];
+                                }
+
+                                $newData[] = $hole;
+                            }
                         }
                     }
 
@@ -268,15 +300,13 @@ class Updater090000
             }
 
             // Update answer data
-            if (!empty($newData)) {
-                $sth = $this->connection->prepare('
-                    UPDATE ujm_response SET `response` = :data WHERE id = :id 
-                ');
-                $sth->execute([
-                    'id' => $answer['answerId'],
-                    'data' => json_encode($newData),
-                ]);
-            }
+            $sth = $this->connection->prepare('
+                UPDATE ujm_response SET `response` = :data WHERE id = :id 
+            ');
+            $sth->execute([
+                'id' => $answer['answerId'],
+                'data' => !empty($newData) ? json_encode($newData) : null,
+            ]);
         }
 
         $this->log('done !');
