c15dc5dc812983605956d9ff4d3daaeaea33a45f
Stéphane Klein <stephaneklein221@gmail.com>
2017-02-06 16:32:45 +0100

    Convert translations to json

--- main/dev/Resources/translation/prettify.js
+++ main/dev/Resources/translation/prettify.js
@@ -1,9 +1,11 @@
 // Rewrites JSON translations files with proper pretty formatting if needed.
 
-var glob = require('glob')
-var fs = require('fs')
+const glob = require('glob')
+const fs = require('fs')
 
-glob('vendor/claroline/distribution/*/*/Resources/translations/*.json', (er, files) => {
+const packageDir = `${__dirname}/../../../..`
+
+glob(`${packageDir}/*/*/Resources/translations/*.json`, (er, files) => {
   files.forEach(file => {
     const content = fs.readFileSync(file, 'utf8')
     const decoded = JSON.parse(content)
