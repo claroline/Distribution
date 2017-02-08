7f318ec2f00e504434c1848474bb8b65fe997d9a
Stéphane Klein <stephaneklein221@gmail.com>
2017-02-08 11:34:33 +0100

    Exclude main component from import script

--- main/dev/Resources/translation/weblate-import.js
+++ main/dev/Resources/translation/weblate-import.js
@@ -1,5 +1,7 @@
 // Outputs JSON data for all translation units suitable for weblate import.
 // See https://docs.weblate.org/en/latest/admin/management.html#import-json
+//
+// Note: core-platform component must be created beforehand.
 
 const glob = require('glob')
 const path = require('path')
@@ -14,16 +16,19 @@ glob(`${packageDir}/*/*/Resources/translations/*.en.json`, (er, files) => {
     const type = parsed.dir.split('/').slice(0, -3).pop()
     const bundle = parsed.dir.split('/').slice(0, -2).pop()
     const domain = parsed.name.split('.')[0]
+    const componentName = `${bundle}-${domain}`
 
-    components.push({
-      name: `${type}-${bundle}-${domain}`,
-      repository_url: `weblate://project/main-core-platform`,
-      vcs: 'git',
-      branch: 'translations',
-      file_format: 'json',
-      filemask: `${type}/${bundle}/${domain}.*.json`,
-      template: `${type}/${bundle}/${domain}.en.json`
-    })
+    if (componentName !== 'core-platform') {
+      components.push({
+        name: `${bundle}-${domain}`,
+        repository_url: `weblate://project/core-platform`,
+        vcs: 'github',
+        branch: 'translations',
+        file_format: 'json',
+        filemask: `${type}/${bundle}/${domain}.*.json`,
+        template: `${type}/${bundle}/${domain}.en.json`
+      })
+    }
   })
 
   console.log(JSON.stringify(components, null, 2))
