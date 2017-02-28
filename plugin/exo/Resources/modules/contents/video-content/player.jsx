7744209e7a16ad82a10fac483ab020df4881ef70
kitan1982 <anhthao.pham@claroline.net>
2017-02-28 11:55:19 +0100

    Deactivates videojs for content item

--- plugin/exo/Resources/modules/contents/video-content/player.jsx
+++ plugin/exo/Resources/modules/contents/video-content/player.jsx
@@ -3,7 +3,7 @@ import {asset} from '#/main/core/asset'
 
 export const VideoContentPlayer = (props) =>
   <div className="video-item-content">
-    <video className="video-js vjs-big-play-centered vjs-default-skin vjs-16-9" controls>
+    <video className="not-video-js vjs-big-play-centered vjs-default-skin vjs-16-9" controls>
       <source src={asset(props.item.url)} type={props.item.type} />
     </video>
   </div>
