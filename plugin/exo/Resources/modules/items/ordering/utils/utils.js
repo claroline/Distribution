40bf11593c3cbd0dfa0c08de5ed1986e74e09ae8
pitrackster <pitrackster@gmail.com>
2017-03-07 17:06:13 +0100

    papers

--- plugin/exo/Resources/modules/items/ordering/utils/utils.js
+++ plugin/exo/Resources/modules/items/ordering/utils/utils.js
@@ -1,1 +1,15 @@
-export const utils = {}
+export const utils = {
+  answerIsValid(answer, solutions){
+    const solution = solutions.find(solution => solution.itemId === answer.itemId)
+    return undefined === solution.position ? false : solution.position === answer.position
+  },
+  showScore(answer, solutions) {
+    return (utils.answerIsValid(answer, solutions) && solutions.find(solution => solution.itemId === answer.itemId).score > 0)
+      || (!utils.answerIsValid(answer, solutions) && solutions.find(solution => solution.itemId === answer.itemId).score < 1)
+  },
+  checkAllAnswers(solutions, answer) {
+    const correctAnswers = solutions.filter(solution => solution.score > 0 && undefined !== answer.find(a => a.itemId === solution.itemId && a.position === solution.position))
+    const correctSolutions = solutions.filter(solution => solution.score > 0)
+    return correctAnswers.length === correctSolutions.length
+  }
+}
