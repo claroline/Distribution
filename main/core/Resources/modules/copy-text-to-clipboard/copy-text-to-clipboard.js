ba492dfe8993d64c24664d1c56a13e4216eebfbe
dtisch <david.tischmacher@gmail.com>
2017-04-25 08:24:32 +0200

    Fix ES6 typos

--- main/core/Resources/modules/copy-text-to-clipboard/copy-text-to-clipboard.js
+++ main/core/Resources/modules/copy-text-to-clipboard/copy-text-to-clipboard.js
@@ -1,31 +1,31 @@
-"use strict";
+'use strict'
 
 window.copyTextToClipboard = function (textToCopy) {
 
-  var textArea = document.createElement("textarea");
-  textArea.style.position = 'fixed';
-  textArea.style.top = 0;
-  textArea.style.left = 0;
-  textArea.style.width = '1px';
-  textArea.style.height = '1px';
-  textArea.style.padding = 0;
-  textArea.style.border = 'none';
-  textArea.style.outline = 'none';
-  textArea.style.boxShadow = 'none';
-  textArea.style.background = 'transparent';
-  textArea.style.zIndex = -1000;
-  textArea.value = textToCopy;
+  let textArea = document.createElement('textarea')
+  textArea.style.position = 'fixed'
+  textArea.style.top = 0
+  textArea.style.left = 0
+  textArea.style.width = '1px'
+  textArea.style.height = '1px'
+  textArea.style.padding = 0
+  textArea.style.border = 'none'
+  textArea.style.outline = 'none'
+  textArea.style.boxShadow = 'none'
+  textArea.style.background = 'transparent'
+  textArea.style.zIndex = -1000
+  textArea.value = textToCopy
 
-  document.body.appendChild(textArea);
+  document.body.appendChild(textArea)
 
-  textArea.select();
+  textArea.select()
 
   try {
-    document.execCommand('copy');
+    document.execCommand('copy')
   } catch (error) {
-    alert(window.Translator.trans('copy_to_clipboard_success', 'platform'));
+    alert(window.Translator.trans('copy_to_clipboard_success', 'platform'))
   }
 
-  document.body.removeChild(textArea);
-  alert(window.Translator.trans('copy_to_clipboard_success', 'platform'));
+  document.body.removeChild(textArea)
+  alert(window.Translator.trans('copy_to_clipboard_success', 'platform'))
 }
