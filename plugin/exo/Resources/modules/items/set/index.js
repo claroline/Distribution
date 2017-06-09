c491b9e352fd28d9bc2c18c933ea07c7dbe35a06
ngodfraind <nicolas.godfraind@gmail.com>
2017-06-09 11:25:16 +0200

    set

--- plugin/exo/Resources/modules/items/set/index.js
+++ plugin/exo/Resources/modules/items/set/index.js
@@ -2,14 +2,31 @@ import editor from './editor'
 import {SetPaper} from './paper.jsx'
 import {SetPlayer} from './player.jsx'
 import {SetFeedback} from './feedback.jsx'
-import {CorrectedAnswer} from '#/plugin/exo/quiz/correction/components/corrected-answer'
+import {CorrectedAnswer, Answerable} from '#/plugin/exo/quiz/correction/components/corrected-answer'
+import times from 'lodash/times'
 
 function expectAnswer(item) {
   return item.solutions && item.solutions.associations ? item.solutions.associations : []
 }
 
-function getCorrectedAnswer() {
+function getCorrectedAnswer(item, answer = {data: []}) {
   const corrected = new CorrectedAnswer()
+
+  item.solutions.associations.forEach(association => {
+    const userAnswer = answer ? answer.data.find(answer => (answer.itemId === association.itemId) && (answer.setId === association.setId)): null
+
+    userAnswer ?
+      corrected.addExpected(new Answerable(association.score)):
+      corrected.addMissing(new Answerable(association.score))
+  })
+
+  item.solutions.odd.forEach(odd => {
+    const penalty = answer ? answer.data.find(answer => answer.itemId === odd.itemId): null
+    if (penalty) corrected.addPenalty(new Answerable(-odd.score))
+  })
+
+  const found = answer ? answer.data.length: 0
+  times(item.solutions.associations.length - found, () => corrected.addPenalty(new Answerable(item.penalty)))
 
   return corrected
 }
