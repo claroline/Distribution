bb08f7796cf90cd041f3bea0fbc6d9ab34b9c297
ngodfraind <nicolas.godfraind@gmail.com>
2017-06-09 11:50:05 +0200

    words

--- plugin/exo/Resources/modules/items/words/index.js
+++ plugin/exo/Resources/modules/items/words/index.js
@@ -2,16 +2,35 @@ import editor from './editor'
 import {WordsPaper} from './paper.jsx'
 import {WordsPlayer} from './player.jsx'
 import {WordsFeedback} from './feedback.jsx'
-import {CorrectedAnswer} from '#/plugin/exo/quiz/correction/components/corrected-answer'
+import {CorrectedAnswer, Answerable} from '#/plugin/exo/quiz/correction/components/corrected-answer'
 
 function expectAnswer(item) {
   return item.solutions
 }
 
-function getCorrectedAnswer() {
+function getCorrectedAnswer(item, answer = {data: ''}) {
   const corrected = new CorrectedAnswer()
 
+  item.solutions.forEach(solution => {
+    const hasKeyword = containsKeyword(solution.text, solution.caseSensitive, answer ? answer.data: '')
+
+    if (hasKeyword) {
+      solution.score > 0 ?
+        corrected.addExpected(new Answerable(solution.score)):
+        corrected.addUnexpected(new Answerable(solution.score))
+    } else {
+      if (solution.score > 0) corrected.addMissing(new Answerable(solution.score))
+    }
+  })
+
   return corrected
+}
+
+function containsKeyword(keyword, caseSensitive, text = '') {
+  const regex = new RegExp(keyword, caseSensitive ? '': 'i')
+  const test = regex.test(text)
+
+  return test
 }
 
 export default {
