bba6a8b26638788877c3dc41b52e4195e43f06cc
Axel Penin <Elorfin@users.noreply.github.com>
2017-06-21 18:38:02 +0200

    Update decorators.test.js

--- plugin/exo/Resources/modules/quiz/decorators.test.js
+++ plugin/exo/Resources/modules/quiz/decorators.test.js
@@ -81,7 +81,8 @@ describe('Decorator', () => {
           anonymizeAttempts: false,
           showScoreAt: SHOW_SCORE_AT_CORRECTION,
           showStatistics: false,
-          showFullCorrection: true
+          showFullCorrection: true,
+          endMessage: ""
         }
       },
       steps: {
