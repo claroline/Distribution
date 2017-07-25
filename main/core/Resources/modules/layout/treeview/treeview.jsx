d79d0c9f1d63dd44285e6cd713e489fa31f84942
ngodfraind <nicolas.godfraind@gmail.com>
2017-07-25 16:10:30 +0200

    Adding collapsable items

--- main/core/Resources/modules/layout/treeview/treeview.jsx
+++ main/core/Resources/modules/layout/treeview/treeview.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 import {PropTypes as T} from 'prop-types'
 import classes from 'classnames'
 
-class TreeView extends Component {
+class TreeNode extends Component {
   constructor(props) {
     super(props)
   }
@@ -15,19 +15,39 @@ class TreeView extends Component {
     return (
       <ul>
         {this.props.data.map(el =>
-          (<li>
-            {this.props.options.selectable &&
-            <input
-              type="checkbox"
-              checked={this.isChecked(el)}
-              name={this.props.options.name + '[]'} value={el.id}
-              onChange={() => this.props.onChange(el)}
-            />
-          } {el.name}
-            <TreeView data={el.children} options={this.props.options} onChange={this.props.onChange}/>
-          </li>)
-        )}
+          (
+            <li>
+              <input type="checkbox" id={"node" + el.id}/>
+              <label>
+                <input type="checkbox"
+                  checked={this.isChecked(el)}
+                  name={this.props.options.name + '[]'} value={el.id}
+                  onChange={() => this.props.onChange(el)}
+                />
+                <span></span>
+              </label>
+              <label htmlFor={"node" + el.id}>{el.name}</label>
+              {el.children.length > 0 &&
+                <TreeNode data={el.children} options={this.props.options} onChange={this.props.onChange}/>
+              }
+            </li>)
+          )
+        }
       </ul>
+    )
+  }
+}
+
+class TreeView extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <div className="acidjs-css3-treeview">
+        <TreeNode {...this.props} />
+      </div>
     )
   }
 }
