69944d3ffce487df8421f46abd2efffbd6744c16
ngodfraind <nicolas.godfraind@gmail.com>
2017-08-02 13:36:14 +0200

    phpmd

--- plugin/exo/Migrations/pdo_mysql/Version20170801115141.php
+++ plugin/exo/Migrations/pdo_mysql/Version20170801115141.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration based on mapping information: modify it with caution
+ * Auto-generated migration based on mapping information: modify it with caution.
  *
  * Generation date: 2017/08/01 11:51:42
  */
@@ -14,17 +14,17 @@ class Version20170801115141 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
-            ALTER TABLE ujm_question 
+        $this->addSql('
+            ALTER TABLE ujm_question
             ADD protect_update TINYINT(1) NOT NULL
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
-            ALTER TABLE ujm_question 
+        $this->addSql('
+            ALTER TABLE ujm_question
             DROP protect_update
-        ");
+        ');
     }
 }
