86f4131f14d92de8f823a6304c260785a1ea1e29
Laurent Gruber <laurent@sk1.be>
2017-08-24 10:01:36 +0200

    add missing use

--- main/core/Command/PlatformUpdateCommand.php
+++ main/core/Command/PlatformUpdateCommand.php
@@ -21,6 +21,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
