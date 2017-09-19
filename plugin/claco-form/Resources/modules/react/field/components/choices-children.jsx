c4d3d123115b47444d8c8332289eaf2d54316213
kitan1982 <anhthao.pham@claroline.net>
2017-09-19 08:44:46 +0200

    eslint

--- plugin/claco-form/Resources/modules/react/field/components/choices-children.jsx
+++ plugin/claco-form/Resources/modules/react/field/components/choices-children.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {PropTypes as T} from 'prop-types'
-import classes from 'classnames'
 import {trans} from '#/main/core/translation'
 import {ChoiceField} from './choice-field.jsx'
 import {getFieldType} from '../../clacoForm/utils'
