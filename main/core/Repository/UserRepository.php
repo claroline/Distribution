2dfbcc43482cee6887ee06f8cf97b9916c230d36
ptsavdar <ptsavdar@gmail.com>
2017-09-29 10:41:00 +0200

    Include username to search users by name

--- main/core/Repository/UserRepository.php
+++ main/core/Repository/UserRepository.php
@@ -665,6 +665,7 @@ class UserRepository extends EntityRepository implements UserProviderInterface
             AND (
                 UPPER(u.lastName) LIKE :search
                 OR UPPER(u.firstName) LIKE :search
+                OR UPPER(u.username) LIKE :search
             )
             ORDER BY u.{$orderedBy} {$order}
         ";
