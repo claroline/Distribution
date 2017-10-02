93ec73ba4a9d8ac2ea99694767caf2844f869d8b
ptsavdar <ptsavdar@gmail.com>
2017-10-02 16:36:49 +0200

    Add target option for resource type pages

--- plugin/website/Resources/modules/view-app/main.controller.js
+++ plugin/website/Resources/modules/view-app/main.controller.js
@@ -4,7 +4,7 @@
 import $ from 'jquery'
 
 export default class MainController {
-  constructor (websiteData) {
+  constructor(websiteData) {
     this.menu = websiteData.pages[ 0 ]
     this.options = websiteData.options
     this.contentHeight = 400
@@ -33,11 +33,16 @@ export default class MainController {
     }
   }
 
-  buildHref (page) {
-    if (page.type=='url' && page.target==1) {
+  buildHref(page) {
+    if (page.type === 'url' && page.target === 1) {
       return page.url
+    } else if (page.type === 'resource' && page.target === 1) {
+      return window.Routing.generate('claro_resource_open', {
+        'resourceType': page.resourceNodeType,
+        'node': page.resourceNodeId
+      })
     }
-    return this.basePath + "/" + page.id
+    return this.basePath + '/' + page.id
   }
 }
 
