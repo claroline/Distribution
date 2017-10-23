15a71be540f0ffdbed3314e854e39a0c7471f875
kitan1982 <anhthao.pham@claroline.net>
2017-10-23 10:24:28 +0200

    Updates rights for announcement deletion

--- plugin/announcement/Controller/API/AnnouncementController.php
+++ plugin/announcement/Controller/API/AnnouncementController.php
@@ -119,7 +119,7 @@ class AnnouncementController
      */
     public function deleteAction(AnnouncementAggregate $aggregate, Announcement $announcement)
     {
-        $this->checkPermission('DELETE', $aggregate->getResourceNode(), [], true);
+        $this->checkPermission('EDIT', $aggregate->getResourceNode(), [], true);
 
         try {
             $this->manager->delete($announcement);
