e204be7458805989887d3c7fbfd1cc69a94fd4bf
David Tischmacher <david.tischmacher@gmail.com>
2017-10-31 15:10:33 +0100

    Travis fixes

--- main/core/Migrations/pdo_mysql/Version20171031142013.php
+++ main/core/Migrations/pdo_mysql/Version20171031142013.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration based on mapping information: modify it with caution
+ * Auto-generated migration based on mapping information: modify it with caution.
  *
  * Generation date: 2017/10/31 02:20:14
  */
@@ -14,17 +14,17 @@ class Version20171031142013 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_resource_node 
             ADD views_count INT DEFAULT 0 NOT NULL
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_resource_node 
             DROP views_count
-        ");
+        ');
     }
 }
