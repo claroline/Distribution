0674036131fe236b10ac53fa3a40d652460ffc83
Elorfin <penin.axel@gmail.com>
2018-02-13 10:22:26 +0100

    [CoreBundle] initializes resource text api

---
+++ main/core/Controller/APINew/Resource/TextController.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+ * This file is part of the Claroline Connect package.
+ *
+ * (c) Claroline Consortium <consortium@claroline.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Claroline\CoreBundle\Controller\APINew\Contact;
+
+use Claroline\CoreBundle\Entity\Resource\Text;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration as EXT;
+
+/**
+ * @EXT\Route("resource/text")
+ */
+class TextController
+{
+    /**
+     * @EXT\Route(
+     *     "/{id}/content",
+     *     name="apiv2_resource_text_content"
+     * )
+     * @param Text $text
+     *
+     * @return string
+     */
+    public function getContentAction(Text $text)
+    {
+        return $text->getContent();
+    }
+}
