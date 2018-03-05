d906bec4b15428ce688cbaed667eb80a0c77a52d
ngodfraind <nicolas.godfraind@gmail.com>
2018-03-05 15:14:33 +0100

    more useless code removed

--- main/core/Resources/modules/data/types/enum/prop-types.js
+++ main/core/Resources/modules/data/types/enum/prop-types.js
@@ -3,14 +3,12 @@ import {PropTypes as T} from 'prop-types'
 const EnumOptions = {
   propTypes: {
     choices: T.object.isRequired,
-    filterChoices: T.function,
     multiple: T.bool,
     noEmpty: T.bool
   },
   defaultProps: {
     multiple: false,
-    noEmpty: false,
-    filterChoices: () => true
+    noEmpty: false
   }
 }
 
