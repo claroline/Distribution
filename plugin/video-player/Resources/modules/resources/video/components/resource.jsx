b9dd94df9304a90a947384bc06470edbf17a3ff5
kitan1982 <anhthao.pham@claroline.net>
2018-03-13 09:51:54 +0100

    Allows to copy permalink to clipboard

--- plugin/video-player/Resources/modules/resources/video/components/resource.jsx
+++ plugin/video-player/Resources/modules/resources/video/components/resource.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import {PropTypes as T} from 'prop-types'
 import {connect} from 'react-redux'
 
+import {generateUrl} from '#/main/core/api/router'
 import {trans} from '#/main/core/translation'
+import {copyToClipboard} from '#/main/core/copy-text-to-clipboard'
 import {select as resourceSelect} from '#/main/core/resource/selectors'
 import {RoutedPageContent} from '#/main/core/layout/router'
 import {ResourcePageContainer} from '#/main/core/resource/containers/page.jsx'
 
-import {actions as playerActions} from '#/plugin/video-player/resources/video/player/actions'
 import {Player} from '#/plugin/video-player/resources/video/player/components/player.jsx'
 
 const Resource = props => {
@@ -23,9 +24,14 @@ const Resource = props => {
     customActions.push({
       icon: 'fa fa-fw fa-download',
       label: trans('download'),
-      action: () => props.download(props.resource.autoId)
+      action: () => window.location = generateUrl('claro_resource_download') + '?ids[]=' + props.resource.autoId
     })
   }
+  customActions.push({
+    icon: 'fa fa-fw fa-clipboard',
+    label: trans('copy_permalink_to_clipboard'),
+    action: () => copyToClipboard(props.url)
+  })
 
   return (
     <ResourcePageContainer
@@ -44,16 +50,15 @@ Resource.propTypes = {
   resource: T.shape({
     autoId: T.number.isRequired
   }).isRequired,
+  url: T.string.isRequired,
   canDownload: T.bool.isRequired
 }
 
 const VideoPlayerResource = connect(
   state => ({
     resource: state.resourceNode,
+    url: state.url,
     canDownload: resourceSelect.exportable(state)
-  }),
-  (dispatch) => ({
-    download: (id) => dispatch(playerActions.download(id))
   })
 )(Resource)
 
--- plugin/video-player/Resources/modules/resources/video/player/actions.js
+++
@@ -1,5 +0,0 @@
-import {generateUrl} from '#/main/core/api/router'
-
-export const actions = {}
-
-actions.download = id => window.location = generateUrl('claro_resource_download') + '?ids[]=' + id
