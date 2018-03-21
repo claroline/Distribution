3615b120083c1ef1017e95260b5f79979fac44e3
ptsavdar <ptsavdar@gmail.com>
2018-03-21 13:45:58 +0100

    First implementation of select-plus

--- main/core/Resources/modules/layout/select-plus/components/optgroup.jsx
+++ main/core/Resources/modules/layout/select-plus/components/optgroup.jsx
@@ -4,16 +4,48 @@ import {trans} from '#/main/core/translation'
 import {Option} from '#/main/core/layout/select-plus/components/option.jsx'
 
 class Optgroup extends Component {
+  constructor(props) {
+    super(props)
+    
+    this.handleOnClick = this.handleOnClick.bind(this)
+  }
+  
+  handleOnClick(evt) {
+    evt.preventDefault()
+    evt.stopPropagation()
+  }
+  
   render() {
     const props = this.props
     return (
-      <optgroup  label={props.transDomain ? trans(props.label, {}, props.transDomain) : props.label}>
+      <div
+        className="select-plus-option-group"
+        onClick={this.handleOnClick}
+      >
+        <div className="select-plus-option-group-label">
+          { props.transDomain ?
+            trans(props.label, {}, props.transDomain) :
+            props.label
+          }
+        </div>
         {props.choices.map(choice =>(
           choice.choices.length < 1 ?
-            <Option key={choice.value} value={choice.value} label={choice.label} transDomain={props.transDomain}/> :
-            <Optgroup key={choice.value} label={choice.label} choices={choice.choices} transDomain={props.transDomain}/>
+            <Option
+              key={choice.value}
+              value={choice.value}
+              label={choice.label}
+              transDomain={props.transDomain}
+              onSelect={props.onSelect}
+            /> :
+            <Optgroup
+              key={choice.value}
+              label={choice.label}
+              choices={choice.choices}
+              transDomain={props.transDomain}
+              onSelect={props.onSelect}
+            />
         ))}
-      </optgroup>
+      </div>
     )
   }
 }
@@ -21,6 +53,7 @@ class Optgroup extends Component {
 Optgroup.propTypes = {
   choices: T.array.isRequired,
   label: T.string.isRequired,
+  onSelect: T.func.isRequired,
   transDomain: T.string
 }
 
--- main/core/Resources/modules/layout/select-plus/components/select.jsx
+++ main/core/Resources/modules/layout/select-plus/components/select.jsx
@@ -1,32 +1,73 @@
 import React, {Component} from 'react'
 import {PropTypes as T} from 'prop-types'
+import {classNames} from '#/main/core/scaffolding/classnames'
 import {Option} from '#/main/core/layout/select-plus/components/option.jsx'
 import {Optgroup}  from '#/main/core/layout/select-plus/components/optgroup.jsx'
 
 class Select extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {collapsed: true}
+    this.handleOnChange = this.handleOnChange.bind(this)
+    this.handleOnClick  = this.handleOnClick.bind(this)
+    this.collapse = this.collapse.bind(this)
+  }
+  
+  handleOnChange(newValue, event) {
+    if (this.props.value !== newValue) {
+      this.props.onChange(newValue)
+    }
+    event.preventDefault()
+    event.stopPropagation()
+  }
+  
+  handleOnClick(event) {
+    this.setState({collapsed: !this.state.collapsed})
+    event.preventDefault()
+    event.stopPropagation()
+  }
+  
+  collapse() {
+    this.setState({collapsed: true})
+  }
+  
   render() {
     const props = this.props
     return (
-      <select
+      <div
         value={props.value}
-        className="form-control input-sm select-plus"
-        onClick={e => {
-          e.preventDefault()
-          e.stopPropagation()
-        }}
-        onChange={e => {
-          const val = e.target.value
-          props.onChange(val)
-          e.preventDefault()
-          e.stopPropagation()
-        }}
+        className={classNames('form-control input-sm select-plus', props.className)}
+        onClick={this.handleOnClick}
+        tabIndex={0}
+        onBlur={this.collapse}
       >
+        <div
+          className="select-plus-value"
+        >
+          {props.value}
+        </div>
+        <div
+          className={classNames('select-plus-options', this.state.collapsed ? 'hidden' : '')}
+        >
         {props.choices.map(choice =>(
           choice.choices.length < 1 ?
-            <Option key={choice.value} value={choice.value} label={choice.label} transDomain={props.transDomain}/> :
-            <Optgroup key={choice.value} label={choice.label} choices={choice.choices} transDomain={props.transDomain}/>
+            <Option
+              key={choice.value}
+              value={choice.value}
+              label={choice.label}
+              transDomain={props.transDomain}
+              onSelect={this.handleOnChange}
+            /> :
+            <Optgroup
+              key={choice.value}
+              label={choice.label}
+              choices={choice.choices}
+              transDomain={props.transDomain}
+              onSelect={this.handleOnChange}
+            />
         ))}
-      </select>
+        </div>
+      </div>
     )
   }
 }
@@ -35,6 +76,7 @@ Select.propTypes = {
   choices: T.array.isRequired,
   onChange: T.func.isRequired,
   value: T.any,
+  className: T.string,
   transDomain: T.string
 }
 
