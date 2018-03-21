6b945087c3a1845e490d8e124bcc724f3fe2e003
kitan1982 <anhthao.pham@claroline.net>
2018-03-21 13:23:50 +0100

    Import pdfjs

--- plugin/pdf-player/Resources/modules/resources/pdf/player/components/player.jsx
+++ plugin/pdf-player/Resources/modules/resources/pdf/player/components/player.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {PropTypes as T} from 'prop-types'
 import {connect} from 'react-redux'
 
 import {generateUrl} from '#/main/core/api/router'
 
-const PlayerComponent = props =>
-  <div className="pdf-player">
-      <canvas id="the-canvas" className="pdf-player-page"></canvas>
-  </div>
+class PlayerComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      pdf: null,
+      scale: 1.2
+    }
+  }
+
+  componentDidMount() {
+    PDFJS.getDocument(this.props.url).then((pdf) => {
+      console.log(pdf)
+      this.setState({ pdf })
+    })
+  }
+
+  render() {
+    return (
+      <div className="pdf-player">
+        <canvas id="the-canvas" className="pdf-player-page"></canvas>
+      </div>
+    )
+  }
+}
 
 PlayerComponent.propTypes = {
   resource: T.shape({
