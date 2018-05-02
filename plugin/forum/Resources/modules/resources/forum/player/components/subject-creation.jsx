d8d4c59e4c3810ff5959bb35beb7a3d00ac3fc0d
Pauline Ballot <ballotp@gmail.com>
2018-05-02 15:22:21 +0200

    new subject form

--- plugin/forum/Resources/modules/resources/forum/player/components/subject-creation.jsx
+++ plugin/forum/Resources/modules/resources/forum/player/components/subject-creation.jsx
@@ -9,7 +9,7 @@ import {UserAvatar} from '#/main/core/user/components/avatar'
 const user = currentUser()
 const SubjectCreationComponent = () =>
   <div>
-    <h3>{trans('new_subject', {}, 'forum')}</h3>
+    <h3 className="h2">{trans('new_subject', {}, 'forum')}</h3>
     <div className='user-message-container user-message-form-container user-message-left'>
       <UserAvatar picture={user.picture} />
 
@@ -19,62 +19,64 @@ const SubjectCreationComponent = () =>
             {user.name}
           </div>
         </div>
-        <FormContainer
-          level={3}
-          displayLevel={2}
-          name="subjectForm"
-          // title={trans('new_subject', {}, 'forum')}
-          className="content-container"
-          sections={[
-            {
-              title: trans('general'),
-              primary: true,
-              fields: [
-                {
-                  name: 'display.title',
-                  type: 'text',
-                  label: trans('forum_subject_title_form_title', {}, 'forum'),
-                  required: true
-                },
-                {
-                  name: 'display.post',
-                  type: 'html',
-                  label: trans('post', {}, 'forum'),
-                  required: true
-                },
-                {
-                  name: 'display.tags',
-                  type: 'text',
-                  label: trans('tags')
-                }
-              ]
-            }, {
-              icon: 'fa fa-fw fa-desktop',
-              title: trans('display_parameters'),
-              fields: [
-                {
-                  name: 'display.sticked',
-                  type: 'boolean',
-                  label: trans('stick', {}, 'forum'),
-                  help: trans('stick_explanation', {}, 'forum')
-                },
-                {
-                  name: 'display.poster',
-                  label: trans('poster'),
-                  type: 'image',
-                  options: {
-                    ratio: '3:1'
+        <div className="user-message-content embedded-form-section">
+          <FormContainer
+            level={3}
+            displayLevel={2}
+            name="subjectForm"
+            // title={trans('new_subject', {}, 'forum')}
+            className="content-container"
+            sections={[
+              {
+                title: trans('general'),
+                primary: true,
+                fields: [
+                  {
+                    name: 'display.title',
+                    type: 'text',
+                    label: trans('forum_subject_title_form_title', {}, 'forum'),
+                    required: true
+                  },
+                  {
+                    name: 'display.post',
+                    type: 'html',
+                    label: trans('post', {}, 'forum'),
+                    required: true
+                  },
+                  {
+                    name: 'display.tags',
+                    type: 'text',
+                    label: trans('tags')
                   }
-                }
-              ]
-            }
-          ]}
-        />
+                ]
+              }, {
+                icon: 'fa fa-fw fa-desktop',
+                title: trans('display_parameters'),
+                fields: [
+                  {
+                    name: 'display.sticked',
+                    type: 'boolean',
+                    label: trans('stick', {}, 'forum'),
+                    help: trans('stick_explanation', {}, 'forum')
+                  },
+                  {
+                    name: 'display.poster',
+                    label: trans('poster'),
+                    type: 'image',
+                    options: {
+                      ratio: '3:1'
+                    }
+                  }
+                ]
+              }
+            ]}
+          />
+        </div>
         <button
           className="btn btn-block btn-primary btn-save"
           // onClick={() => }
         >
-          {trans('create_the_subject', {}, 'forum')}
+          {trans('post_the_subject', {}, 'forum')}
         </button>
       </div>
     </div>
