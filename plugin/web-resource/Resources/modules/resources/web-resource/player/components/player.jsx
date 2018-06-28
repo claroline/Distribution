0449b762e2f1a279abcbe7147eeb28bd134f7738
Pauline Ballot <ballotp@gmail.com>
2018-06-28 11:21:35 +0200

    setInterval adapt height

--- plugin/web-resource/Resources/modules/resources/web-resource/player/components/player.jsx
+++ plugin/web-resource/Resources/modules/resources/web-resource/player/components/player.jsx
@@ -7,25 +7,35 @@ import {selectors as resourceSelector} from '#/main/core/resource/store'
 import {select} from '#/plugin/web-resource/resources/web-resource/selectors'
 
 class PlayerComponent extends Component {
+  constructor(props) {
+    super(props)
+    this.checkHeight = this.checkHeight.bind(this)
+    this.state = {
+      height: 0
+    }
+  }
 
-  componentDidMount() {
-    const iframe = document.getElementById('web-resource-iframe')
-    const height = iframe.contentWindow.document.body.scrollHeight
-    return height
+  checkHeight() {
+    const contentHeight = this.iframe.contentWindow.document.body.scrollHeight
+    if (contentHeight !== this.state.height) {
+      this.setState({height: contentHeight})
+    }
+  }
+
+  handleResize() {
+    window.setInterval(this.checkHeight, 3000)
   }
 
   render() {
-
     return (
-      <div>
-        <iframe
-          className="web-resource"
-          id="web-resource-iframe"
-          height={this.height}
-          src={`${asset('uploads/webresource/')}${this.props.workspaceId}/${this.props.path}`}
-          allowFullScreen={true}
-        />
-      </div>
+      <iframe
+        className="web-resource"
+        ref={el => this.iframe = el}
+        onLoad={this.handleResize()}
+        height={this.state.height}
+        src={asset(`uploads/webresource/${this.props.workspaceId}/${this.props.path}`)}
+        allowFullScreen={true}
+      />
     )
   }
 }
