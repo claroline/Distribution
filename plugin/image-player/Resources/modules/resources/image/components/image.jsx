ba452ab70e73eba5b286da401671a0ae1199a9f3
ptsavdar <ptsavdar@gmail.com>
2018-06-29 09:35:13 +0200

    Unused dependency

--- plugin/image-player/Resources/modules/resources/image/components/image.jsx
+++ plugin/image-player/Resources/modules/resources/image/components/image.jsx
@@ -3,7 +3,6 @@ import {PropTypes as T} from 'prop-types'
 import {connect} from 'react-redux'
 
 import {trans} from '#/main/core/translation'
-import {asset} from '#/main/app/config'
 import {copy} from '#/main/app/clipboard'
 import {PageContent} from '#/main/core/layout/page'
 import {ResourcePageContainer} from '#/main/core/resource/containers/page'
