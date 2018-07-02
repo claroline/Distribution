3cfecf80b9299cb0f983f96440ad9906879b5607
nicolasjacquemet <nicolas.jacquemet@recherche.univ-lyon1.fr>
2018-07-02 15:31:27 +0200

    updated plugin.js

--- plugin/blog/Resources/modules/plugin.js
+++ plugin/blog/Resources/modules/plugin.js
@@ -2,5 +2,6 @@ import {registry} from '#/main/app/plugins/registry'
 
 registry.add('blog', {
   resources: {
+    'icap_blog': () => { return import(/* webpackChunkName: "plugin-blog-blog-resource" */ '#/plugin/blog/resources/blog') }
   }
 })
