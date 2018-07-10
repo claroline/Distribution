b064cdc06edadf1ad1c968d6664b1d771c8d93c0
ngodfraind <nicolas.godfraind@gmail.com>
2018-07-10 14:48:34 +0200

    fix

--- main/core/Library/Installation/AdditionalInstaller.php
+++ main/core/Library/Installation/AdditionalInstaller.php
@@ -11,6 +11,7 @@
 
 namespace Claroline\CoreBundle\Library\Installation;
 
+use Claroline\CoreBundle\Entity\Workspace\Workspace;
 use Claroline\InstallationBundle\Additional\AdditionalInstaller as BaseInstaller;
 use Psr\Log\LogLevel;
 use Symfony\Bundle\SecurityBundle\Command\InitAclCommand;
@@ -387,12 +388,13 @@ class AdditionalInstaller extends BaseInstaller implements ContainerAwareInterfa
         $this->log('Updating resource icons...');
         $this->container->get('claroline.manager.icon_set_manager')->setLogger($this->logger);
         $this->container->get('claroline.manager.icon_set_manager')->addDefaultIconSets();
+        $om = $this->container->get('claroline.persistence.object_manager');
 
-        if (!$this->workspaceRepo->findOneBy(['code' => 'default_personal', 'personal' => false, 'model' => true])) {
+        if (!$om->getRepository(Workspace::class)->findOneBy(['code' => 'default_personal', 'personal' => false, 'model' => true])) {
             $this->container->get('claroline.manager.workspace_manager')->getDefaultModel(false, true);
         }
 
-        if (!$this->workspaceRepo->findOneBy(['code' => 'default_workspace', 'personal' => true, 'model' => true])) {
+        if (!$om->getRepository(Workspace::class)->findOneBy(['code' => 'default_workspace', 'personal' => true, 'model' => true])) {
             $this->container->get('claroline.manager.workspace_manager')->getDefaultModel(true, true);
         }
     }
