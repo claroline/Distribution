0ca6e3ceedeb382047417d92cc30ea1b496af8a9
kitan1982 <anhthao.pham@claroline.net>
2018-07-13 11:29:32 +0200

    Sets subject creation as primary action in forum

--- plugin/forum/Resources/modules/resources/forum/editor/components/editor.jsx
+++ plugin/forum/Resources/modules/resources/forum/editor/components/editor.jsx
@@ -5,6 +5,7 @@ import {connect} from 'react-redux'
 import {trans} from '#/main/core/translation'
 import {FormContainer} from '#/main/core/data/form/containers/form'
 import {select as formSelect} from '#/main/core/data/form/selectors'
+import {actions as formActions} from '#/main/core/data/form/actions'
 
 import {Forum as ForumType} from '#/plugin/forum/resources/forum/prop-types'
 import {constants} from '#/plugin/forum/resources/forum/constants'
@@ -16,6 +17,16 @@ const EditorComponent = (props) =>
     name="forumForm"
     title={trans('parameters')}
     className="content-container"
+    buttons={true}
+    save={{
+      type: 'callback',
+      callback: () => props.saveForm(props.forumForm.id)
+    }}
+    cancel={{
+      type: 'link',
+      target: '/',
+      exact: true
+    }}
     sections={[
       {
         icon: 'fa fa-fw fa-home',
@@ -102,6 +113,11 @@ EditorComponent.propTypes = {
 const Editor = connect(
   (state) => ({
     forumForm: formSelect.data(formSelect.form(state, 'forumForm'))
+  }),
+  (dispatch) => ({
+    saveForm(forumId) {
+      dispatch(formActions.saveForm('forumForm', ['apiv2_forum_update', {id: forumId}]))
+    }
   })
 )(EditorComponent)
 
