9872f0b773600884b78aeb6040c8add58c573515
ngodfraind <nicolas.godfraind@gmail.com>
2018-07-23 18:23:03 +0200

    Travis will like this migration.

--- main/core/Migrations/pdo_mysql/Version20180723154314.php
+++ main/core/Migrations/pdo_mysql/Version20180723154314.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration based on mapping information: modify it with caution
+ * Auto-generated migration based on mapping information: modify it with caution.
  *
  * Generation date: 2018/07/23 03:43:15
  */
@@ -14,15 +14,15 @@ class Version20180723154314 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_home_tab CHANGE name name VARCHAR(255) DEFAULT NULL
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_home_tab CHANGE name name VARCHAR(255) NOT NULL COLLATE utf8_unicode_ci
-        ");
+        ');
     }
 }
