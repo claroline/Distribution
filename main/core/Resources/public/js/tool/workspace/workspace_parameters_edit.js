450fb884e794c905c1bdee6ee41653e54e6b4ae2
ngodfraind <nicolas.godfraind@gmail.com>
2018-08-10 11:52:07 +0200

    Removing more and more

--- main/core/Resources/public/js/tool/workspace/workspace_parameters_edit.js
+++ main/core/Resources/public/js/tool/workspace/workspace_parameters_edit.js
@@ -8,26 +8,26 @@
  */
 
 (function () {
-    'use strict';
+  'use strict'
 
-    var twigWorkspaceId = document.getElementById('twig-workspace-id').getAttribute('data-workspaceId');
+  var twigWorkspaceId = document.getElementById('twig-workspace-id').getAttribute('data-workspaceId')
 
-    $('.workspace-delete-confirmation').click(function () {
-        $('#delete-ws-validation-box').modal('show');
-    });
+  $('.workspace-delete-confirmation').click(function () {
+    $('#delete-ws-validation-box').modal('show')
+  })
 
-    $('#delete-confirm-ok').click(function () {
-        $.ajax({
-            url: Routing.generate('claro_workspace_delete', {'workspaceId': twigWorkspaceId}),
-            type: 'DELETE',
-            success: function () {
-                window.location = Routing.generate('claro_desktop_open_tool', {'toolName': 'home'});
-            },
-            error: function (xhr) {
-                if (xhr.status === 403) {
-                  window.location.reload();
-                }
-            }
-        });
-    });
-})();
+  $('#delete-confirm-ok').click(function () {
+    $.ajax({
+      url: Routing.generate('claro_workspace_delete', {'workspaceId': twigWorkspaceId}),
+      type: 'DELETE',
+      success: function () {
+        window.location = Routing.generate('claro_desktop_open_tool', {'toolName': 'home'})
+      },
+      error: function (xhr) {
+        if (xhr.status === 403) {
+          window.location.reload()
+        }
+      }
+    })
+  })
+})()
