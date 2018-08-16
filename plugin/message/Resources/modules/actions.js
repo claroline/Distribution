d562c9ba1fbd901302952a1e6ce88a3b856d8f0c
Pauline Ballot <ballotp@gmail.com>
2018-08-16 17:18:31 +0200

    mailNotifications WIP

--- plugin/message/Resources/modules/actions.js
+++ plugin/message/Resources/modules/actions.js
@@ -137,3 +137,13 @@ actions.unreadMessages = (messages) => ({
     }
   }
 })
+
+actions.setMailNotification = (user, mailNotified) => ({
+  [API_REQUEST]: {
+    url: ['apiv2_user_update', {id: user.id}],
+    request: {
+      body: JSON.stringify(Object.assign({}, user, {meta: {mailNotified:mailNotified}})),
+      method: 'PUT'
+    }
+  }
+})
--- plugin/message/Resources/modules/components/messages-parameters.jsx
+++ plugin/message/Resources/modules/components/messages-parameters.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
+import {connect} from 'react-redux'
+import {PropTypes as T} from 'prop-types'
 
 import {trans, transChoice} from '#/main/core/translation'
+import {CALLBACK_BUTTON} from '#/main/app/buttons'
 import {FormData} from '#/main/app/content/form/containers/data'
 import {currentUser} from '#/main/core/user/current'
+import {selectors as formSelectors} from '#/main/app/content/form/store/selectors'
+
+import {actions} from '#/plugin/message/actions'
 
 const authenticatedUser = currentUser()
 
-const MessagesParameters = () =>
+const MessagesParametersComponent = (props) =>
   <div>
+    {console.log(authenticatedUser)}
     <h2>{trans('preferences')}</h2>
     <FormData
       level={3}
       displayLevel={2}
       buttons={true}
-      // target={}
+      save={{
+        type: CALLBACK_BUTTON,
+        callback: () => props.setMailNotification(authenticatedUser, props.messagesParameters.mailNotified)
+      }}
       name="messagesParameters"
       className="content-container"
       sections={[
@@ -22,7 +32,7 @@ const MessagesParameters = () =>
           primary: true,
           fields: [
             {
-              name: 'emailNotifications',
+              name: 'mailNotified',
               type: 'boolean',
               label: transChoice('get_mail_notifications', authenticatedUser.email, {adress: authenticatedUser.email}),
               required: true
@@ -32,6 +42,24 @@ const MessagesParameters = () =>
       ]}
     />
   </div>
+
+MessagesParametersComponent.propTypes = {
+  setMailNotification: T.func.isRequired,
+  messagesParameters: T.shape({
+    isMailNotified: T.bool
+  })
+}
+
+const MessagesParameters = connect(
+  state => ({
+    messagesParameters: formSelectors.data(formSelectors.form(state, 'messagesParameters'))
+  }),
+  dispatch => ({
+    setMailNotification(user, mailNotified) {
+      dispatch(actions.setMailNotification(user, mailNotified))
+    }
+  })
+)(MessagesParametersComponent)
 
 export {
   MessagesParameters
