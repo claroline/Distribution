9c2a285e18cd65ca38e59b8ee281e52d92eca55d
Elorfin <penin.axel@gmail.com>
2018-08-17 11:22:04 +0200

    fixes

--- plugin/exo/Resources/modules/quiz/selectors.js
+++ plugin/exo/Resources/modules/quiz/selectors.js
@@ -4,28 +4,81 @@ import {currentUser} from '#/main/core/user/current'
 import {selectors as resourceSelect} from '#/main/core/resource/store'
 import {hasPermission} from '#/main/core/resource/permissions'
 
-// TODO : use reselect
 // TODO : there is possible code refactoring with editor/selectors.js
 
-const quiz = state => state.quiz
-const id = state => state.quiz.id
-const empty = state => state.quiz.steps.length === 0
-const steps = state => state.steps
-const items = state => state.items
-const description = state => state.quiz.description
-const parameters = state => state.quiz.parameters
-const title = state => state.quiz.title
-const meta = state => state.quiz.meta
-const viewMode = state => state.viewMode
+const STORE_NAME = 'resource'
+
+const registered = () => null !== currentUser()
+
+const resource = (state) => state[STORE_NAME]
+
+const steps = createSelector(
+  resource,
+  (resource) => resource.steps
+)
+const items = createSelector(
+  resource,
+  (resource) => resource.items
+)
+
+const viewMode = createSelector(
+  resource,
+  (resource) => resource.viewMode
+)
+
+const quiz = createSelector(
+  resource,
+  (resource) => resource.quiz
+)
+
+const id = createSelector(
+  quiz,
+  (quiz) => quiz.id
+)
+
+const testMode = createSelector(
+  quiz,
+  (quiz) => quiz.testMode || false
+)
+
+const quizSteps = createSelector(
+  quiz,
+  (quiz) => quiz.steps || []
+)
+
+const empty = createSelector(
+  quizSteps,
+  (quizSteps) => quizSteps.length === 0
+)
+
+const description = createSelector(
+  quiz,
+  (quiz) => quiz.description
+)
+
+const parameters = createSelector(
+  quiz,
+  (quiz) => quiz.parameters
+)
+
+const title = createSelector(
+  quiz,
+  (quiz) => quiz.title
+)
+
+const meta = createSelector(
+  quiz,
+  (quiz) => quiz.meta
+)
+
+
 const hasPapers = state => state.quiz.meta.paperCount > 0 || (state.papers.papers && state.papers.papers.length > 0)
 const hasUserPapers = state => state.quiz.meta.userPaperCount > 0
 
-const registered = () => null !== currentUser()
-const saveEnabled = state => !state.editor.saved && !state.editor.saving
 const noItems = state => Object.keys(state.quiz.steps).length === 1 && Object.keys(state.items).length === 0
 const firstStepId = state => state.quiz.steps[0]
 const hasOverview = state => state.quiz.parameters.showOverview
-const testMode = state => state.quiz.testMode
+
 const papersShowExpectedAnswers = state => state.quiz.parameters.showFullCorrection
 const papersShowStatistics = state => state.quiz.parameters.showStatistics
 const allPapersStatistics = state => state.quiz.parameters.allPapersStatistics
@@ -47,6 +100,8 @@ const docimologyAdmin = createSelector(
 
 // TODO : remove default export and use named one
 export default {
+  STORE_NAME,
+  resource,
   id,
   quiz,
   steps,
@@ -62,7 +117,6 @@ export default {
   parameters,
   title,
   viewMode,
-  saveEnabled,
   noItems,
   firstStepId,
   hasOverview,
@@ -74,6 +128,8 @@ export default {
 }
 
 export const select = {
+  STORE_NAME,
+  resource,
   id,
   quiz,
   steps,
@@ -89,7 +145,6 @@ export const select = {
   parameters,
   title,
   viewMode,
-  saveEnabled,
   noItems,
   firstStepId,
   hasOverview,
