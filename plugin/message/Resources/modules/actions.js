ae497ecfeaa8b6fffd78e32b9a30466aeb21707b
Pauline Ballot <ballotp@gmail.com>
2018-08-17 17:32:43 +0200

    sending simple object

--- plugin/message/Resources/modules/actions.js
+++ plugin/message/Resources/modules/actions.js
@@ -144,11 +144,7 @@ actions.setMailNotification = (user, mailNotified) => ({
   [API_REQUEST]: {
     url: ['apiv2_user_update', {id: user.id}],
     request: {
-      body: JSON.stringify(Object.assign({}, user, {
-        meta: merge({}, user.meta, {
-          mailNotified: mailNotified
-        })
-      })),
+      body: JSON.stringify(Object.assign({}, user, {meta: {mailNotified: mailNotified}})),
       method: 'PUT'
     },
     success: (data, dispatch) => {
