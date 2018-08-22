e84f60a52d6fc1f5f2a84afd1c154112f716f680
Pauline Ballot <ballotp@gmail.com>
2018-08-22 10:31:46 +0200

    show modal

--- main/core/Resources/modules/widget/types/resource/components/parameters.jsx
+++ main/core/Resources/modules/widget/types/resource/components/parameters.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-// import {connect} from 'react-redux'
-// import {PropTypes as T} from 'prop-types'
+import {connect} from 'react-redux'
+import {PropTypes as T} from 'prop-types'
 
 import {trans} from '#/main/core/translation'
 import {FormData} from '#/main/app/content/form/containers/data'
-// import {actions as modalActions} from '#/main/app/overlay/modal/store'
-// import {actions as formActions} from '#/main/app/content/form/store'
-// import {ResourceNode as ResourceNodeTypes} from '#/main/core/resource/data/types/resource/prop-types'
-//
-// import {MODAL_CONTENT_PARAMETERS} from '#/main/core/widget/content/modals/parameters'
+import {actions as modalActions} from '#/main/app/overlay/modal/store'
+import {actions as formActions} from '#/main/app/content/form/store'
+import {ResourceNode as ResourceNodeTypes} from '#/main/core/resource/data/types/resource/prop-types'
 
-const ResourceWidgetParameters = (props) =>
+import {MODAL_WIDGET_CONTENT} from '#/main/core/widget/content/modals/creation'
+
+const ResourceWidgetForm = (props) =>
   <FormData
+    {...props}
     embedded={true}
     level={5}
     name={props.name}
@@ -24,42 +25,44 @@ const ResourceWidgetParameters = (props) =>
             name: 'parameters.resource',
             label: trans('resource'),
             type: 'resource',
-            required: true
-            // onChange: (selected) => {
-            //   props.updateProp(props.name, 'parameters.resource', selected)
-            //   if (props.modal) {
-            //     props.showContentParametersModal(props.resource)
-            //   }
-            // }
+            required: true,
+            onChange: (selected) => {
+              console.log('onChange')
+              console.log(selected)
+              props.updateProp(props.name, 'parameters.resource', selected)
+              // if (props.modal) {
+              props.showContentParametersModal(props.resource)
+              // }
+            }
           }
         ]
       }
     ]}
   />
 
-// ResourceWidgetForm.propTypes = {
-//   name: T.string.isRequired,
-//   resource: T.shape(ResourceNodeTypes.propTypes),
-//   updateProp: T.func.isRequired,
-//   showContentParametersModal: T.func.isRequired,
-//   modal: T.bool.isRequired
-// }
+ResourceWidgetForm.propTypes = {
+  name: T.string.isRequired,
+  resource: T.shape(ResourceNodeTypes.propTypes),
+  updateProp: T.func.isRequired,
+  showContentParametersModal: T.func.isRequired,
+  modal: T.bool.isRequired
+}
 
-// ResourceWidgetForm.defaultProps = {
-//   modal: false
-// }
+ResourceWidgetForm.defaultProps = {
+  modal: false
+}
 
-// const ResourceWidgetParameters = connect(
-//   null
-//   // (dispatch) => ({
-//   //   updateProp(formName, prop, value) {
-//   //     dispatch(formActions.updateProp(formName, prop, value))
-//   //   },
-//   //   showContentParametersModal(resource) {
-//   //     dispatch(modalActions.showModal(MODAL_CONTENT_PARAMETERS, {resource: resource}))
-//   //   }
-//   // })
-// )(ResourceWidgetForm)
+const ResourceWidgetParameters = connect(
+  null,
+  (dispatch) => ({
+    updateProp(formName, prop, value) {
+      dispatch(formActions.updateProp(formName, prop, value))
+    },
+    showContentParametersModal(resource) {
+      dispatch(modalActions.showModal(MODAL_WIDGET_CONTENT, {resource: resource}))
+    }
+  })
+)(ResourceWidgetForm)
 
 export {
   ResourceWidgetParameters
