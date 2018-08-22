e79ab61deecff0d8f5928a2ae5ad918243e07b9c
Pauline Ballot <ballotp@gmail.com>
2018-08-22 08:47:14 +0200

    add onChange

--- main/core/Resources/modules/widget/types/resource/components/parameters.jsx
+++ main/core/Resources/modules/widget/types/resource/components/parameters.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {PropTypes as T} from 'prop-types'
 
 import {trans} from '#/main/core/translation'
 import {FormData} from '#/main/app/content/form/containers/data'
+import {actions as modalActions} from '#/main/app/overlay/modal/store'
+import {actions as formActions} from '#/main/app/content/form/store'
+import {ResourceNode as ResourceNodeTypes} from '#/main/core/resource/data/types/resource/prop-types'
+
+import {MODAL_CONTENT_PARAMETERS} from '#/main/core/widget/content/modals/parameters'
 
 const ResourceWidgetForm = (props) =>
   <FormData
@@ -18,30 +24,41 @@ const ResourceWidgetForm = (props) =>
             name: 'parameters.resource',
             label: trans('resource'),
             type: 'resource',
-            required: true,
-            onChange: (selected) => {
-              props.updateProp(this.props.name, 'parameters.resource', selected)
-
-              if (props.modal) {
-                props.showContentParametersModal(props.workspace)
-              }
-            }
+            required: true
+            // onChange: (selected) => {
+            //   props.updateProp(props.name, 'parameters.resource', selected)
+            //   if (props.modal) {
+            //     props.showContentParametersModal(props.resource)
+            //   }
+            // }
           }
         ]
       }
     ]}
   />
 
+ResourceWidgetForm.propTypes = {
+  name: T.string.isRequired,
+  resource: T.shape(ResourceNodeTypes.propTypes),
+  updateProp: T.func.isRequired,
+  showContentParametersModal: T.func.isRequired,
+  modal: T.bool.isRequired
+}
+
+ResourceWidgetForm.defaultProps = {
+  modal: false
+}
+
 const ResourceWidgetParameters = connect(
-  null,
-  (dispatch) => ({
-    updateProp(formName, prop, value) {
-      dispatch(formActions.updateProp(formName, prop, value))
-    },
-    showContentParametersModal(workspace) {
-      dispatch(modalActions.showModal(MODAL_WORKSPACE_PARAMETERS, {workspace: workspace, workspaceLoading: false}))
-    }
-  })
+  null
+  // (dispatch) => ({
+  //   updateProp(formName, prop, value) {
+  //     dispatch(formActions.updateProp(formName, prop, value))
+  //   },
+  //   showContentParametersModal(resource) {
+  //     dispatch(modalActions.showModal(MODAL_CONTENT_PARAMETERS, {resource: resource}))
+  //   }
+  // })
 )(ResourceWidgetForm)
 
 export {
