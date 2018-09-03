45a09bd2b78d1fec17fd3210f8fa084d891cf140
Pauline Ballot <ballotp@gmail.com>
2018-09-03 17:30:27 +0200

    migrations without double quotes

--- main/core/Migrations/pdo_mysql/Version20180903171211.php
+++ main/core/Migrations/pdo_mysql/Version20180903171211.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration based on mapping information: modify it with caution
+ * Auto-generated migration based on mapping information: modify it with caution.
  *
  * Generation date: 2018/09/03 05:12:12
  */
@@ -14,17 +14,17 @@ class Version20180903171211 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
-            ALTER TABLE claro_widget_resource 
+        $this->addSql('
+            ALTER TABLE claro_widget_resource
             ADD showResourceHeader TINYINT(1) NOT NULL
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
-            ALTER TABLE claro_widget_resource 
+        $this->addSql('
+            ALTER TABLE claro_widget_resource
             DROP showResourceHeader
-        ");
+        ');
     }
 }
