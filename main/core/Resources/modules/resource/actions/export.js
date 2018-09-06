8c2fddec1ce206de799c802af6dc54fbca66d3ea
ngodfraind <nicolas.godfraind@gmail.com>
2018-09-06 18:28:17 +0200

    Use download button instead of async.

--- main/core/Resources/modules/resource/actions/export.js
+++ main/core/Resources/modules/resource/actions/export.js
@@ -1,13 +1,13 @@
 import {trans} from '#/main/core/translation'
-import {ASYNC_BUTTON} from '#/main/app/buttons'
+import {DOWNLOAD_BUTTON} from '#/main/app/buttons'
 import {url} from '#/main/app/api'
 
 const action = (resourceNodes) => ({ // todo collection
   name: 'export',
-  type: ASYNC_BUTTON,
+  type: DOWNLOAD_BUTTON,
   icon: 'fa fa-fw fa-download',
   label: trans('export', {}, 'actions'),
-  request: {
+  file: {
     url: url(
       ['claro_resource_download'],
       {ids: resourceNodes.map(resourceNode => resourceNode.id)}
