9f6edd114a15b64efedff5c0cf34f5eb92f61796
Pauline Ballot <ballotp@gmail.com>
2018-09-07 17:18:45 +0200

    single quotes in migrations

--- main/core/Migrations/pdo_mysql/Version20180907163533.php
+++ main/core/Migrations/pdo_mysql/Version20180907163533.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration based on mapping information: modify it with caution
+ * Auto-generated migration based on mapping information: modify it with caution.
  *
  * Generation date: 2018/09/07 04:35:35
  */
@@ -14,23 +14,23 @@ class Version20180907163533 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_home_tab_config CHANGE centerTitle centerTitle TINYINT(1) NOT NULL
-        ");
-        $this->addSql("
-            ALTER TABLE claro_widget_container_config 
+        ');
+        $this->addSql('
+            ALTER TABLE claro_widget_container_config
             ADD alignName VARCHAR(255) NOT NULL
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             ALTER TABLE claro_home_tab_config CHANGE centerTitle centerTitle TINYINT(1) DEFAULT NULL
-        ");
-        $this->addSql("
-            ALTER TABLE claro_widget_container_config 
+        ');
+        $this->addSql('
+            ALTER TABLE claro_widget_container_config
             DROP alignName
-        ");
+        ');
     }
 }
