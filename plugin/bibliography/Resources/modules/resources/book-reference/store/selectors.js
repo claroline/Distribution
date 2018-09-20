406f5e129ba22ffdb06b51a6f10ae95b3e0ed40f
kitan1982 <anhthao.pham@claroline.net>
2018-09-20 15:18:58 +0200

    Fixes bibliography resource

--- plugin/bibliography/Resources/modules/resources/book-reference/store/selectors.js
+++ plugin/bibliography/Resources/modules/resources/book-reference/store/selectors.js
@@ -8,19 +8,19 @@ const FORM_NAME = `${STORE_NAME}.bookReference`
 
 const resource = (state) => state[STORE_NAME]
 
-const bookReferenceForm = (state) => formSelectors.data(formSelectors.form(state, FORM_NAME))
-const bookReferenceOriginal = (state) => formSelectors.originalData(formSelectors.form(state, FORM_NAME))
+// const bookReferenceForm = (state) => formSelectors.data(formSelectors.form(state, FORM_NAME))
+// const bookReferenceOriginal = (state) => formSelectors.originalData(formSelectors.form(state, FORM_NAME))
 
-const bookReferenceId = createSelector(
-  [bookReferenceForm],
-  (bookReferenceForm) => bookReferenceForm.id
-)
+// const bookReferenceId = createSelector(
+//   [bookReferenceForm],
+//   (bookReferenceForm) => bookReferenceForm.id
+// )
 
 export const selectors = {
   STORE_NAME,
   FORM_NAME,
   resource,
-  bookReferenceId,
-  bookReferenceForm,
-  bookReferenceOriginal
+  // bookReferenceId,
+  // bookReferenceForm,
+  // bookReferenceOriginal
 }
