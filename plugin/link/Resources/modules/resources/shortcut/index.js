762fce7c703eb9f54b7f54ca7561f274f6eca4bf
kitan1982 <anhthao.pham@claroline.net>
2018-09-21 10:23:57 +0200

    Fixes Shortcut creation

---
+++ plugin/link/Resources/modules/resources/shortcut/index.js
@@ -0,0 +1,18 @@
+import {ShortcutCreation} from '#/plugin/link/resources/shortcut/components/creation'
+// import {ShorcutResource} from '#/plugin/link/resources/shortcut/containers/resource'
+
+/**
+ * Shortcut creation app.
+ */
+export const Creation = () => ({
+  component: ShortcutCreation
+})
+
+// /**
+//  * Shortcut resource application.
+//  *
+//  * @constructor
+//  */
+// export const App = () => ({
+//   component: ShortcutResource
+// })
