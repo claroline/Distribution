15c1196199ef7af05df5254639a65e56e8355e16
Gruber Laurent <laurent@sk1.be>
2018-10-15 07:43:18 +0200

    happy travis

--- main/core/Resources/modules/user/registration/components/main.jsx
+++ main/core/Resources/modules/user/registration/components/main.jsx
@@ -3,7 +3,6 @@ import {PropTypes as T} from 'prop-types'
 import {connect} from 'react-redux'
 
 import {t} from '#/main/core/translation'
-import cloneDeep from 'lodash/cloneDeep'
 
 import {PageContainer, PageHeader} from '#/main/core/layout/page/index'
 import {FormStepper} from '#/main/core/layout/form/components/form-stepper.jsx'
@@ -12,8 +11,6 @@ import {selectors as formSelect} from '#/main/app/content/form/store/selectors'
 
 import {MODAL_CONFIRM} from '#/main/app/modals/confirm'
 import {actions as modalActions} from '#/main/app/overlay/modal/store'
-
-import {formatFormSections} from '#/main/core/user/profile/utils'
 
 import {Facet} from '#/main/core/user/registration/components/facet.jsx'
 import {Required} from '#/main/core/user/registration/components/required.jsx'
