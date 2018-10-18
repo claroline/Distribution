98ac72938382836cb33bf06b20903cff61232445
kitan1982 <anhthao.pham@claroline.net>
2018-10-18 09:31:42 +0200

    temp

--- main/core/Listener/Tool/ToolListener.php
+++ main/core/Listener/Tool/ToolListener.php
@@ -13,10 +13,13 @@ namespace Claroline\CoreBundle\Listener\Tool;
 
 use Claroline\AppBundle\API\FinderProvider;
 use Claroline\AppBundle\API\SerializerProvider;
+use Claroline\AppBundle\Persistence\ObjectManager;
+use Claroline\CoreBundle\Entity\Role;
 use Claroline\CoreBundle\Entity\Tool\Tool;
 use Claroline\CoreBundle\Event\DisplayToolEvent;
 use JMS\DiExtraBundle\Annotation as DI;
 use Symfony\Bundle\TwigBundle\TwigEngine;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 /**
  * @DI\Service()
@@ -26,33 +29,51 @@ class ToolListener
     /** @var FinderProvider */
     private $finder;
 
+    /** @var ObjectManager */
+    private $om;
+
     /** @var SerializerProvider */
     private $serializer;
 
     /** @var TwigEngine */
     private $templating;
 
+    /** @var TokenStorageInterface */
+    private $tokenStorage;
+
+    private $roleRepo;
+
     /**
      * ToolListener constructor.
      *
      * @DI\InjectParams({
-     *     "finder"      = @DI\Inject("claroline.api.finder"),
-     *     "serializer"  = @DI\Inject("claroline.api.serializer"),
-     *     "templating"  = @DI\Inject("templating")
+     *     "finder"       = @DI\Inject("claroline.api.finder"),
+     *     "om"           = @DI\Inject("claroline.persistence.object_manager"),
+     *     "serializer"   = @DI\Inject("claroline.api.serializer"),
+     *     "templating"   = @DI\Inject("templating"),
+     *     "tokenStorage" = @DI\Inject("security.token_storage")
      * })
      *
-     * @param FinderProvider     $finder
-     * @param SerializerProvider $serializer
-     * @param TwigEngine         $templating
+     * @param FinderProvider        $finder
+     * @param ObjectManager         $om
+     * @param SerializerProvider    $serializer
+     * @param TwigEngine            $templating
+     * @param TokenStorageInterface $tokenStorage
      */
     public function __construct(
         FinderProvider $finder,
+        ObjectManager $om,
         SerializerProvider $serializer,
-        TwigEngine $templating
+        TwigEngine $templating,
+        TokenStorageInterface $tokenStorage
     ) {
         $this->finder = $finder;
+        $this->om = $om;
         $this->serializer = $serializer;
         $this->templating = $templating;
+        $this->tokenStorage = $tokenStorage;
+
+        $this->roleRepo = $om->getRepository('ClarolineCoreBundle:Role');
     }
 
     /**
@@ -62,6 +83,13 @@ class ToolListener
      */
     public function onDisplayDesktopParameters(DisplayToolEvent $event)
     {
+        $user = $this->tokenStorage->getToken()->getUser();
+        $roles = 'anon.' !== $user ?
+            array_filter($user->getEntityRoles(), function (Role $role) {
+                return Role::PLATFORM_ROLE === $role->getType();
+            }) :
+            [$this->roleRepo->findOneBy(['name' => 'ROLE_ANONYMOUS'])];
+
         $desktopTools = $this->finder->get(Tool::class)->find(
             ['isDisplayableInDesktop' => true],
             ['property' => 'name', 'direction' => 1]
