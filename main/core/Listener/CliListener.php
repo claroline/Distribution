2e3918f4357031fe464b2f477b65024dab5373ab
ngodfraind <nicolas.godfraind@gmail.com>
2018-10-24 15:16:50 +0200

    Fixing install

--- main/core/Listener/CliListener.php
+++ main/core/Listener/CliListener.php
@@ -11,16 +11,13 @@
 
 namespace Claroline\CoreBundle\Listener;
 
-use Claroline\CoreBundle\Library\Security\TokenUpdater;
-use Claroline\CoreBundle\Manager\RoleManager;
+use Claroline\CoreBundle\Command\AdminCliCommand;
 use Claroline\CoreBundle\Manager\UserManager;
-use Doctrine\DBAL\Exception\ConnectionException;
 use Doctrine\ORM\EntityManager;
 use JMS\DiExtraBundle\Annotation as DI;
 use Symfony\Component\Console\Event\ConsoleCommandEvent;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
-use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 
 /**
  * @DI\Service
@@ -28,34 +25,23 @@ use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
 class CliListener
 {
     private $tokenStorage;
-    private $authorization;
-    private $roleManager;
-    private $tokenUpdater;
     private $userManager;
+    private $em;
 
     /**
      * @DI\InjectParams({
-     *     "authorization" = @DI\Inject("security.authorization_checker"),
      *     "tokenStorage"  = @DI\Inject("security.token_storage"),
      *     "em"            = @DI\Inject("doctrine.orm.entity_manager"),
-     *     "roleManager"   = @DI\Inject("claroline.manager.role_manager"),
-     *     "tokenUpdater"  = @DI\Inject("claroline.security.token_updater"),
      *     "userManager"   = @DI\Inject("claroline.manager.user_manager")
      * })
      */
     public function __construct(
         TokenStorageInterface $tokenStorage,
-        AuthorizationCheckerInterface $authorization,
         EntityManager $em,
-        UserManager $userManager,
-        RoleManager $roleManager,
-        TokenUpdater $tokenUpdater
+        UserManager $userManager
     ) {
         $this->tokenStorage = $tokenStorage;
-        $this->authorization = $authorization;
         $this->em = $em;
-        $this->roleManager = $roleManager;
-        $this->tokenUpdater = $tokenUpdater;
         $this->userManager = $userManager;
     }
 
@@ -66,14 +52,14 @@ class CliListener
      *
      * @param GetResponseEvent $event
      */
-    public function setLocale(ConsoleCommandEvent $event)
+    public function setDefaultUser(ConsoleCommandEvent $event)
     {
-        try {
+        $command = $event->getCommand();
+
+        if ($command instanceof AdminCliCommand) {
             $user = $this->userManager->getDefaultClarolineAdmin();
             $token = new UsernamePasswordToken($user, null, 'main', $user->getRoles());
             $this->tokenStorage->setToken($token);
-        } catch (ConnectionException $e) {
-            //database does not exists yet
         }
     }
 }
