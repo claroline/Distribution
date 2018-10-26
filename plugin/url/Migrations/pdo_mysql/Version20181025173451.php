c86428d4d4811fe782dfce89ca978183f8b8d670
ngodfraind <nicolas.godfraind@gmail.com>
2018-10-26 15:19:52 +0200

    travis

--- plugin/url/Migrations/pdo_mysql/Version20181025173451.php
+++ plugin/url/Migrations/pdo_mysql/Version20181025173451.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration based on mapping information: modify it with caution
+ * Auto-generated migration based on mapping information: modify it with caution.
  *
  * Generation date: 2018/10/25 05:34:53
  */
@@ -14,17 +14,17 @@ class Version20181025173451 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
-            ALTER TABLE hevinci_url 
+        $this->addSql('
+            ALTER TABLE hevinci_url
             ADD mode VARCHAR(255) NOT NULL
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
-            ALTER TABLE hevinci_url 
+        $this->addSql('
+            ALTER TABLE hevinci_url
             DROP mode
-        ");
+        ');
     }
 }
--- plugin/url/Migrations/pdo_mysql/Version20181026112357.php
+++ plugin/url/Migrations/pdo_mysql/Version20181026112357.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration based on mapping information: modify it with caution
+ * Auto-generated migration based on mapping information: modify it with caution.
  *
  * Generation date: 2018/10/26 11:23:59
  */
@@ -14,17 +14,17 @@ class Version20181026112357 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
-            ALTER TABLE hevinci_url 
+        $this->addSql('
+            ALTER TABLE hevinci_url
             ADD ratio DOUBLE PRECISION DEFAULT NULL
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
-            ALTER TABLE hevinci_url 
+        $this->addSql('
+            ALTER TABLE hevinci_url
             DROP ratio
-        ");
+        ');
     }
 }
