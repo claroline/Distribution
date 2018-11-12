bee7dc4530fec1838e574b9e27f3aede75d4d953
ngodfraind <nicolas.godfraind@gmail.com>
2018-11-12 16:34:32 +0100

    [CoreBundle] Administration: technical settings can pick the admin tools.

--- main/core/Resources/modules/administration/parameters/technical/store/reducer.js
+++ main/core/Resources/modules/administration/parameters/technical/store/reducer.js
@@ -1,6 +1,7 @@
 import {makeFormReducer} from '#/main/app/content/form/store/reducer'
 
 const reducer = {
+  parameters: makeFormReducer('parameters'),
   parameters: makeFormReducer('parameters')
 }
 
