72735373cd42eb2490939300254856c773886f3c
ngodfraind <nicolas.godfraind@gmail.com>
2018-11-12 16:50:59 +0100

    oops

--- main/core/Resources/modules/administration/parameters/technical/store/reducer.js
+++ main/core/Resources/modules/administration/parameters/technical/store/reducer.js
@@ -1,7 +1,6 @@
 import {makeFormReducer} from '#/main/app/content/form/store/reducer'
 
 const reducer = {
-  parameters: makeFormReducer('parameters'),
   parameters: makeFormReducer('parameters')
 }
 
