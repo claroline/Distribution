97a391451b103b6ae78fc69b695bdeb6a37ed4a6
ngodfraind <nicolas.godfraind@gmail.com>
2018-11-19 19:51:02 +0100

    Handles tool permissions

--- main/core/API/Serializer/Workspace/OrderedToolSerializer.php
+++ main/core/API/Serializer/Workspace/OrderedToolSerializer.php
@@ -5,8 +5,10 @@ namespace Claroline\CoreBundle\API\Serializer\Workspace;
 use Claroline\AppBundle\API\Options;
 use Claroline\CoreBundle\API\Serializer\Tool\ToolSerializer;
 use Claroline\CoreBundle\API\Serializer\User\RoleSerializer;
+use Claroline\CoreBundle\Entity\Role;
 use Claroline\CoreBundle\Entity\Tool\OrderedTool;
 use Claroline\CoreBundle\Entity\Tool\Tool;
+use Claroline\CoreBundle\Entity\Tool\ToolRights;
 use Claroline\CoreBundle\Entity\Workspace\Workspace;
 use JMS\DiExtraBundle\Annotation as DI;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -48,6 +50,7 @@ class OrderedToolSerializer
     {
         $data = [
           'tool' => $orderedTool->getTool()->getName(),
+          'name' => $orderedTool->setName(),
           'position' => $orderedTool->getOrder(),
           'restrictions' => $this->serializeRestrictions($orderedTool, $options),
         ];
@@ -74,7 +77,7 @@ class OrderedToolSerializer
                 $role = $this->roleSerializer->serialize($right->getRole(), [Options::SERIALIZE_MINIMAL]);
             }
 
-            $restrictions['roles'][] = [
+            $restrictions[] = [
               'role' => $role,
               'mask' => $right->getMask(),
             ];
@@ -83,11 +86,34 @@ class OrderedToolSerializer
         return $restrictions;
     }
 
+    //only work for creation... other not supported. It's not a true Serializer anyway atm
     public function deserialize(array $data, OrderedTool $orderedTool, array $options = [], Workspace $workspace = null)
     {
+        $om = $this->container->get('claroline.persistence.object_manager');
         $orderedTool->setWorkspace($workspace);
-        $tool = $this->container->get('claroline.persistence.object_manager')->getRepository(Tool::class)->findOneByName($data['tool']);
+        $tool = $om->getRepository(Tool::class)->findOneByName($data['tool']);
         $orderedTool->setTool($tool);
+        $orderedTool->setName($data['name']);
+        $orderedTool->setOrder($data['position']);
+
+        foreach ($data['restrictions'] as $restriction) {
+            if (isset($restriction['role']['name'])) {
+                $role = $om->getRepository(Role::class)->findOneBy(['name' => $restriction['role']['name']]);
+            } else {
+                $role = $om->getRepository(Role::class)->findOneBy(
+                [
+                  'translationKey' => $restriction['role']['translationKey'],
+                  'workspace' => $workspace->getId(),
+                ]
+              );
+            }
+
+            $rights = new ToolRights();
+            $rights->setRole($role);
+            $rights->setMask($restriction['mask']);
+            $rights->setOrderedTool($orderedTool);
+            $om->persist($rights);
+        }
 
         $serviceName = 'claroline.serializer.tool.'.$orderedTool->getTool()->getName();
 
