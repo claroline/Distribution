134284c8b0f833d3ce0544d400878a02ef3ddf2f
ngodfraind <nicolas.godfraind@gmail.com>
2018-11-28 11:13:45 +0100

    Create branch

---
+++ plugin/open-badge/Serializer/EvidenceSerializer.php
@@ -0,0 +1,25 @@
+
+<?php
+
+namespace Claroline\OpenBadgeBundle\Serializer;
+
+use Claroline\AppBundle\API\Serializer\SerializerTrait;
+use JMS\DiExtraBundle\Annotation as DI;
+
+/**
+ * @DI\Service()
+ * @DI\Tag("claroline.serializer")
+ */
+class EvidenceSerializer
+{
+    use SerializerTrait;
+
+    public function __construct()
+    {
+    }
+
+    public function getClass()
+    {
+        return self::class;
+    }
+}
