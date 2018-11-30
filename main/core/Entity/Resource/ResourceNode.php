8b757c45c3644ac1f5e8ab74f1b6c7aaa158a167
ngodfraind <nicolas.godfraind@gmail.com>
2018-11-30 13:19:35 +0100

    more cleanup

--- main/core/Entity/Resource/ResourceNode.php
+++ main/core/Entity/Resource/ResourceNode.php
@@ -24,7 +24,6 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\ORM\Event\PreFlushEventArgs;
 use Doctrine\ORM\Mapping as ORM;
 use Gedmo\Mapping\Annotation as Gedmo;
-use Ramsey\Uuid\Uuid as BaseUuid;
 
 /**
  * Base entity for all resources.
@@ -39,47 +38,7 @@ class ResourceNode
     const PATH_SEPARATOR = '/';
     // identifiers
     use Id;
-
-    /**
-     * @var string
-     *
-     * @Gedmo\TreePathSource
-     * @ORM\Column("uuid", type="string", length=36, unique=true)
-     */
-    private $uuid;
-
-    /**
-     * Gets UUID.
-     *
-     * @return string
-     */
-    public function getUuid()
-    {
-        return $this->uuid;
-    }
-
-    /**
-     * Sets UUID.
-     *
-     * @param $uuid
-     */
-    public function setUuid($uuid)
-    {
-        $this->uuid = $uuid;
-    }
-
-    /**
-     * Generates a new UUID.
-     */
-    public function refreshUuid()
-    {
-        $this->uuid = $this->generateUuid();
-    }
-
-    public function generateUuid(): string
-    {
-        return BaseUuid::uuid4()->toString();
-    }
+    use Uuid;
 
     // meta
     use Thumbnail;
