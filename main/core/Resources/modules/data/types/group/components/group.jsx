aaf3a616c297015ad1c7ebf808d149b94874f6da
ngodfraind <nicolas.godfraind@gmail.com>
2019-01-08 16:29:00 +0100

    merge fix

--- main/core/Resources/modules/data/types/group/components/group.jsx
+++ main/core/Resources/modules/data/types/group/components/group.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {PropTypes as T, implementPropTypes} from '#/main/app/prop-types'
 
 import {FormGroupWithField as FormGroupWithFieldTypes} from '#/main/core/layout/form/prop-types'
-import {FormGroup} from '#/main/core/layout/form/components/group/form-group'
+import {FormGroup} from '#/main/app/content/form/components/group'
 import {Group as GroupType} from '#/main/core/user/prop-types'
 import {GroupInput} from '#/main/core/data/types/group/components/input'
 
--- plugin/open-badge/Resources/modules/tools/badges/data/types/badge/components/group.jsx
+++ plugin/open-badge/Resources/modules/tools/badges/data/types/badge/components/group.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import {PropTypes as T, implementPropTypes} from '#/main/app/prop-types'
 
 import {FormGroupWithField as FormGroupWithFieldTypes} from '#/main/core/layout/form/prop-types'
-import {FormGroup} from '#/main/core/layout/form/components/group/form-group'
+import {FormGroup} from '#/main/app/content/form/components/group'
 import {Badge as BadgeType} from '#/plugin/open-badge/tools/badges/prop-types'
 import {BadgeInput} from '#/plugin/open-badge/tools/badges/data/types/badge/components/input'
 
