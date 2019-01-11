a227f371cbb2177e56cea706fd8c17e080f41ff4
ngodfraind <nicolas.godfraind@gmail.com>
2019-01-11 10:12:00 +0100

    typo for test fix

--- main/core/Converter/CurrentUserConverter.php
+++ main/core/Converter/CurrentUserConverter.php
@@ -57,7 +57,7 @@ class CurrentUserConverter implements ParamConverterInterface
         // Check whether we need to let pass anonymous
         $allowAnonymous = false;
         $options = $configuration->getOptions();
-        if ($options && isset($options['$allowAnonymous']) && true === $options['allowAnonymous']) {
+        if ($options && isset($options['allowAnonymous']) && true === $options['allowAnonymous']) {
             $allowAnonymous = true;
         }
 
