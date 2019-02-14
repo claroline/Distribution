9eb0b082f7c433dd85bcffe80fb5ae843c18fa73
Axel Penin <Elorfin@users.noreply.github.com>
2019-02-14 18:55:06 +0100

    Update plugin.js

--- plugin/rss/Resources/modules/plugin.js
+++ plugin/rss/Resources/modules/plugin.js
@@ -2,7 +2,7 @@
 
 import {registry} from '#/main/app/plugins/registry'
 
-registry.add('rss', {
+registry.add('ClarolineRssBundle', {
   resources: {
     'rss_feed': () => { return import(/* webpackChunkName: "plugin-rss-rss-feed-resource" */ '#/plugin/rss/resources/rss-feed') }
   }
