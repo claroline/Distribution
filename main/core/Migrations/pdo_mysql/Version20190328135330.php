bd76566c03d78132e9f88b10e9cba504f65d11f3
ngodfraind <nicolas.godfraind@gmail.com>
2019-03-28 16:28:31 +0100

    Travis

--- main/core/Migrations/pdo_mysql/Version20190328135330.php
+++ main/core/Migrations/pdo_mysql/Version20190328135330.php
@@ -6,7 +6,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 
 /**
- * Auto-generated migration based on mapping information: modify it with caution
+ * Auto-generated migration based on mapping information: modify it with caution.
  *
  * Generation date: 2019/03/28 01:53:31
  */
@@ -14,17 +14,17 @@ class Version20190328135330 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
-            ALTER TABLE claro_user 
+        $this->addSql('
+            ALTER TABLE claro_user
             ADD code VARCHAR(255) DEFAULT NULL
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
-            ALTER TABLE claro_user 
+        $this->addSql('
+            ALTER TABLE claro_user
             DROP code
-        ");
+        ');
     }
 }
