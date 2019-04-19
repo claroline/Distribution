3b5a2f884ee3d0d78206f1815075bd107beb8cea
ngodfraind <nicolas.godfraind@gmail.com>
2019-04-19 13:01:29 +0200

    Archiver defaut first year.

--- main/core/Command/Logs/LogsArchiverCommand.php
+++ main/core/Command/Logs/LogsArchiverCommand.php
@@ -11,51 +11,60 @@
 
 namespace Claroline\CoreBundle\Command\Logs;
 
-use Claroline\AppBundle\Command\BaseCommandTrait;
 use Claroline\CoreBundle\Entity\Log\Log;
 use Claroline\CoreBundle\Library\Logger\ConsoleLogger;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class LogsArchiverCommand extends ContainerAwareCommand
 {
-    use BaseCommandTrait;
-
-    private $params = [
-        'from' => 'from',
-        'to' => 'to',
-    ];
-
     protected function configure()
     {
         parent::configure();
 
         $this->setName('claroline:logs:archive')
             ->setDescription('Archive logs');
-        $this->setDefinition(
-            [
-                new InputArgument('from', InputArgument::REQUIRED, 'from date'),
-                new InputArgument('to', InputArgument::REQUIRED, 'to date'),
-            ]
-        );
         $this->addOption(
             'keep',
             'k',
             InputOption::VALUE_NONE,
             'When set to true, keep the archived logs (format: m-d-Y)'
         );
+        $this->addOption(
+            'from',
+            'f',
+            InputOption::VALUE_REQUIRED,
+            'From date (format: m-d-Y)'
+        );
+        $this->addOption(
+            'to',
+            't',
+            InputOption::VALUE_REQUIRED,
+            'To date (format: m-d-Y)'
+        );
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $from = \DateTime::createFromFormat('m-d-Y', $input->getArgument('from'));
-        $to = \DateTime::createFromFormat('m-d-Y', $input->getArgument('to'));
+        $from = $input->getOption('from');
+        $to = $input->getOption('to');
+
+        if ($from) {
+            $from = \DateTime::createFromFormat('m-d-Y', $from);
+        } else {
+            $from = $this->getContainer()->get('claroline.persistence.object_manager')->getRepository(Log::class)->findBy([], null, 1)[0]->getDateLog();
+        }
+
+        $searches = ['dateLog' => $from->format('Y-m-d h:i:s')];
+
+        if ($to) {
+            $to = \DateTime::createFromFormat('m-d-Y', $to);
+            $searches['dateTo'] = $to->format('Y-m-d h:i:s');
+        }
 
         $consoleLogger = ConsoleLogger::get($output);
-
         $databaseManager = $this->getContainer()->get('claroline.manager.database_manager');
         $databaseManager->setLogger($consoleLogger);
 
@@ -63,15 +72,17 @@ class LogsArchiverCommand extends ContainerAwareCommand
             'claro_log',
         ];
 
+        $searches = [];
+
         $delete = $input->getOption('keep') ? false : true;
         $archiveDir = $this->getContainer()->getParameter('claroline.param.archive_directory');
 
         foreach ($logTables as $table) {
-            $name = str_replace('-', '_', $input->getArgument('from')).'_'.uniqid();
+            $name = str_replace('-', '_', $from->format('Y')).'_'.uniqid();
 
             $databaseManager->backupRows(
                 Log::class,
-                ['dateLog' => $from->format('Y-m-d h:i:s'), 'dateTo' => $to->format('Y-m-d h:i:s')],
+                $searches,
                 $table,
                 $name,
                 $delete
