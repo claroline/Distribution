b273a74b2ecb92e28c6fd9926d767482363afd20
ngodfraind <nicolas.godfraind@gmail.com>
2019-05-02 17:22:47 +0200

    fix

--- plugin/scorm/Listener/ScormListener.php
+++ plugin/scorm/Listener/ScormListener.php
@@ -214,9 +214,8 @@ class ScormListener
         $workspace = $event->getWorkspace();
 
         if ($bag) {
-            $file = new File($bag->get($data['_path']));
-
             try {
+                $file = new File($bag->get($data['_path']));
                 $this->scormManager->parseScormArchive($file);
                 $this->scormManager->unzipScormArchive($workspace, $file, $data['hashName']);
             } catch (\Exception $e) {
