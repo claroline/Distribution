52e3a82ee1923e2d2ca939c86e9a38c2532c0354
kitan1982 <anhthao.pham@claroline.net>
2019-05-17 22:05:10 +0200

    Allows specific editor by type for file resource

--- main/core/Resources/modules/resources/file/editor/components/editor.jsx
+++ main/core/Resources/modules/resources/file/editor/components/editor.jsx
@@ -1,10 +1,16 @@
 import React, {Component} from 'react'
 import {PropTypes as T} from 'prop-types'
 import {connect} from 'react-redux'
+import get from 'lodash/get'
 
+import {theme} from '#/main/app/config'
 import {trans} from '#/main/app/intl/translation'
 import {LINK_BUTTON} from '#/main/app/buttons'
 import {FormData} from '#/main/app/content/form/containers/data'
+import {FormSections, FormSection} from '#/main/app/content/form/components/sections'
+import {Await} from '#/main/app/components/await'
+
+import {getFile, getTypeName} from '#/main/core/files'
 import {selectors} from '#/main/core/resources/file/editor/store/selectors'
 
 // TODO : find a way to make this kind of component generic (duplicated for all apps coming from dynamic loading)
@@ -49,21 +55,35 @@ class Editor extends Component {
                 label: trans('activate_comments'),
                 type: 'boolean',
                 displayed: -1 < this.props.mimeType.indexOf('video')
-              }, {
-                name: 'sectionCommentsAllowed',
-                label: trans('activate_section_comments', {}, 'audio'),
-                type: 'boolean',
-                displayed: -1 < this.props.mimeType.indexOf('audio')
-              }, {
-                name: 'rateControl',
-                label: trans('activate_rate_control', {}, 'audio'),
-                type: 'boolean',
-                displayed: -1 < this.props.mimeType.indexOf('audio')
               }
             ]
           }
         ]}
       >
+        <Await
+          for={getFile(this.props.mimeType)}
+          then={module => {
+            if (get(module, 'fileType.components.editor')) {
+              return (
+                <FormSections level={3}>
+                  <FormSection
+                    className="embedded-list-section"
+                    title={trans(getTypeName(this.props.mimeType))}
+                  >
+                    {React.createElement(get(module, 'fileType.components.editor'), {
+                      file: this.props.file
+                    })}
+
+                    {get(module, 'fileType.styles') &&
+                      <link rel="stylesheet" type="text/css" href={theme(get(module, 'fileType.styles'))} />
+                    }
+                  </FormSection>
+                </FormSections>
+              )
+            }
+            return null
+          }}
+        />
       </FormData>
     )
   }
