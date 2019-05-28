73b6204563cc9bca98c5e51914dea5dd34f2f0fb
kitan1982 <anhthao.pham@claroline.net>
2019-05-28 14:01:55 +0200

    Keeps sections options in memory in audio player

--- plugin/audio-player/Resources/modules/files/audio/components/player.jsx
+++ plugin/audio-player/Resources/modules/files/audio/components/player.jsx
@@ -40,135 +40,126 @@ Transcripts.propTypes = {
   transcripts: T.arrayOf(T.string)
 }
 
-class Section extends Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      showHelp: false,
-      showComment: constants.USER_TYPE === props.section.type,
-      showCommentForm: false,
-      showAudioUrl: false
-    }
-  }
-
-  render() {
-    return (
-      <div className="audio-player-section">
-        <div className="section-controls">
-          {this.props.section.showHelp &&
-            <CallbackButton
-              className={classes('btn section-btn', {'activated': this.state.showHelp})}
-              callback={() => this.setState({showHelp: !this.state.showHelp})}
-            >
-              <span className="fa fa-file-text-o"/>
-            </CallbackButton>
-          }
-          {this.props.section.commentsAllowed &&
-            <CallbackButton
-              className={classes('btn section-btn', {'activated': this.state.showComment})}
-              callback={() => this.setState({showComment: !this.state.showComment})}
-            >
-              <span className="fa fa-comment-alt"/>
-            </CallbackButton>
-          }
-          {this.props.section.showAudio && this.props.section.audioUrl &&
-            <CallbackButton
-              className={classes('btn section-btn', {'activated': this.state.showAudioUrl})}
-              callback={() => this.setState({showAudioUrl: !this.state.showAudioUrl})}
-            >
-              <span className="fa fa-volume-up"/>
-            </CallbackButton>
-          }
-          {constants.USER_TYPE === this.props.section.type && authenticatedUser &&
-            <CallbackButton
-              className="btn section-btn"
-              callback={() => this.props.deleteSection()}
-              dangerous={true}
-            >
-              <span className="fa fa-trash-o"/>
-            </CallbackButton>
-          }
-        </div>
-        <div className="section-display">
-          {this.state.showHelp &&
-            <HtmlText className="section-help">
-              {this.props.section.help}
-            </HtmlText>
-          }
-          {this.state.showComment && (!this.props.section.comment || this.state.showCommentForm ?
-            <UserMessageForm
-              user={authenticatedUser}
-              content={this.props.section.comment ? this.props.section.comment.content : ''}
-              allowHtml={true}
-              submitLabel={trans('add_comment')}
-              submit={(content) => {
-                const comment = {
-                  content: content,
-                  meta: {
-                    user: authenticatedUser,
-                    section: this.props.section
-                  }
-                }
-
-                if (this.props.section.comment) {
-                  comment['id'] = this.props.section.comment.id
-                }
-                this.props.saveComment(comment)
-                this.setState({showCommentForm: false})
-              }}
-              cancel={() => this.setState({showCommentForm: false})}
-            /> :
-            <UserMessage
-              user={this.props.section.comment && this.props.section.comment.meta && this.props.section.comment.meta.user ?
-                this.props.section.comment.meta.user :
-                undefined
+const Section = props =>
+  <div className="audio-player-section">
+    <div className="section-controls">
+      {props.section.showHelp &&
+        <CallbackButton
+          className={classes('btn section-btn', {'activated': props.options.showHelp})}
+          callback={() => props.updateOption(props.section.id, 'showHelp', !props.options.showHelp)}
+        >
+          <span className="fa fa-file-text-o"/>
+        </CallbackButton>
+      }
+      {props.section.commentsAllowed &&
+        <CallbackButton
+          className={classes('btn section-btn', {'activated': props.options.showComment})}
+          callback={() => props.updateOption(props.section.id, 'showComment', !props.options.showComment)}
+        >
+          <span className="fa fa-comment-alt"/>
+        </CallbackButton>
+      }
+      {props.section.showAudio && props.section.audioUrl &&
+        <CallbackButton
+          className={classes('btn section-btn', {'activated': props.options.showAudioUrl})}
+          callback={() => props.updateOption(props.section.id, 'showAudioUrl', !props.options.showAudioUrl)}
+        >
+          <span className="fa fa-volume-up"/>
+        </CallbackButton>
+      }
+      {constants.USER_TYPE === props.section.type && authenticatedUser &&
+        <CallbackButton
+          className="btn section-btn"
+          callback={() => props.deleteSection()}
+          dangerous={true}
+        >
+          <span className="fa fa-trash-o"/>
+        </CallbackButton>
+      }
+    </div>
+    <div className="section-display">
+      {props.options.showHelp &&
+        <HtmlText className="section-help">
+          {props.section.help}
+        </HtmlText>
+      }
+      {props.options.showComment && (!props.section.comment || props.options.showCommentForm ?
+        <UserMessageForm
+          user={authenticatedUser}
+          content={props.section.comment ? props.section.comment.content : ''}
+          allowHtml={true}
+          submitLabel={trans('add_comment')}
+          submit={(content) => {
+            const comment = {
+              content: content,
+              meta: {
+                user: authenticatedUser,
+                section: props.section
               }
-              date={this.props.section.comment && this.props.section.comment.meta ? this.props.section.comment.meta.creationDate : ''}
-              content={this.props.section.comment ? this.props.section.comment.content : ''}
-              allowHtml={true}
-              actions={[
-                {
-                  icon: 'fa fa-fw fa-pencil',
-                  type: CALLBACK_BUTTON,
-                  label: trans('edit'),
-                  displayed: true,
-                  callback: () => this.setState({showCommentForm: true})
-                }, {
-                  icon: 'fa fa-fw fa-trash-o',
-                  type: CALLBACK_BUTTON,
-                  label: trans('delete'),
-                  displayed: true,
-                  callback: () => this.props.section.comment && this.props.section.comment.id ?
-                    this.props.deleteComment(this.props.section.comment.id) :
-                    false,
-                  dangerous: true
-                }
-              ]}
-            />
-          )}
-          {this.state.showAudioUrl && this.props.section.audioUrl &&
-            <div>
-              <audio
-                controls
-              >
-                <source
-                  src={asset(this.props.section.audioUrl)}
-                />
-              </audio>
-            </div>
+            }
+
+            if (props.section.comment) {
+              comment['id'] = props.section.comment.id
+            }
+            props.saveComment(comment)
+            props.updateOption(props.section.id, 'showCommentForm', false)
+          }}
+          cancel={() => props.updateOption(props.section.id, 'showCommentForm', false)}
+        /> :
+        <UserMessage
+          user={props.section.comment && props.section.comment.meta && props.section.comment.meta.user ?
+            props.section.comment.meta.user :
+            undefined
           }
+          date={props.section.comment && props.section.comment.meta ? props.section.comment.meta.creationDate : ''}
+          content={props.section.comment ? props.section.comment.content : ''}
+          allowHtml={true}
+          actions={[
+            {
+              icon: 'fa fa-fw fa-pencil',
+              type: CALLBACK_BUTTON,
+              label: trans('edit'),
+              displayed: true,
+              callback: () => props.updateOption(props.section.id, 'showCommentForm', true)
+            }, {
+              icon: 'fa fa-fw fa-trash-o',
+              type: CALLBACK_BUTTON,
+              label: trans('delete'),
+              displayed: true,
+              callback: () => props.section.comment && props.section.comment.id ?
+                props.deleteComment(props.section.comment.id) :
+                false,
+              dangerous: true
+            }
+          ]}
+        />
+      )}
+      {props.options.showAudioUrl && props.section.audioUrl &&
+        <div>
+          <audio
+            controls
+          >
+            <source
+              src={asset(props.section.audioUrl)}
+            />
+          </audio>
         </div>
-      </div>
-    )
-  }
-}
+      }
+    </div>
+  </div>
 
 Section.propTypes = {
   section: T.shape(SectionType.propTypes),
+  options: T.shape({
+    showHelp: T.bool,
+    showComment: T.bool,
+    showCommentForm: T.bool,
+    showAudioUrl: T.bool
+  }),
   deleteSection: T.func.isRequired,
   saveComment: T.func.isRequired,
-  deleteComment: T.func.isRequired
+  deleteComment: T.func.isRequired,
+  updateOption: T.func.isRequired
 }
 
 const Sections = props =>
@@ -177,18 +168,22 @@ const Sections = props =>
       <Section
         key={`section-${section.id}`}
         section={section}
+        options={props.options[section.id]}
         deleteSection={() => props.deleteSection(section.id)}
         saveComment={(comment) => props.saveComment(section.id, comment)}
         deleteComment={(commentId) => props.deleteComment(section.id, commentId)}
+        updateOption={props.updateOption}
       />
     )}
   </div>
 
 Sections.propTypes = {
   sections: T.arrayOf(T.shape(SectionType.propTypes)),
+  options: T.object.isRequired,
   deleteSection: T.func.isRequired,
   saveComment: T.func.isRequired,
-  deleteComment: T.func.isRequired
+  deleteComment: T.func.isRequired,
+  updateOption: T.func.isRequired
 }
 
 class Audio extends Component {
@@ -196,6 +191,16 @@ class Audio extends Component {
     super(props)
 
     this.state = {
+      sectionsOptions: props.file.sections.reduce((acc, section) => {
+        acc[section.id] = {
+          showHelp: false,
+          showComment: constants.USER_TYPE === section.type,
+          showCommentForm: false,
+          showAudioUrl: false
+        }
+
+        return acc
+      }, {}),
       ongoingSections: []
     }
   }
@@ -261,8 +266,10 @@ class Audio extends Component {
                     end: end
                   })
                 } else {
+                  const newId = makeId()
+
                   newSection = Object.assign({}, SectionType.defaultProps, {
-                    id: makeId(),
+                    id: newId,
                     regionId: region.id,
                     start: start,
                     end: end,
@@ -274,6 +281,15 @@ class Audio extends Component {
                     }
                   })
                   isNew = true
+
+                  const newOptions = cloneDeep(this.state.sectionsOptions)
+                  newOptions[newId] = {
+                    showHelp: false,
+                    showComment: constants.USER_TYPE === this.props.file.sectionsType,
+                    showCommentForm: false,
+                    showAudioUrl: false
+                  }
+                  this.setState({sectionsOptions: newOptions})
                 }
                 this.props.saveSection(this.props.file.sections, newSection, isNew)
               }
@@ -286,6 +302,21 @@ class Audio extends Component {
             deleteSection={(sectionId) => this.props.deleteSection(this.props.file.sections, sectionId)}
             saveComment={(sectionId, comment) => this.props.saveComment(this.props.file.sections, sectionId, comment)}
             deleteComment={(sectionId, commentId) => this.props.deleteComment(this.props.file.sections, sectionId, commentId)}
+            options={this.state.sectionsOptions}
+            updateOption={(sectionId, prop, value) => {
+              const newOptions = cloneDeep(this.state.sectionsOptions)
+
+              if (!newOptions[sectionId]) {
+                newOptions[sectionId] = {
+                  showHelp: false,
+                  showComment: constants.USER_TYPE === this.props.file.sectionsType,
+                  showCommentForm: false,
+                  showAudioUrl: false
+                }
+              }
+              newOptions[sectionId][prop] = value
+              this.setState({sectionsOptions: newOptions})
+            }}
           />
         }
       </div>
