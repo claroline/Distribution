b618c778e670aa9e7691002a141100973749d9c7
ngodfraind <nicolas.godfraind@gmail.com>
2019-06-11 14:49:02 +0200

    First  draft

---
+++ main/core/Security/Authentication/ApiTokenAutenticator.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Claroline\CoreBundle\Security\Authentication;
+
+use Claroline\AppBundle\Persistence\ObjectManager;
+use Claroline\CoreBundle\Entity\Cryptography\ApiToken;
+use JMS\DiExtraBundle\Annotation as DI;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authentication\Token\PreAuthenticatedToken;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Http\Authentication\SimplePreAuthenticatorInterface;
+
+/**
+ * Manages authentication of users with api tokens.
+ *
+ * @DI\Service("claroline.security.authentication.apitoken_authenticator")
+ */
+class ApiTokenAuthenticator implements SimplePreAuthenticatorInterface
+{
+    private $om;
+
+    /**
+     * @DI\InjectParams({
+     *     "om" = @DI\Inject("claroline.persistence.object_manager"),
+     * })
+     *
+     * @param ObjectManager $om
+     */
+    public function inject(ObjectManager $om)
+    {
+        $this->om = $om;
+    }
+
+    public function supportsToken(TokenInterface $token, $providerKey)
+    {
+        return $token instanceof PreAuthenticatedToken && $token->getProviderKey() === $providerKey;
+    }
+
+    public function createToken(Request $request, $providerKey)
+    {
+        $apiKey = $request->query->get('apitoken');
+        //skip if unavailable
+
+        if (!$apiKey) {
+            return null;
+        }
+
+        // initialize a new token for the user
+        return new PreAuthenticatedToken(
+            'anon.',
+            $apiKey,
+            $providerKey
+        );
+    }
+
+    public function authenticateToken(TokenInterface $token, UserProviderInterface $userProvider, $providerKey)
+    {
+        $apiKey = $token->getCredentials();
+        $user = $this->om->getRepository(ApiToken::class)->findOneByToken($apiKey)->getUser();
+
+        if (!$user) {
+            throw new AuthenticationException(
+                sprintf('No user found for api token "%s"', $apiKey)
+            );
+        }
+
+        return new PreAuthenticatedToken(
+            $user,
+            $apiKey,
+            $providerKey,
+            $user->getRoles()
+        );
+    }
+}
