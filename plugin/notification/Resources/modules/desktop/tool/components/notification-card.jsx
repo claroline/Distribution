baa3441d53c998dd600cf085b52e25c2333c5ad2
ngodfraind <nicolas.godfraind@gmail.com>
2019-07-08 14:09:29 +0200

    travis

--- plugin/notification/Resources/modules/desktop/tool/components/notification-card.jsx
+++ plugin/notification/Resources/modules/desktop/tool/components/notification-card.jsx
@@ -2,17 +2,13 @@ import React from 'react'
 import {PropTypes as T} from 'prop-types'
 
 import {trans} from '#/main/app/intl/translation'
-import {asset} from '#/main/app/config/asset'
 import {displayDate} from '#/main/app/intl/date'
 
 import {DataCard} from '#/main/app/content/card/components/data'
 import {UserAvatar} from '#/main/core/user/components/avatar'
 import {User as UserTypes} from '#/main/core/user/prop-types'
 
-const Notification = props => <div dangerouslySetInnerHTML={{__html: props.notification.text}}/>
-
 const NotificationCard = props => {
-  console.log(props)
   return(
     <DataCard
       {...props}
