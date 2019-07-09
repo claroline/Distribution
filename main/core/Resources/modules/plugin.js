411a291ab7622f00b72b859d3a7d11d450e82225
ngodfraind <nicolas.godfraind@gmail.com>
2019-07-09 14:33:46 +0200

    chunk name

--- main/core/Resources/modules/plugin.js
+++ main/core/Resources/modules/plugin.js
@@ -95,7 +95,7 @@ registry.add('ClarolineCoreBundle', {
     'home'            :   () => { return import(/* webpackChunkName: "core-tool-home" */       '#/main/core/tools/home') },
     'workspaces'      :   () => { return import(/* webpackChunkName: "core-tool-workspaces" */ '#/main/core/tools/workspaces') },
     'resource_manager':   () => { return import(/* webpackChunkName: "core-tool-resources" */  '#/main/core/tools/resources') },
-    'parameters': () => { return import(/* webpackChunkName: "core-tool-resources" */  '#/main/core/tools/desktop-parameters') }
+    'parameters'      :   () => { return import(/* webpackChunkName: "core-tool-parameters" */  '#/main/core/tools/desktop-parameters') }
   },
 
   /**
