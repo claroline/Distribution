dce020d090aec83ac817757e8f7ddf4c44711008
ngodfraind <nicolas.godfraind@gmail.com>
2019-07-10 13:17:03 +0200

    Fixes

--- plugin/notification/Resources/modules/desktop/tool/components/notifications.jsx
+++ plugin/notification/Resources/modules/desktop/tool/components/notifications.jsx
@@ -2,41 +2,31 @@ import {trans} from '#/main/app/intl/translation'
 import React from 'react'
 
 import {ListData} from '#/main/app/content/list/containers/data'
-import {
-  PageContainer,
-  PageHeader,
-  PageContent
-} from '#/main/core/layout/page'
-
+import {ToolPage} from '#/main/core/tool/containers/page'
 
 import {constants as listConstants} from '#/main/app/content/list/constants'
 import {NotificationCard} from '#/plugin/notification/desktop/tool/components/notification-card'
 
 const List = () =>
-  <PageContainer>
-    <PageHeader
-      title={trans('notifications', {}, 'tools')}
+  <ToolPage>
+    <ListData
+      name="notification.notifications"
+      fetch={{
+        url: ['apiv2_workspace_list_notifications_current'],
+        autoload: true
+      }}
+      display={{
+        available: [listConstants.DISPLAY_LIST],
+        current: listConstants.DISPLAY_LIST
+      }}
+      definition={[{
+        name: 'text',
+        label: trans('text'),
+        displayed: true
+      }]}
+      card={(row) => <NotificationCard {...row}/>}
     />
-    <PageContent>
-      <ListData
-        name="notification.notifications"
-        fetch={{
-          url: ['apiv2_workspace_list_notifications_current'],
-          autoload: true
-        }}
-        display={{
-          available: [listConstants.DISPLAY_LIST],
-          current: listConstants.DISPLAY_LIST
-        }}
-        definition={[{
-          name: 'text',
-          label: trans('text'),
-          displayed: true
-        }]}
-        card={(row) => <NotificationCard {...row}/>}
-      />
-    </PageContent>
-  </PageContainer>
+  </ToolPage>
 
 export {
   List
--- plugin/notification/Resources/modules/desktop/tool/index.js
+++ plugin/notification/Resources/modules/desktop/tool/index.js
@@ -1,16 +1,6 @@
 import {reducer} from '#/plugin/notification/desktop/tool/store/reducer'
 import {List} from '#/plugin/notification/desktop/tool/components/notifications'
 
-/**
- * Notification application.
- *
- * @constructor
- */
-export const App = () => ({
-  component: List,
-  styles: []
-})
-
 export default {
   component: List,
   store: reducer,
