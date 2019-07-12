f1e1f3e56972e231ab8b51806e2298c073119d2c
kitan1982 <anhthao.pham@claroline.net>
2019-07-12 11:48:28 +0200

    Refactors menu for blocked & flagged messages

---
+++ plugin/forum/Resources/modules/resources/forum/containers/menu.jsx
@@ -0,0 +1,9 @@
+import {withRouter} from '#/main/app/router'
+
+import {ForumMenu as ForumMenuComponent} from '#/plugin/forum/resources/forum/components/menu'
+
+const ForumMenu = withRouter(ForumMenuComponent)
+
+export {
+  ForumMenu
+}
--- plugin/forum/Resources/modules/resources/forum/moderation/components/flagged.jsx
+++ plugin/forum/Resources/modules/resources/forum/moderation/components/flagged.jsx
@@ -6,31 +6,20 @@ import {trans} from '#/main/app/intl/translation'
 
 import {FlaggedMessages} from '#/plugin/forum/resources/forum/moderation/components/flagged-messages'
 import {FlaggedSubjects} from '#/plugin/forum/resources/forum/moderation/components/flagged-subjects'
-import {FlaggedPostsNav} from '#/plugin/forum/resources/forum/moderation/components/flagged-posts-nav'
 
 const Flagged = (props) =>
-  <div>
-    <h2>{trans('flagged_messages_subjects', {}, 'forum')}</h2>
-    <div className="row">
-      <div className="col-md-3">
-        <FlaggedPostsNav path={props.path} />
-      </div>
-      <div className="col-md-9">
-        <Routes
-          path={props.path}
-          routes={[
-            {
-              path: '/moderation/flagged/subjects',
-              component: FlaggedSubjects
-            }, {
-              path: '/moderation/flagged/messages',
-              component: FlaggedMessages
-            }
-          ]}
-        />
-      </div>
-    </div>
-  </div>
+  <Routes
+    path={props.path}
+    routes={[
+      {
+        path: '/moderation/flagged/subjects',
+        component: FlaggedSubjects
+      }, {
+        path: '/moderation/flagged/messages',
+        component: FlaggedMessages
+      }
+    ]}
+  />
 
 Flagged.propTypes = {
   path: T.string.isRequired
