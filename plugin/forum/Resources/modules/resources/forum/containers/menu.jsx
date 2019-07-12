77181ff10cb90806c2d7aa4e996ae7c6f844a7e3
kitan1982 <anhthao.pham@claroline.net>
2019-07-12 16:02:22 +0200

    Restores old menu for blocked & flagged messages

--- plugin/forum/Resources/modules/resources/forum/containers/menu.jsx
+++
@@ -1,9 +0,0 @@
-import {withRouter} from '#/main/app/router'
-
-import {ForumMenu as ForumMenuComponent} from '#/plugin/forum/resources/forum/components/menu'
-
-const ForumMenu = withRouter(ForumMenuComponent)
-
-export {
-  ForumMenu
-}
--- plugin/forum/Resources/modules/resources/forum/index.js
+++ plugin/forum/Resources/modules/resources/forum/index.js
@@ -1,13 +1,11 @@
 import {reducer} from '#/plugin/forum/resources/forum/store'
 import {ForumResource} from '#/plugin/forum/resources/forum/containers/resource'
-import {ForumMenu} from '#/plugin/forum/resources/forum/containers/menu'
 
 /**
  * Forum resource application.
  */
 export default {
   component: ForumResource,
-  menu: ForumMenu,
   store: reducer,
   styles: ['claroline-distribution-plugin-forum-forum-resource']
 }
