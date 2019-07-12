2096d0bac71be5b1a4473cc91f163f9521f0233a
kitan1982 <anhthao.pham@claroline.net>
2019-07-12 11:21:30 +0200

    Refactors forum for SPA

--- plugin/forum/Resources/modules/resources/forum/moderation/components/flagged.jsx
+++ plugin/forum/Resources/modules/resources/forum/moderation/components/flagged.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {PropTypes as T} from 'prop-types'
 
 import {Routes} from '#/main/app/router'
 import {trans} from '#/main/app/intl/translation'
@@ -7,15 +8,16 @@ import {FlaggedMessages} from '#/plugin/forum/resources/forum/moderation/compone
 import {FlaggedSubjects} from '#/plugin/forum/resources/forum/moderation/components/flagged-subjects'
 import {FlaggedPostsNav} from '#/plugin/forum/resources/forum/moderation/components/flagged-posts-nav'
 
-const Flagged = () =>
+const Flagged = (props) =>
   <div>
     <h2>{trans('flagged_messages_subjects', {}, 'forum')}</h2>
     <div className="row">
       <div className="col-md-3">
-        <FlaggedPostsNav />
+        <FlaggedPostsNav path={props.path} />
       </div>
       <div className="col-md-9">
         <Routes
+          path={props.path}
           routes={[
             {
               path: '/moderation/flagged/subjects',
@@ -29,6 +31,10 @@ const Flagged = () =>
       </div>
     </div>
   </div>
+
+Flagged.propTypes = {
+  path: T.string.isRequired
+}
 
 export {
   Flagged
