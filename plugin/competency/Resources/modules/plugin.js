bd28d7911c7ab26778d0ef4bf67596adbfd0c8f9
kitan1982 <anhthao.pham@claroline.net>
2019-07-16 11:26:27 +0200

    Refactors competencies admin tool for SPA

--- plugin/competency/Resources/modules/plugin.js
+++ plugin/competency/Resources/modules/plugin.js
@@ -7,11 +7,14 @@ registry.add('HeVinciCompetencyBundle', {
     types: {
       'competency_scale' : () => { return import(/* webpackChunkName: "plugin-competency-data-scale" */   '#/plugin/competency/data/types/scale') },
       'ability'          : () => { return import(/* webpackChunkName: "plugin-competency-data-ability" */ '#/plugin/competency/data/types/ability') }
-    },
+    }
   },
   actions: {
     resource: {
       'manage_competencies': () => { return import(/* webpackChunkName: "competency-action-resource-manage-competencies" */ '#/plugin/competency/resource/actions/manage-competencies') }
     }
+  },
+  administration: {
+    'competencies' : () => { return import(/* webpackChunkName: "plugin-competency-admin-competency" */ '#/plugin/competency/administration/competency') }
   }
 })
