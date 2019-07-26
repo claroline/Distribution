16acae4987e967716d3efddd00c6735bf13a6fb0
kitan1982 <anhthao.pham@claroline.net>
2019-07-26 17:14:48 +0200

    Refactors WS users tool for SPA

---
+++ main/core/Resources/modules/tools/users/containers/menu.jsx
@@ -0,0 +1,18 @@
+import {connect} from 'react-redux'
+
+import {withRouter} from '#/main/app/router'
+import {selectors as securitySelectors} from '#/main/app/security/store'
+
+import {selectors as toolSelectors} from '#/main/core/tool/store'
+import {UsersMenu as UsersMenuComponent} from '#/main/core/tools/users/components/menu'
+
+const UsersMenu = connect(
+  (state) => ({
+    currentUser: securitySelectors.currentUser(state),
+    workspace: toolSelectors.contextData(state)
+  })
+)(UsersMenuComponent)
+
+export {
+  UsersMenu
+}
