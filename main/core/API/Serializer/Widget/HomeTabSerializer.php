c49fc85e0a9c053930c178eae8b9c8581e00ba3f
ngodfraind <nicolas.godfraind@gmail.com>
2019-08-08 14:50:55 +0200

    [CoreBundle] HomeTabs urls slugified.

--- main/core/API/Serializer/Widget/HomeTabSerializer.php
+++ main/core/API/Serializer/Widget/HomeTabSerializer.php
@@ -119,6 +119,7 @@ class HomeTabSerializer
         $data = [
             'id' => $homeTab->getUuid(),
             'title' => $homeTabConfig->getName(),
+            'slug' => $homeTabConfig->getName() ? strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $homeTabConfig->getName()))) : 'new',
             'longTitle' => $homeTabConfig->getLongTitle(),
             'centerTitle' => $homeTabConfig->isCenterTitle(),
             'poster' => $poster,
