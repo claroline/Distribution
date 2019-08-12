b15780e52f0666f5767953e3223eda230d1216d3
ngodfraind <nicolas.godfraind@gmail.com>
2019-08-12 21:45:56 +0200

    Fix store

--- main/core/Resources/modules/modals/text/components/modal.jsx
+++ main/core/Resources/modules/modals/text/components/modal.jsx
@@ -5,16 +5,19 @@ import {trans} from '#/main/app/intl/translation'
 import {Button} from '#/main/app/action/components/button'
 import {Modal} from '#/main/app/overlays/modal/components/modal'
 import {ListData} from '#/main/app/content/list/containers/data'
-import {selectors} from '#/main/core/modals/groups/store'
+import {selectors} from '#/main/core/modals/text/store'
 import {CALLBACK_BUTTON} from '#/main/app/buttons'
 
 class TextModal extends Component {
   constructor(props) {
     super(props)
 
-    this.state = {text: '', fetched: false}
+    this.state = {text: '', display: false}
   }
 
+  callback(data) {
+    this.props.handleSelect(data.map(data => data.id))
+  }
 
   render() {
     return(
@@ -23,9 +26,8 @@ class TextModal extends Component {
         icon="fa fa-fw fa-users"
         className="data-text-modal"
         bsSize="lg"
-        onExiting={this.resetText}
       >
-        {!this.state.fetched &&
+        {!this.state.display &&
       <Fragment>
         <textarea
           rows={20}
@@ -39,12 +41,12 @@ class TextModal extends Component {
           type={CALLBACK_BUTTON}
           callback = {() => {
             this.props.search(this.props.fetch.url, this.state.text)
-            this.setState({fetched: true})
+            this.setState({display: true})
           }}
         />
       </Fragment>
         }
-        {this.state.fetched &&
+        {this.state.display &&
         <Fragment>
           <ListData
             name={selectors.STORE_NAME}
@@ -62,16 +64,15 @@ class TextModal extends Component {
             className="modal-btn btn"
             primary={true}
             type={CALLBACK_BUTTON}
-            callback = {() => {
-              this.props.handleSelect(this.props.data)
-            }}
+            callback = {() => this.callback(this.props.data)}
           />
         </Fragment>
         }
-      </Modal>)
+      </Modal>
+    )
   }
-
 }
+
 
 TextModal.propTypes = {
   fetch: T.object,
@@ -81,7 +82,7 @@ TextModal.propTypes = {
   resetText: T.func.isRequired,
   definition: T.object.isRequired,
   card: T.object.isRequired,
-  handleSelect: T.function.isRequired,
+  handleSelect: T.func.isRequired,
   data: T.array
 }
 
