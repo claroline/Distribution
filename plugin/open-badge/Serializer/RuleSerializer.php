3d05d1e47a69628e47e4625cb441d003aa32f19d
ngodfraind <nicolas.godfraind@gmail.com>
2019-08-27 17:30:04 +0200

    Rules fully saved in database for quick search.

--- plugin/open-badge/Serializer/RuleSerializer.php
+++ plugin/open-badge/Serializer/RuleSerializer.php
@@ -3,6 +3,10 @@
 namespace Claroline\OpenBadgeBundle\Serializer;
 
 use Claroline\AppBundle\API\Serializer\SerializerTrait;
+use Claroline\AppBundle\Persistence\ObjectManager;
+use Claroline\CoreBundle\Entity\Group;
+use Claroline\CoreBundle\Entity\Resource\ResourceNode;
+use Claroline\CoreBundle\Entity\Workspace\Workspace;
 use Claroline\OpenBadgeBundle\Entity\Rules\Rule;
 use JMS\DiExtraBundle\Annotation as DI;
 
@@ -12,6 +16,18 @@ use JMS\DiExtraBundle\Annotation as DI;
 class RuleSerializer
 {
     use SerializerTrait;
+
+    /**
+     * @DI\InjectParams({
+     *     "om" = @DI\Inject("claroline.persistence.object_manager")
+     * })
+     *
+     * @param Router $router
+     */
+    public function __construct(ObjectManager $om)
+    {
+        $this->om = $om;
+    }
 
     public function serialize(Rule $rule, array $options = [])
     {
@@ -35,6 +51,32 @@ class RuleSerializer
     {
         $rule->setData($data['data']);
         $rule->setAction($data['type']);
+
+        if (isset($data['data']['workspace'])) {
+            $rule->setWorkspace($this->om->getObject($data['data']['workspace'], Workspace::class));
+        }
+
+        if (isset($data['data']['resource'])) {
+            $rule->setResourceNode($this->om->getObject($data['data']['resource'], ResourceNode::class));
+        }
+
+        switch ($data['type']) {
+            case Rule::RULE_RESOURCE_PASSED:
+                $rule->setResourceNode($this->om->getObject($data['data'], ResourceNode::class));
+                break;
+            case Rule::RULE_RESOURCE_PARTICIPATED:
+                $rule->setResourceNode($this->om->getObject($data['data'], ResourceNode::class));
+                break;
+            case Rule::RULE_WORKSPACE_PASSED:
+                $rule->setWorkspace($this->om->getObject($data['data'], Workspace::class));
+                break;
+            case Rule::IN_GROUP:
+                $rule->setGroup($this->om->getObject($data['data'], Group::class));
+                break;
+            case Rule::IN_ROLE:
+                $rule->setRole($this->om->getObject($data['data'], Role::class));
+                break;
+        }
 
         return $rule;
     }
