91dbf751677649824eaf91ac51fbb5d125ac532b
Elorfin <penin.axel@gmail.com>
2019-08-30 18:28:21 +0200

    travis

--- main/app/Resources/modules/security/store/actions.js
+++ main/app/Resources/modules/security/store/actions.js
@@ -1,6 +1,5 @@
 import get from 'lodash/get'
 
-import {makeActionCreator} from '#/main/app/store/actions'
 import {API_REQUEST} from '#/main/app/api'
 
 import {selectors} from '#/main/app/security/store/selectors'
