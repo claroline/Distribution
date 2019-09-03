0215e7c59ab3688319497ff394cf48e6f9dacf59
ngodfraind <nicolas.godfraind@gmail.com>
2019-09-03 14:43:06 +0200

    Slugs

--- plugin/path/Migrations/pdo_mysql/Version20190821151113.php
+++ plugin/path/Migrations/pdo_mysql/Version20190821151113.php
@@ -16,10 +16,10 @@ class Version20190821151113 extends AbstractMigration
     {
         $this->addSql('
             ALTER TABLE innova_step
-            ADD slug VARCHAR(128) NOT NULL
+            ADD slug VARCHAR(128)
         ');
         $this->addSql("
-             UPDATE innova_step step SET slug = CONCAT(SUBSTR(step.title,1,100) , '-', step.id)
+             UPDATE innova_step step SET slug = REGEXP_REPLACE(SUBSTR(step.title,1,100), '[^A-Za-z0-9]+', '-')
         ");
         $this->addSql('
             CREATE UNIQUE INDEX UNIQ_86F48567989D9B62 ON innova_step (slug)
@@ -33,7 +33,7 @@ class Version20190821151113 extends AbstractMigration
         ');
         $this->addSql('
             ALTER TABLE innova_step
-            DROP slug,
+            DROP slug
         ');
     }
 }
