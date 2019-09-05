ae98cbd456eaf63c70a8040870a2486a41afcd22
kitan1982 <anhthao.pham@claroline.net>
2019-09-05 11:33:04 +0200

    Restores updater120500

--- main/core/Library/Installation/Updater/Updater120500.php
+++ main/core/Library/Installation/Updater/Updater120500.php
@@ -74,6 +74,12 @@ class Updater120500 extends Updater
         $this->createDefaultAdminHomeTab();
         $this->updateSlugs();
         $this->createDefaultWorkspaceShortcuts();
+
+        $this->log('Build default workspace');
+        $this->workspaceManager->getDefaultModel(false, true);
+
+        $this->log('Build default personal workspace');
+        $this->workspaceManager->getDefaultModel(true, true);
     }
 
     private function updatePlatformOptions()
@@ -230,60 +236,6 @@ class Updater120500 extends Updater
         ]);
         $i = 0;
         $this->om->startFlushSuite();
-
-        $this->log('Generating default shortcuts for default workspace models...');
-
-        $defaultModel = $this->om->getRepository(Workspace::class)->findOneBy([
-            'model' => true,
-            'personal' => false,
-            'code' => 'default_workspace',
-        ]);
-        $defaultPersonal = $this->om->getRepository(Workspace::class)->findOneBy([
-            'model' => true,
-            'personal' => true,
-            'code' => 'default_personal',
-        ]);
-
-        if ($defaultModel && 0 === count($shortcutsRepo->findBy(['workspace' => $defaultModel]))) {
-            $managerRole = $this->roleManager->getManagerRole($defaultModel);
-            $collaboratorRole = $this->roleManager->getCollaboratorRole($defaultModel);
-
-            if ($managerRole) {
-                $shortcuts = new Shortcuts();
-                $shortcuts->setWorkspace($defaultModel);
-                $shortcuts->setRole($managerRole);
-                $shortcuts->setData($managerShortCuts);
-                $this->om->persist($shortcuts);
-            }
-            if ($collaboratorRole) {
-                $shortcuts = new Shortcuts();
-                $shortcuts->setWorkspace($defaultModel);
-                $shortcuts->setRole($collaboratorRole);
-                $shortcuts->setData($collaboratorShortCuts);
-                $this->om->persist($shortcuts);
-            }
-        }
-        if ($defaultPersonal && 0 === count($shortcutsRepo->findBy(['workspace' => $defaultPersonal]))) {
-            $managerRole = $this->roleManager->getManagerRole($defaultPersonal);
-            $collaboratorRole = $this->roleManager->getCollaboratorRole($defaultPersonal);
-
-            if ($managerRole) {
-                $shortcuts = new Shortcuts();
-                $shortcuts->setWorkspace($defaultPersonal);
-                $shortcuts->setRole($managerRole);
-                $shortcuts->setData($managerShortCuts);
-                $this->om->persist($shortcuts);
-            }
-            if ($collaboratorRole) {
-                $shortcuts = new Shortcuts();
-                $shortcuts->setWorkspace($defaultPersonal);
-                $shortcuts->setRole($collaboratorRole);
-                $shortcuts->setData($collaboratorShortCuts);
-                $this->om->persist($shortcuts);
-            }
-        }
-
-        $this->log('Default shortcuts for default workspace models generated.');
 
         $this->log('Generating default shortcuts for workspaces...');
 
