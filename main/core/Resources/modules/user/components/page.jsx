5e0b8d0da71353a7b9ec0d89a5e1bd6915b1d1d3
kitan1982 <anhthao.pham@claroline.net>
2019-09-05 19:09:12 +0200

    eslint

--- main/core/Resources/modules/user/components/page.jsx
+++ main/core/Resources/modules/user/components/page.jsx
@@ -70,48 +70,48 @@ const UserPage = props =>
             update: (users) => props.history.push(route(users[0])),
             delete: () => false
           }, props.path, props.currentUser)
-          .then(actions => [
-            {
-              name: 'edit',
-              type: LINK_BUTTON,
-              icon: 'fa fa-pencil',
-              label: trans('edit', {}, 'actions'),
-              target: props.path + '/edit',
-              displayed: hasPermission('edit', props.user),
-              primary: true
-            }/*, {
-              name: 'add-contact',
-              type: CALLBACK_BUTTON,
-              label: trans('add_contact'),
-              icon: 'fa fa-address-book-o',
-              callback: () => true,
-              displayed: false  // TODO : restore
-            }, {
-              name: 'show-badges',
-              type: URL_BUTTON,
-              icon: 'fa fa-trophy',
-              label: trans('user-badges'),
-              group: trans('badges'),
-              target: '#/badges/'+props.user.id,
-              displayed: false // TODO : restore
-            }*/
-          ].concat(actions.map((action, index) => {
-            return {
-              name: `action-${index}`,
-              type: action.type,
-              icon: action.icon,
-              label: action.label,
-              displayed: action.displayed,
-              disabled: action.disabled,
-              dangerous: action.dangerous,
-              primary: action.primary,
-              target: action.target,
-              modal: action.modal,
-              callback: action.callback,
-              request: action.request,
-              confirm: action.confirm
-            }
-          })))
+            .then(actions => [
+              {
+                name: 'edit',
+                type: LINK_BUTTON,
+                icon: 'fa fa-pencil',
+                label: trans('edit', {}, 'actions'),
+                target: props.path + '/edit',
+                displayed: hasPermission('edit', props.user),
+                primary: true
+              }/*, {
+                name: 'add-contact',
+                type: CALLBACK_BUTTON,
+                label: trans('add_contact'),
+                icon: 'fa fa-address-book-o',
+                callback: () => true,
+                displayed: false  // TODO : restore
+              }, {
+                name: 'show-badges',
+                type: URL_BUTTON,
+                icon: 'fa fa-trophy',
+                label: trans('user-badges'),
+                group: trans('badges'),
+                target: '#/badges/'+props.user.id,
+                displayed: false // TODO : restore
+              }*/
+            ].concat(actions.map((action, index) => {
+              return {
+                name: `action-${index}`,
+                type: action.type,
+                icon: action.icon,
+                label: action.label,
+                displayed: action.displayed,
+                disabled: action.disabled,
+                dangerous: action.dangerous,
+                primary: action.primary,
+                target: action.target,
+                modal: action.modal,
+                callback: action.callback,
+                request: action.request,
+                confirm: action.confirm
+              }
+            })))
         }
         scope="object"
       />
