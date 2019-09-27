79d95cfbce246722bc51880568510e0e66cf7ac7
ngodfraind <nicolas.godfraind@gmail.com>
2019-09-27 09:52:39 +0200

    Travis

--- main/app/Command/DebugRouterCommand.php
+++ main/app/Command/DebugRouterCommand.php
@@ -17,7 +17,6 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
-use Symfony\Component\Routing\Router;
 
 class DebugRouterCommand extends ContainerAwareCommand
 {
