94e748d0bf840730165d809ec3e1a293f654bedd
ngodfraind <nicolas.godfraind@gmail.com>
2019-10-10 18:08:24 +0200

    [CoreBundle] Register to archived workspace command.

--- main/core/Command/Dev/WorkspaceRegisterCommand.php
+++ main/core/Command/Dev/WorkspaceRegisterCommand.php
@@ -9,37 +9,30 @@
  * file that was distributed with this source code.
  */
 
-namespace Claroline\CoreBundle\Command\Import;
+namespace Claroline\CoreBundle\Command\Dev;
 
 use Claroline\AppBundle\Command\BaseCommandTrait;
+use Claroline\CoreBundle\Entity\User;
+use Claroline\CoreBundle\Entity\Workspace\Workspace;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class RegisterUserToWorkspaceFromCsvCommand extends ContainerAwareCommand
+class WorkspaceRegisterCommand extends ContainerAwareCommand
 {
     use BaseCommandTrait;
-    private $params = ['user' => 'The username', 'role' => 'the role key'];
+    private $params = ['user' => 'The username'];
 
     protected function configure()
     {
-        $this->setName('claroline:workspace:register-user')
-            ->setDescription('Registers users to workspaces from a csv file')
-            ->setAliases(['claroline:csv:workspace_register']);
+        $this->setName('claroline:workspace:register-to-archived');
         $this->setDefinition(
             [
                 new InputArgument(
                     'user',
                     InputArgument::REQUIRED,
                     'The user username.'
-                ),
-            ],
-            [
-                new InputArgument(
-                    'role',
-                    InputArgument::REQUIRED,
-                    'The role key.'
                 ),
             ]
         );
@@ -48,5 +41,26 @@ class RegisterUserToWorkspaceFromCsvCommand extends ContainerAwareCommand
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $om = $this->getContainer()->get('Claroline\AppBundle\Persistence\ObjectManager');
+        $workspaces = $om->getRepository(Workspace::class)->findBy(['archived' => true, 'personal' => false]);
+        $user = $input->getArgument('user');
+        $user = $om->getRepository(User::class)->findOneByUsername($user);
+        $count = count($workspaces);
+        $output->writeln($count.' found.');
+
+        $i = 0;
+        foreach ($workspaces as $workspace) {
+            ++$i;
+            $roles = $workspace->getRoles();
+            foreach ($roles as $role) {
+                if ('collaborator' === $role->getTranslationKey()) {
+                    $output->writeln($i.'/'.$count.' collaborator added');
+
+                    $user->addRole($role);
+                    $om->persist($user);
+                }
+            }
+        }
+
+        $om->flush();
     }
 }
