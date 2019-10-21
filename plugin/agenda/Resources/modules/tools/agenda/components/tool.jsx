7489c62a82eea6c894106c1a93c1e1a9b8605e87
NICO <nicolas.godfraind@gmail.com>
2019-10-21 13:11:21 +0200

    Remove console.log

--- plugin/agenda/Resources/modules/tools/agenda/components/tool.jsx
+++ plugin/agenda/Resources/modules/tools/agenda/components/tool.jsx
@@ -168,10 +168,7 @@ class AgendaTool extends Component {
                       range: currentRange,
                       previous: currentView.previous,
                       next: currentView.next,
-                      create: (event) => {
-                        console.log(event)
-                        this.props.create(event, this.props.contextData, this.props.currentUser)
-                      },
+                      create: (event) => this.props.create(event, this.props.contextData, this.props.currentUser),
                       events: this.props.events,
                       eventActions: (event) => [
                         {
