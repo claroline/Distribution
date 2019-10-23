40c46742d0fb8991d03480df843ffc7c8dc379f3
Elorfin <penin.axel@gmail.com>
2019-10-23 13:00:40 +0200

    restores send message and add contact actions

--- main/core/Library/Testing/RepositoryTestCase.php
+++ main/core/Library/Testing/RepositoryTestCase.php
@@ -407,7 +407,7 @@ abstract class RepositoryTestCase extends WebTestCase
         $userMessage->setUser($sender);
         $userMessage->setMessage($message);
         if ($removed) {
-            $userMessage->setIsRemoved($removed);
+            $userMessage->setRemoved($removed);
         }
         self::create($alias.'/'.$sender->getUsername(), $userMessage);
         foreach ($receivers as $receiver) {
