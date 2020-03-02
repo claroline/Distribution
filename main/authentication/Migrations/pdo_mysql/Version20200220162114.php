4c6e317779a6582e18b830bf42454cd1cfa72cc3
Elorfin <penin.axel@gmail.com>
2020-03-02 09:54:11 +0100

    phpmd

--- main/authentication/Migrations/pdo_mysql/Version20200220162114.php
+++ main/authentication/Migrations/pdo_mysql/Version20200220162114.php
@@ -14,20 +14,20 @@ class Version20200220162114 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             CREATE TABLE claro_saml_id_entry (
                 id VARCHAR(255) NOT NULL, 
                 entityId VARCHAR(255) NOT NULL, 
                 expiryTimestamp INT NOT NULL, 
                 PRIMARY KEY(entityId, id)
             ) DEFAULT CHARACTER SET UTF8 COLLATE UTF8_unicode_ci ENGINE = InnoDB
-        ");
+        ');
     }
 
     public function down(Schema $schema)
     {
-        $this->addSql("
+        $this->addSql('
             DROP TABLE claro_saml_id_entry
-        ");
+        ');
     }
 }
