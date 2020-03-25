34249d9e4e8bc048cc3f91563751824b62c4e2f1
Elorfin <penin.axel@gmail.com>
2020-03-25 09:10:50 +0100

    yolo

--- main/authentication/Security/Saml/UserCreator.php
+++ main/authentication/Security/Saml/UserCreator.php
@@ -7,23 +7,30 @@ use Claroline\CoreBundle\Entity\User;
 use LightSaml\Model\Protocol\Response;
 use LightSaml\SpBundle\Security\User\UserCreatorInterface;
 use LightSaml\SpBundle\Security\User\UsernameMapperInterface;
+use Symfony\Component\Security\Core\Authentication\Token\AnonymousToken;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserCreator implements UserCreatorInterface
 {
+    /** @var string */
+    private $secret;
+    /** @var TokenStorageInterface */
+    private $tokenStorage;
     /** @var UsernameMapperInterface */
     private $usernameMapper;
-
     /** @var Crud */
     private $crud;
 
     /**
+     * @param string                  $secret
+     * @param TokenStorageInterface   $tokenStorage
      * @param UsernameMapperInterface $usernameMapper
      * @param Crud                    $crud
      */
-    public function __construct(TokenStorageInterface $tokenStorage, UsernameMapperInterface $usernameMapper, Crud $crud)
+    public function __construct(string $secret, TokenStorageInterface $tokenStorage, UsernameMapperInterface $usernameMapper, Crud $crud)
     {
+        $this->secret = $secret;
         $this->tokenStorage = $tokenStorage;
         $this->usernameMapper = $usernameMapper;
         $this->crud = $crud;
@@ -55,9 +62,11 @@ class UserCreator implements UserCreatorInterface
             ->getFirstAttributeStatement()
             ->getFirstAttributeByName('iam-lastname')
             ->getFirstAttributeValue();
+        
 
-        var_dump($this->tokenStorage->getToken());
-        die();
+        // FIXME : I need a token to be able to start user creation process
+        // but it's not already filled and I can't rewrite the whole process to test token existence.
+        $this->setToken();
 
         try {
             /** @var User $user */
@@ -74,5 +83,18 @@ class UserCreator implements UserCreatorInterface
         }
 
         return $user;
+    }
+
+    private function setToken()
+    {
+        if (null !== $this->tokenStorage->getToken()) {
+            // user is already authenticated, there is nothing to do.
+            return;
+        }
+
+        // creates an anonymous token with a dedicated role.
+        $this->tokenStorage->setToken(
+            new AnonymousToken($this->secret, 'anon.', ['ROLE_ANONYMOUS'])
+        );
     }
 }
