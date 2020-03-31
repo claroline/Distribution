8ef1742c8df1134ced003ac49931b5242b4272ab
Elorfin <penin.axel@gmail.com>
2020-03-31 16:06:51 +0200

    fixes new plugin

--- plugin/saml/Resources/modules/sso/saml/index.js
+++ plugin/saml/Resources/modules/sso/saml/index.js
@@ -1,7 +1,7 @@
 import {trans} from '#/main/app/intl/translation'
 
 import {constants} from '#/plugin/saml/sso/saml/constants'
-import {SamlButton} from 'plugin/saml/sso/saml/components/button'
+import {SamlButton} from '#/plugin/saml/sso/saml/components/button'
 
 export default {
   name: constants.SERVICE_NAME,
