7617c7db12ce4ac71f85aad9ef89219b07ef0528
Elorfin <penin.axel@gmail.com>
2020-03-31 16:06:51 +0200

    fixes new plugin

--- plugin/saml/Configuration/PlatformDefaults.php
+++ plugin/saml/Configuration/PlatformDefaults.php
@@ -22,7 +22,13 @@ class PlatformDefaults implements ParameterProviderInterface
                 'saml' => [
                     'active' => false,
                     'entity_id' => 'claroline', // the sp name
-                    'credentials' => [], // the app certificates and secrets
+                    'credentials' => [
+                        [
+                            "certificate" => '%kernel.root_dir%/../saml.crt',
+                            "key" => '%kernel.root_dir%/../saml.pem',
+                            "password" => null
+                        ],
+                    ], // the app certificates and secrets
                     'idp' => [], // the list of IDPs metadata files (either URL or local files are allowed)
                 ]
             ],
