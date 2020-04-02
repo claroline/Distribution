f528fc1134ae1ab6878a508bc466d16daaf6665b
Elorfin <penin.axel@gmail.com>
2020-04-02 10:03:11 +0200

    fix

--- plugin/exo/Tests/Controller/Api/AttemptControllerTest.php
+++ plugin/exo/Tests/Controller/Api/AttemptControllerTest.php
@@ -80,6 +80,8 @@ class AttemptControllerTest extends TransactionalTestCase
     public function testAnonymousAttempt()
     {
         $this->request('POST', "/api/exercises/{$this->ex1->getUuid()}/attempts");
+        var_dump($this->client->getResponse()->getContent());
+        die();
         $this->assertEquals(401, $this->client->getResponse()->getStatusCode());
     }
 
