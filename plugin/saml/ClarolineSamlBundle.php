f351c7905ff397bfd9baba15f88277fd7448811c
Elorfin <penin.axel@gmail.com>
2020-04-02 09:25:06 +0200

    fix

--- plugin/saml/ClarolineSamlBundle.php
+++ plugin/saml/ClarolineSamlBundle.php
@@ -12,14 +12,13 @@
 namespace Claroline\SamlBundle;
 
 use Claroline\CoreBundle\Library\DistributionPluginBundle;
-use Claroline\KernelBundle\Bundle\AutoConfigurableInterface;
 use Claroline\KernelBundle\Bundle\ConfigurationBuilder;
 use Claroline\KernelBundle\Bundle\ConfigurationProviderInterface;
 use Claroline\SamlBundle\DependencyInjection\Compiler\SamlConfigPass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class ClarolineSamlBundle extends DistributionPluginBundle implements ConfigurationProviderInterface, AutoConfigurableInterface
+class ClarolineSamlBundle extends DistributionPluginBundle implements ConfigurationProviderInterface
 {
     public function isActiveByDefault()
     {
