59e552d6a5566a566b8931e47b685d9bc109f371
Elorfin <penin.axel@gmail.com>
2020-09-18 17:12:27 +0200

    fixes application logs

--- main/app/Persistence/ObjectManager.php
+++ main/app/Persistence/ObjectManager.php
@@ -11,16 +11,17 @@
 
 namespace Claroline\AppBundle\Persistence;
 
-use Claroline\BundleRecorder\Log\LoggableTrait;
+use Claroline\AppBundle\Log\LoggableTrait;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\UnitOfWork;
 use Doctrine\Persistence\ObjectManager as ObjectManagerInterface;
 use Doctrine\Persistence\ObjectManagerDecorator;
+use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerInterface;
 use Psr\Log\LogLevel;
 
-class ObjectManager extends ObjectManagerDecorator
+class ObjectManager extends ObjectManagerDecorator implements LoggerAwareInterface
 {
     use LoggableTrait;
 
@@ -36,16 +37,14 @@ class ObjectManager extends ObjectManagerDecorator
      * ObjectManager constructor.
      *
      * @param ObjectManagerInterface $om
-     * @param LoggerInterface        $logger
      */
-    public function __construct(ObjectManagerInterface $om, LoggerInterface $logger)
+    public function __construct(ObjectManagerInterface $om)
     {
         $this->wrapped = $om;
         $this->supportsTransactions
             = $this->hasEventManager
             = $this->hasUnitOfWork
             = $om instanceof EntityManagerInterface;
-        $this->logger = $logger;
     }
 
     /**
