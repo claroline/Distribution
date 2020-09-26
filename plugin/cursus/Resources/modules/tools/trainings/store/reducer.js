2ae39e8c6e15c80d9f3476922ed9a9960a43bf1a
Elorfin <penin.axel@gmail.com>
2020-09-26 09:38:07 +0200

    fix

--- plugin/cursus/Resources/modules/tools/trainings/store/reducer.js
+++ plugin/cursus/Resources/modules/tools/trainings/store/reducer.js
@@ -1,15 +1,8 @@
-import {makeInstanceAction} from '#/main/app/store/actions'
-import {makeReducer, combineReducers} from '#/main/app/store/reducer'
+import {combineReducers} from '#/main/app/store/reducer'
 
-import {TOOL_LOAD} from '#/main/core/tool/store/actions'
-
-import {selectors} from '#/plugin/cursus/tools/trainings/store/selectors'
 import {reducer as catalogReducer} from '#/plugin/cursus/tools/trainings/catalog/store/reducer'
 
 const reducer = combineReducers({
-  parameters: makeReducer({}, {
-    [makeInstanceAction(TOOL_LOAD, selectors.STORE_NAME)]: (state, action) => action.toolData.parameters || {}
-  }),
   catalog: catalogReducer
 })
 
