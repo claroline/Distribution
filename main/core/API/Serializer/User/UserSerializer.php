fa991117c7d4025c5926040a0483f57c470cc652
Axel Penin <Elorfin@users.noreply.github.com>
2020-10-19 18:53:05 +0200

    always expose email to profile owner (#6910)

--- main/core/API/Serializer/User/UserSerializer.php
+++ main/core/API/Serializer/User/UserSerializer.php
@@ -148,7 +148,8 @@ class UserSerializer
         $showEmailRoles = $this->config->getParameter('profile.show_email') ?? [];
         $showEmail = false;
         if ($token) {
-            $showEmail = !empty(array_filter($token->getRoles(), function (BaseRole $role) use ($showEmailRoles) {
+            $isOwner = $token->getUser() instanceof User && $token->getUser()->getUuid() === $user->getUuid();
+            $showEmail = $isOwner || !empty(array_filter($token->getRoles(), function (BaseRole $role) use ($showEmailRoles) {
                 return 'ROLE_ADMIN' === $role->getRole() || in_array($role->getRole(), $showEmailRoles);
             }));
         }
